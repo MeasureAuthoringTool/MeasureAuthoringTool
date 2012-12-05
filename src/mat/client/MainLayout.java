bf7ac22bd7caafcc5792a67e24baaeb1ed962923
Jyoti Narang <JNarang@hcareis.com>
2012-12-05 17:30:50 +0000

    Added New constants  MainLayout.java Added util class for adding anchor in right side of footer. This contains all the events added for this anchor
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@115 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: e6f07958a890bffeddc0a2bd74f3c79eb8c372fa

--- src/mat/client/MainLayout.java
+++ src/mat/client/MainLayout.java
@@ -9,18 +9,9 @@ import mat.client.shared.SpacerWidget;
 
 import mat.client.util.ClientConstants;
 import mat.client.util.FooterLinksUtility;
+import mat.client.util.FooterRightSideAnchor;
 import mat.shared.ConstantMessages;
 
-
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyPressEvent;
-import com.google.gwt.event.dom.client.KeyPressHandler;
-import com.google.gwt.event.dom.client.MouseOutEvent;
-import com.google.gwt.event.dom.client.MouseOutHandler;
-import com.google.gwt.event.dom.client.MouseOverEvent;
-import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Anchor;
@@ -139,47 +130,7 @@ public abstract class MainLayout {
 		desc.setStylePrimaryName("footer-address-text");
 		footerLogoPanel.add(desc);
 		
-		final Anchor footerRightAnchor = new Anchor();
-		footerRightAnchor.setStylePrimaryName("footerLogo-Right");
-		footerRightAnchor.setTitle(ClientConstants.TEXT_HHS_LINK);
-		
-		final Image rightlogo = new Image(ImageResources.INSTANCE.hhslogo());
-		
-		footerRightAnchor.addClickHandler(new ClickHandler() {
-			@Override
-			public void onClick(final ClickEvent event) {
-				if(!MatContext.get().getCurrentModule().equalsIgnoreCase(ConstantMessages.LOGIN_MODULE)){
-					MatContext.get().restartTimeoutWarning();
-				}
-				MatContext.get().openURL(ClientConstants.EXT_LINK_HHS_Site);
-			}
-		  }
-		);
-		footerRightAnchor.addKeyPressHandler(new KeyPressHandler() {
-			@Override
-			public void onKeyPress(KeyPressEvent event) {
-				if(event.getCharCode() == KeyCodes.KEY_ENTER){
-					if(!MatContext.get().getCurrentModule().equalsIgnoreCase(ConstantMessages.LOGIN_MODULE)){
-						MatContext.get().restartTimeoutWarning();
-					}
-					MatContext.get().openURL(ClientConstants.EXT_LINK_HHS_Site);
-				}
-			}
-		});
-		footerRightAnchor.addMouseOverHandler(new MouseOverHandler(){
-			@Override
-			public void onMouseOver(MouseOverEvent event) {
-				DOM.setStyleAttribute(rightlogo.getElement(), "cursor", "pointer");
-			}
-		 });
-		
-		footerRightAnchor.addMouseOutHandler(new MouseOutHandler() {
-		    @Override
-		    public void onMouseOut(MouseOutEvent event) {
-		        DOM.setStyleAttribute(rightlogo.getElement(), "cursor", "default");                        
-		    }
-		});
-		footerRightAnchor.getElement().appendChild(rightlogo.getElement());
+		final Anchor footerRightAnchor = FooterRightSideAnchor.rightSideAnchorMatFooter();
 		footerLogoPanel.add(footerRightAnchor);
 		footerMainPanel.add(footerLogoPanel);
 		

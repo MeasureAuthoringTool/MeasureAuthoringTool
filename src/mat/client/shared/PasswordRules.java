ea6c725b029324f4a4fcae2277d624cec5ca053f
Jyoti Narang <JNarang@hcareis.com>
2012-12-10 14:52:09 +0000

    "Passwords must not consist of a single dictionary word with letters, numbers and symbols. <br><br>Passwords must be between 8 and 16 characters and contain at least one of each of  the following:"
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@169 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: b23a6d7d559729ea0b6f69e66392bbe18af51388

--- src/mat/client/shared/PasswordRules.java
+++ src/mat/client/shared/PasswordRules.java
@@ -11,7 +11,8 @@ public class PasswordRules extends Composite {
 		FlowPanel fPanel = new FlowPanel();
 		Label rules = new Label("Password Rules");
 		rules.addStyleName("bold");
-		HTML desc = new HTML("Passwords must not consist of a single dictionary word with letters, numbers and symbols. <br><br> Passwords must be between 8 and 16 characters and contain each one the following:");
+		HTML desc = new HTML("Passwords must not consist of a single dictionary word with letters, numbers and symbols. <br><br> Passwords must be between 8 and 16 characters and contain at least one of each of  the following:"
+							);
 		desc.addStyleName("passwordRulesDescription");
 		HTML b1 = new HTML("<img src='images/bullet.png'/> Upper case character");
 		HTML b2 = new HTML("<img src='images/bullet.png'/> Lower case character");

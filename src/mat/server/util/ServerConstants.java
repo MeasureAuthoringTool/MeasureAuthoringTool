1a927a4ce0abb0a5353875ddceef4fd8414e9c65
Jyoti Narang <JNarang@hcareis.com>
2012-12-14 18:21:12 +0000

    Added constants for server module
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@235 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: f23577fe44ec7a8a7d1811e1cc00ce53527d59b3

---
+++ src/mat/server/util/ServerConstants.java
@@ -0,0 +1,7 @@
+package mat.server.util;
+
+public class ServerConstants {
+	
+	public final static String NEW_ACCESS_SUBJECT="Your MAT Account Has Been Created.";
+	public final static String TEMP_PWD_SUBJECT ="Your MAT Account";
+}

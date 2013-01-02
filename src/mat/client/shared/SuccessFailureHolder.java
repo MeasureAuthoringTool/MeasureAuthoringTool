3a55354404a408f712086816e31617db471767da
Jyoti Narang <JNarang@hcareis.com>
2013-01-02 18:36:29 +0000

    Added message List for putting error message
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@389 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 1fe2523fcdf4521e892cd286603a6c425d44850e

--- src/mat/client/shared/SuccessFailureHolder.java
+++ src/mat/client/shared/SuccessFailureHolder.java
@@ -1,10 +1,13 @@
 package mat.client.shared;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class SuccessFailureHolder implements IsSerializable {
 	private boolean success;
 	private int failureReason;
+	private List<String> messages;
 	public boolean isSuccess() {
 		return success;
 	}
@@ -16,6 +19,18 @@ public class SuccessFailureHolder implements IsSerializable {
 	}
 	public void setFailureReason(int failureReason) {
 		this.failureReason = failureReason;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessages(List<String> messages) {
+		this.messages = messages;
+	}
+	/**
+	 * @return the message
+	 */
+	public List<String> getMessages() {
+		return messages;
 	}
 	
 	

88cfaf56a9840a345d4617a7f5b373a9bb63cfa9
Sangeethaa Karunakaran <skarunakaran@hcareis.com>
2013-01-04 17:06:04 +0000

    Moved the "en_US.dic" file to "src" and loaded using classLoader.getResource()
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@421 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: eb54f3dacf822e5463d6ea25c95259c54b73d2bc

---
+++ src/mat/server/util/ResourceLoader.java
@@ -0,0 +1,35 @@
+package mat.server.util;
+
+import java.io.InputStream;
+import java.net.URL;
+
+
+/**
+ * @author skarunakaran
+ *
+ */
+/**
+ * @author skarunakaran
+ *
+ */
+public class ResourceLoader {
+	
+	/**
+	 * Loads the file from /WEB-INF/classes
+	 * @param fileName
+	 * @return the URL object of the File; null if File doesn't exists 
+	 */
+	public URL getResourceAsURL(String fileName){
+		return this.getClass().getClassLoader().getResource(fileName);
+	}
+	
+	/**
+	 * Loads the file from /WEB-INF/classes
+	 * @param fileName
+	 * @return InputStream object of the File; null if File doesn't exists 
+	 */
+	public InputStream getResourceAsStream(String fileName){
+		return this.getClass().getClassLoader().getResourceAsStream(fileName);
+	}
+	
+}
--- src/mat/server/util/dictionary/CheckDictionaryWordInPassword.java
+++ src/mat/server/util/dictionary/CheckDictionaryWordInPassword.java
@@ -10,6 +10,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import mat.server.util.ResourceLoader;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -30,11 +32,14 @@ import edu.vt.middleware.dictionary.sort.ArraysSort;
 public class CheckDictionaryWordInPassword {
 	private static final Log logger = LogFactory.getLog(CheckDictionaryWordInPassword.class);
 	public static boolean containsDictionaryWords(String passWord) throws FileNotFoundException, IOException{
-		//TODO - Load this file from classpathLoader.
-		AbstractWordList awl = WordLists.createFromReader(new FileReader[] {new FileReader("../applications/MeasureAuthoringTool/en_US.dic")},false,new ArraysSort());
-		//Use this line for dev box
-		//AbstractWordList awl = WordLists.createFromReader(new FileReader[] {new FileReader("en_US.dic")},false,new ArraysSort());
-	// 	create a dictionary for searching
+		
+		String fileName = "en_US.dic";
+		URL dicUrl = new ResourceLoader().getResourceAsURL(fileName);
+		if(null == dicUrl){
+			throw new FileNotFoundException(fileName + " doesnot exists in /WEB-INF/classes location");
+		}
+		AbstractWordList awl = WordLists.createFromReader(new FileReader[] {new FileReader(dicUrl.getFile())},false,new ArraysSort());
+		// 	create a dictionary for searching
 		WordListDictionary dict = new WordListDictionary(awl);
 		DictionarySubstringRule dictRule = new DictionarySubstringRule(dict);
 		dictRule.setWordLength(8); // size of words to check in the password

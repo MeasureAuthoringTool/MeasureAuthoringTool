fb263bb46984a0b3398bea2ebc14b93764620fc6
Chinmay Bajikar <CBajikar@telligen.com>
2013-01-08 22:52:35 +0000

    testing
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@461 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 64766f56673dedd1ad0540160df7c27784513936

--- src/mat/server/SpringRemoteServiceServlet.java
+++ src/mat/server/SpringRemoteServiceServlet.java
@@ -14,7 +14,7 @@ import com.google.gwt.user.server.rpc.SerializationPolicy;
 
 public class SpringRemoteServiceServlet extends RemoteServiceServlet {
 	private static final Log logger = LogFactory.getLog(SpringRemoteServiceServlet.class);
-	private static String BASE_URL = "/mat/MeasureAuthoringTool";
+	private static String BASE_URL = "/MeasureAuthoringTool";
 	private static final long serialVersionUID = 8359364426336388916L;
 	protected ApplicationContext context;
 	
@@ -41,9 +41,13 @@ public class SpringRemoteServiceServlet extends RemoteServiceServlet {
 			return super.doGetSerializationPolicy(request, moduleBaseURL, strongName);
 		}
 		catch(Exception exc) {
+			System.out.println("moduleBaseURL:"+moduleBaseURL);
 			String uri = request.getRequestURI();
+			System.out.println("uri:"+uri);
 			String base = request.getScheme() + "://" + request.getServerName() + uri;
+			System.out.println("base 1:"+base);
 			base = base.substring(0,base.lastIndexOf("/") + 1);
+			System.out.println("base 2:"+base);
 			return super.doGetSerializationPolicy(request, base, strongName);
 		}
 	}

2adad4269cea1389fd87c1bd85adffddbc56569c
Jyoti Narang <JNarang@hcareis.com>
2013-01-23 23:51:54 +0000

    Updated Transfer error message
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@658 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: cc18d72d0805b708c76270000ac1256ae661fa8a

--- src/mat/client/shared/MessageDelegate.java
+++ src/mat/client/shared/MessageDelegate.java
@@ -865,7 +865,7 @@ public class MessageDelegate {
 		return MEASURE_SELECTION_ERROR;
 	}
 	
-	private final String TRANSFER_CHECKBOX_ERROR = "Please Select at least one Value Set check box to transfer";
+	private final String TRANSFER_CHECKBOX_ERROR = "Please select at least one Value Set check box to transfer";
 	public String getTransferCheckBoxError(){
 		return TRANSFER_CHECKBOX_ERROR;
 	}

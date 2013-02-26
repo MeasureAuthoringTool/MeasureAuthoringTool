c00f48c5336c9455836dc6b80306b9322bc5efc4
Sangeethaa Karunakaran <skarunakaran@hcareis.com>
2013-02-26 18:35:57 +0000

    renaming modal to model
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@959 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8c70cd760d6a7d17e3b5fa1272d41376ba8e6d86

---
+++ src/mat/client/clause/clauseworkspace/model/MeasureExportModal.java
@@ -0,0 +1,55 @@
+package mat.client.clause.clauseworkspace.model;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class MeasureExportModal implements IsSerializable{
+	
+	private String meausreExportId;
+	
+	private String measureId;
+	
+	private String xml;
+
+	/**
+	 * @return the meausreExportId
+	 */
+	public String getMeausreExportId() {
+		return meausreExportId;
+	}
+
+	/**
+	 * @param meausreExportId the meausreExportId to set
+	 */
+	public void setMeausreExportId(String meausreExportId) {
+		this.meausreExportId = meausreExportId;
+	}
+
+	/**
+	 * @return the measureId
+	 */
+	public String getMeasureId() {
+		return measureId;
+	}
+
+	/**
+	 * @param measureId the measureId to set
+	 */
+	public void setMeasureId(String measureId) {
+		this.measureId = measureId;
+	}
+
+	/**
+	 * @return the xml
+	 */
+	public String getXml() {
+		return xml;
+	}
+
+	/**
+	 * @param xml the xml to set
+	 */
+	public void setXml(String xml) {
+		this.xml = xml;
+	}
+
+}

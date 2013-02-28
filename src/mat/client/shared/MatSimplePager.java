82a6307d8f726fd891c1f430638566444dffb328
Jyoti Narang <JNarang@hcareis.com>
2013-02-28 19:35:14 +0000

    Added Tool Tip and Tab Index on Pager images
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@1014 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 290f51189499be8cf3b3cae8397d9a9d756b983c

--- src/mat/client/shared/MatSimplePager.java
+++ src/mat/client/shared/MatSimplePager.java
@@ -1,31 +1,70 @@
 package mat.client.shared;
 
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
 import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.view.client.Range;
 
 public class MatSimplePager extends SimplePager {
 
-    public MatSimplePager() {
-        this.setRangeLimited(true);
-    }
+	public MatSimplePager() {
+		this.setRangeLimited(true);
+	}
 
-    public MatSimplePager(TextLocation location, Resources resources, boolean showFastForwardButton, int fastForwardRows, boolean showLastPageButton) {
-        super(location, resources, showFastForwardButton, fastForwardRows, showLastPageButton);
-        this.setRangeLimited(true);
-    }
+	public MatSimplePager(TextLocation location, Resources resources, boolean showFastForwardButton, int fastForwardRows, boolean showLastPageButton) {
+		super(location, resources, showFastForwardButton, fastForwardRows, showLastPageButton);
+		this.setRangeLimited(true);
+	}
 
-    public void setPageStart(int index) {
+	public void setPageStart(int index) {
 
-        if (this.getDisplay() != null) {
-          Range range = getDisplay().getVisibleRange();
-          int pageSize = range.getLength();
-          if (!isRangeLimited() && getDisplay().isRowCountExact()) {
-            index = Math.min(index, getDisplay().getRowCount() - pageSize);
-          }
-          index = Math.max(0, index);
-          if (index != range.getStart()) {
-            getDisplay().setVisibleRange(index, pageSize);
-          }
-        }  
-      }
-    }
+		if (this.getDisplay() != null) {
+			Range range = getDisplay().getVisibleRange();
+			int pageSize = range.getLength();
+			if (!isRangeLimited() && getDisplay().isRowCountExact()) {
+				index = Math.min(index, getDisplay().getRowCount() - pageSize);
+			}
+			index = Math.max(0, index);
+			if (index != range.getStart()) {
+				getDisplay().setVisibleRange(index, pageSize);
+			}
+		}  
+	}
+	
+	/**
+	 * Custom method to add tool tip and tab index on First,Next,Previous and Last Page icons.
+	 * */
+	
+	public void setToolTipAndTabIndex(MatSimplePager spager ){
+		final NodeList<Element> tdElems = spager.getElement().getElementsByTagName("td");
+		for (int i = 0; i < tdElems.getLength(); i++) {
+			final String toolTipText;
+			if (i == 0){
+				toolTipText = "First page";
+				tdElems.getItem(i).setTabIndex(0);
+			}
+			else if (i == 1){
+				toolTipText = "Previous page";
+				tdElems.getItem(i).setTabIndex(0);
+			}
+			else if (i == 2){
+				continue; /* This is the middle td - no button */
+			}
+			else if (i == 3){
+				toolTipText = "Next page";
+				tdElems.getItem(i).setTabIndex(0);
+			}
+			else if (i == 4){
+				toolTipText = "Last page";
+				tdElems.getItem(i).setTabIndex(0);
+			}
+			else
+				continue;
+			tdElems.getItem(i).setTitle(toolTipText);
+		}
+
+	}
+
+
+
+}

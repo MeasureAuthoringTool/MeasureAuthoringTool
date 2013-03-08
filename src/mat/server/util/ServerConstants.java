aedb65eceee0db229701a04b69cfeb5f5ab3876c
Jyoti Narang <JNarang@hcareis.com>
2013-03-08 20:13:04 +0000

    Your MAT Account User Information - subject changed for email
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@1096 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: aade13bf1efe6ab91dd96d67c7aec3d081c32878

--- src/mat/server/util/ServerConstants.java
+++ src/mat/server/util/ServerConstants.java
@@ -3,6 +3,6 @@ package mat.server.util;
 public class ServerConstants {
 	
 	public final static String NEW_ACCESS_SUBJECT="Your MAT Account Has Been Created.";
-	public final static String FORGOT_LOGINID_SUBJECT="Your MAT User Account Information.";
+	public final static String FORGOT_LOGINID_SUBJECT="Your MAT User Account Information";
 	public final static String TEMP_PWD_SUBJECT ="Your MAT Account";
 }

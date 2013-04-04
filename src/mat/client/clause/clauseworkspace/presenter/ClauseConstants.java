92f54154027b111a9acb20613d779b5e066d11b8
Jyoti Narang <JNarang@hcareis.com>
2013-04-04 20:10:37 +0000

    Added Constants for type and displayname
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@1327 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8436fc555618e9e16d99d6107886b48259cadd25

--- src/mat/client/clause/clauseworkspace/presenter/ClauseConstants.java
+++ src/mat/client/clause/clauseworkspace/presenter/ClauseConstants.java
@@ -6,18 +6,22 @@ import java.util.Map;
 public class ClauseConstants{
 
 	private static Map<String, String> constantsMap = new HashMap<String, String>();
-	
+
 	private static Map<String, String> childMap = new HashMap<String, String>();
-	
+
 	private static final String[] POPULATIONS = {"Initial Patient Populations", "Numerators", "Numerator Exclusions", "Denominators", "Denominator Exclusions", "Denominator Exceptions", "Measure Populations"};
-	
+
 	public static final String LOG_OP = "logicalOp";
-	
+
 	public static final String CLAUSE_TYPE = "clause";
-	
+
+	public static final String TYPE="type";
+
+	public static final String DISPLAY_NAME="displayName";
+
 	public static final String AND = "AND";
 	public static final String MASTER_ROOT_NODE_POPULATION = "Populations";
-	
+
 	public static final String ROOT_NODES = "|strata|measureObservations|numerators|denominators|numeratorExclusions|denominatorExclusions|denominatorExceptions|initialPatientPopulations|measurePopulations";
 	static{
 		constantsMap.put("populations", "Populations");
@@ -33,53 +37,24 @@ public class ClauseConstants{
 		constantsMap.put("Measure Populations", "measurePopulations");
 		constantsMap.put("Numerator Exclusions", "numeratorExclusions");
 		constantsMap.put("Populations", "populations");
-		//createDefaultChildren();
-		//createClauseTypeNodeNameMap();
 	}
-	
+
 	public static String get(String key){
 		return constantsMap.get(key);
 	}
-	
-	private static void createClauseTypeNodeNameMap() {
-		childMap.put("strata", "stratum");		
-		childMap.put("numerators", "numerator");
-		childMap.put("initialPatientPopulations", "initialPatientPopulation");
-		childMap.put("numeratorExclusions", "numeratorExclusion");
-		childMap.put("denominators", "denominator");
-		childMap.put("denominatorExclusions", "denominatorExclusion");
-		childMap.put("denominatorExceptions", "denominatorException");
-		childMap.put("measurePopulations", "measurePopulation");
-		childMap.put("measureObservations", "measureObservation");
-	}
+
+
 
 	public static String getClauseTypeNodeName(String key){
 		return childMap.get(key);
 	}
-	
+
 	public static String[] getPopulationsChildren(){
 		return POPULATIONS;
 	}
-	
+
 	public static void put(String key, String value){
 		constantsMap.put(key, value);
 	}
-	
-	private static void createDefaultChildren(){
-		for (int i = 0; i < POPULATIONS.length; i++) {
-			createTitleWithDefaultNodeName(POPULATIONS[i]);
-		}
-		createTitleWithDefaultNodeName("measureObservations");
-		createTitleWithDefaultNodeName("stratification");
-	}
-	
-	private static void createTitleWithDefaultNodeName(String nodeName){
-		String key = nodeName + "" + 1;
-		String val = ClauseConstants.get(nodeName);
-		if(nodeName.lastIndexOf("s") == nodeName.length() -1){
-			val =  ClauseConstants.get(nodeName).substring(0, ClauseConstants.get(nodeName).length() - 1);
-		}
-		String value = val + "" + 1;
-		constantsMap.put(key, value);
-	}
+
 }

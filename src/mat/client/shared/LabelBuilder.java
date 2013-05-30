fa2cc8eeb8f433904de3f0b1b7cd2b634b7f5914
Jyoti Narang <JNarang@hcareis.com>
2013-05-30 19:28:08 +0000

    MAT 508 (1569 and 1313)- Added ID attribute in label.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@1996 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 901027421c9ee79fe4c47bd04c6ba7b1a89b6091

--- src/mat/client/shared/LabelBuilder.java
+++ src/mat/client/shared/LabelBuilder.java
@@ -5,35 +5,41 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class LabelBuilder {
 	public static Widget buildLabel(Widget widget, String labelStr) {
-		widget.getElement().setAttribute("id", labelStr);
-		HTML label = new HTML("<label for='" + labelStr + "'>" + labelStr + "</label>");
+		//widget.getElement().setAttribute("id", labelStr);
+		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>" + labelStr + "</label>");
+		label.getElement().setAttribute("id", labelStr);
 		return label;
 	}
 	public static Widget buildRequiredLabel(Widget widget, String labelStr) {
-		widget.getElement().setAttribute("id", labelStr);
-		HTML label = new HTML("<label for='" + labelStr + "'>" + labelStr + RequiredIndicator.get() + "</label>" );
+	//	widget.getElement().setAttribute("id", labelStr);
+		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>" + labelStr + RequiredIndicator.get() + "</label>" );
+		label.getElement().setAttribute("id", labelStr);
 		return label;
 	}
 	public static Widget buildLabel(String labelStr, String forVal) {
-		HTML label = new HTML("<label for='" + forVal + "'>" + labelStr + "</label>");
+		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + forVal + "'>" + labelStr + "</label>");
+		label.getElement().setAttribute("id", labelStr);
 		return label;
 	}
 	public static Widget buildInvisibleLabel(Widget widget, String labelStr) {
-		widget.getElement().setAttribute("id", labelStr);
-		HTML label = new HTML("<label for='" + labelStr + "'>"+labelStr+"</label>");
+		//widget.getElement().setAttribute("id", labelStr);
+		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>"+labelStr+"</label>");
+		label.getElement().setAttribute("id", labelStr);
 		label.setStyleName("invisible");
 		return label;
 	}
 	public static Widget setInvisibleLabel(Widget widget, HTML label, String labelStr) {
-		widget.getElement().setAttribute("id", labelStr);
-		label.setHTML("<label for='" + labelStr + "'>"+labelStr+"</label>");
+	//	widget.getElement().setAttribute("id", labelStr);
+		label.setHTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>"+labelStr+"</label>");
+		label.getElement().setAttribute("id", labelStr);
 		label.setStyleName("invisible");
 		return label;
 	}
 	
 	public static Widget buildLabelWithEmbeddedLink(Widget widget, String labelStr,String embeddedString){
-		widget.getElement().setAttribute("id", labelStr);
+		//widget.getElement().setAttribute("id", labelStr);
 		HTML label = new HTML("<a class='invisible' name='"+embeddedString+"'></a><label for='" + labelStr + "'>" + labelStr + "</label>");
+		label.getElement().setAttribute("id", labelStr);
 		return label;
 	}
 	

3137e4e0af91d453a94931f938eb297a6a056bfe
Jyoti Narang <JNarang@hcareis.com>
2013-06-14 21:41:33 +0000

    Button Text for icons is shown when CSS is turned off. THis is done as 508 fix.Removed class attribute from Button as gwt-button class has font-size=small. For text, Span which is created is not setting style as gwt-button class ovverrides that. new CSS invisibleButtonText is written for sytling for this with font-size:0 px.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@2273 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 09b778dd5cd0c2bead3c871faec0d40c5b96c513

--- src/mat/client/shared/CustomButton.java
+++ src/mat/client/shared/CustomButton.java
@@ -11,6 +11,7 @@ public class CustomButton extends Button {
 	Image img;
 	public CustomButton(){
 		super();
+		getElement().removeAttribute("class");
 	}
 	public void setResource(ImageResource imageResource, String imageTitle){ 
 		img = new Image(imageResource); 
@@ -18,12 +19,14 @@ public class CustomButton extends Button {
 		String definedStyles = img.getElement().getAttribute("style"); 
 		img.getElement().setAttribute("style", definedStyles + "; vertical-align:middle;"); 
 		DOM.insertBefore(getElement(), img.getElement(), DOM.getFirstChild(getElement())); 
+		setText(imageTitle);
 	}
 	@Override 
 	public void setText(String text) { 
 		this.text = text; 
-		Element span = DOM.createElement("span"); span.setInnerText(text); 
-		span.setAttribute("style", "padding-left:3px; vertical-align:middle;"); 
+		Element span = DOM.createElement("span"); 
+		span.setInnerText(text); 
+		span.setAttribute("style", "padding-left:3px; vertical-align:middle;");
 		DOM.insertChild(getElement(), span, 0); 
 	}
 	@Override 

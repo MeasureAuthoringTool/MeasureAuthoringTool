80c1d789b3074381806c2fbff4583b08b955822b
Yogeshree Tawde <ytawde@hcareis.com>
2013-06-17 20:39:13 +0000

    MAT-1980 issues fixed
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@2299 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: b464547057bc88792cc29e307d4d470504d3bb0b

--- src/mat/dao/impl/clause/MeasureXMLDAO.java
+++ src/mat/dao/impl/clause/MeasureXMLDAO.java
@@ -1,14 +1,29 @@
 package mat.dao.impl.clause;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import mat.dao.ListObjectDAO;
 import mat.dao.search.GenericDAO;
+import mat.model.ListObject;
+import mat.model.QualityDataModelWrapper;
+import mat.model.QualityDataSetDTO;
 import mat.model.clause.MeasureXML;
+import mat.shared.ConstantMessages;
 
 public class MeasureXMLDAO extends GenericDAO<MeasureXML, String>  implements mat.dao.clause.MeasureXMLDAO {
+	
+	@Autowired
+	private ListObjectDAO listObjectDAO;
+	
+	@Autowired
+	private mat.dao.DataTypeDAO dataTypeDAO;
 	
 	@Override
 	public MeasureXML findForMeasure(String measureId) {
@@ -24,4 +39,60 @@ public class MeasureXMLDAO extends GenericDAO<MeasureXML, String>  implements ma
 		}
 	}
 
+	@Override
+	public QualityDataModelWrapper createSupplimentalQDM(String measureId, boolean isClone, HashMap<String,String> uuidMap){
+		//Get the Supplimental ListObject from the list_object table
+		List<ListObject> listOfSuppElements = listObjectDAO.getSupplimentalCodeList();
+		QualityDataModelWrapper wrapper = new QualityDataModelWrapper();
+		ArrayList<QualityDataSetDTO> qdsList = new ArrayList<QualityDataSetDTO>();
+		wrapper.setQualityDataDTO(qdsList);
+		for(ListObject lo : listOfSuppElements){
+			QualityDataSetDTO qds = new QualityDataSetDTO();
+			qds.setOid(lo.getOid());
+			qds.setCodeListName(lo.getName());
+			qds.setTaxonomy(lo.getCodeSystem().getDescription());
+			qds.setVersion("1");
+			qds.setId(lo.getId());
+			if(isClone && uuidMap != null){
+				if(lo.getOid().equalsIgnoreCase("2.16.840.1.113762.1.4.1")){
+					//find out patient characteristic gender dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_GENDER, lo.getCategory().getId())).getDescription());
+					qds.setUuid(uuidMap.get(ConstantMessages.GENDER));
+				}else if(lo.getOid().equalsIgnoreCase("2.16.840.1.114222.4.11.836")){
+					//find out patient characteristic race dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_RACE, lo.getCategory().getId())).getDescription());
+					qds.setUuid(uuidMap.get(ConstantMessages.RACE));
+				}else if(lo.getOid().equalsIgnoreCase("2.16.840.1.114222.4.11.837")){
+					//find out patient characteristic ethnicity dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_ETHNICITY, lo.getCategory().getId())).getDescription());
+					qds.setUuid(uuidMap.get(ConstantMessages.ETHNICITY));
+				}else if(lo.getOid().equalsIgnoreCase("2.16.840.1.114222.4.11.3591")){
+					//find out patient characteristic payer dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_PAYER, lo.getCategory().getId())).getDescription());
+					qds.setUuid(uuidMap.get(ConstantMessages.PAYER));
+				}
+				
+			}else{	
+				qds.setUuid(UUID.randomUUID().toString());
+				if(lo.getOid().equalsIgnoreCase("2.16.840.1.113762.1.4.1")){
+					//find out patient characteristic gender dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_GENDER, lo.getCategory().getId())).getDescription());
+				}else if(lo.getOid().equalsIgnoreCase("2.16.840.1.114222.4.11.836")){
+					//find out patient characteristic race dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_RACE, lo.getCategory().getId())).getDescription());
+				}else if(lo.getOid().equalsIgnoreCase("2.16.840.1.114222.4.11.837")){
+					//find out patient characteristic ethnicity dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_ETHNICITY, lo.getCategory().getId())).getDescription());
+				}else if(lo.getOid().equalsIgnoreCase("2.16.840.1.114222.4.11.3591")){
+					//find out patient characteristic payer dataType.
+					qds.setDataType((dataTypeDAO.findDataTypeForSupplimentalCodeList(ConstantMessages.PATIENT_CHARACTERISTIC_PAYER, lo.getCategory().getId())).getDescription());
+				}
+			}
+			qds.setSuppDataElement(true);
+			//getMeasurePackageService().saveSupplimentalQDM(qds);
+			wrapper.getQualityDataDTO().add(qds);
+		}		
+		return wrapper;
+	}
+	
 }

f7f76005838ac82d9bd3342f47dbd348b4bf16ca
Yogeshree Tawde <ytawde@hcareis.com>
2013-06-17 20:37:54 +0000

    MAT-1980 issues fixed
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@2298 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 51c27a5dd13fdd98bb1592a7bcffd169e4877d74

--- src/mat/dao/clause/MeasureXMLDAO.java
+++ src/mat/dao/clause/MeasureXMLDAO.java
@@ -1,8 +1,12 @@
 package mat.dao.clause;
 
+import java.util.HashMap;
+
 import mat.dao.IDAO;
+import mat.model.QualityDataModelWrapper;
 import mat.model.clause.MeasureXML;
 
 public interface MeasureXMLDAO extends IDAO<MeasureXML, String> {
 	public MeasureXML findForMeasure(String measureId);
+	public QualityDataModelWrapper createSupplimentalQDM(String measureId, boolean isClone, HashMap<String,String> uuidMap);
 }

11a800e176629b99f75bd4526dab00e4c454ede9
Chinmay Bajikar <CBajikar@telligen.com>
2013-07-05 15:05:16 +0000

    Moved active user csv list generation code from AdminServiceImpl to ExportServlet
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@2558 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 4e353d79c9a27867a600097d17ef10edfb3e77d0

--- src/mat/server/AdminServiceImpl.java
+++ src/mat/server/AdminServiceImpl.java
@@ -130,11 +130,6 @@ public class AdminServiceImpl extends SpringRemoteServiceServlet implements Admi
 		getUserService().deleteUser(userId);
 	}
 	
-	@Override
-	public void generateCSVOfActiveUserEmails() throws InCorrectUserRoleException{
-		System.out.println("Generating CSV of email addrs for all Active Users...");
-	}
-
 	private UserService getUserService() {
 		return (UserService)context.getBean("userService");
 	}
--- src/mat/server/ExportServlet.java
+++ src/mat/server/ExportServlet.java
@@ -1,20 +1,28 @@
 package mat.server;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mat.client.admin.service.AdminService;
+import mat.model.User;
 import mat.server.service.SimpleEMeasureService;
+import mat.server.service.UserService;
 import mat.server.service.SimpleEMeasureService.ExportResult;
 import mat.shared.FileNameUtility;
+import mat.shared.InCorrectUserRoleException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class ExportServlet extends HttpServlet {
+	private static final Log logger = LogFactory.getLog(ExportServlet.class);
 	private static final long serialVersionUID = 4539514145289378238L;
 	protected ApplicationContext context;
 	
@@ -73,17 +81,56 @@ public class ExportServlet extends HttpServlet {
 				resp.getOutputStream().write(export.wkbkbarr);
 				resp.getOutputStream().close();
 				export.wkbkbarr = null;
+			}else if("exportActiveNonAdminUsersCSV".equals(format)){
+				String userRole = LoggedInUserUtil.getLoggedInUserRole();
+				if("Administrator".equalsIgnoreCase(userRole)){
+					String csvFileString = generateCSVOfActiveUserEmails();
+					resp.setHeader("Content-Disposition", "attachment; filename=activeUsers.csv");
+					resp.setContentType("text/csv");
+					resp.getOutputStream().write(csvFileString.getBytes());
+					resp.getOutputStream().close();
+				}
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			throw new ServletException(e);
 		}
-		if(! "codelist".equals(format))
+		if(! "codelist".equals(format) && ! "exportActiveNonAdminUsersCSV".equals(format)){
 			resp.getOutputStream().println(export.export);
+		}
+	}
+	
+	private String generateCSVOfActiveUserEmails() throws InCorrectUserRoleException{
+		logger.info("Generating CSV of email addrs for all Active Users...");
+		
+		//Get all the active users
+		List<User> allNonAdminActiveUsersList = getUserService().getAllNonAdminActiveUsers();
+		
+		//Iterate through the 'allNonAdminActiveUsersList' and generate a csv
+		return createCSVOfAllNonAdminActiveUsers(allNonAdminActiveUsersList);
 	}
 
+	private String createCSVOfAllNonAdminActiveUsers(
+			List<User> allNonAdminActiveUsersList) {
+		
+		StringBuilder csvStringBuilder = new StringBuilder();
+		//Add the header row
+		csvStringBuilder.append("Last Name,First Name,Email Address,Organization,Organization Id");
+		csvStringBuilder.append("\r\n");
+		//Add data rows
+		for(User user:allNonAdminActiveUsersList){
+			csvStringBuilder.append("\""+user.getLastName()+"\",\""+user.getFirstName()+
+					"\",\""+user.getEmailAddress()+"\",\""+user.getOrganizationName()+"\",\""+user.getOrgOID()+"\"");
+			csvStringBuilder.append("\r\n");
+		}
+		return csvStringBuilder.toString();
+	}
+	
 	private SimpleEMeasureService getService(){
 		SimpleEMeasureService service = (SimpleEMeasureService) context.getBean("eMeasureService");
 		return service;
+	}
+	private UserService getUserService() {
+		return (UserService)context.getBean("userService");
 	}
 }

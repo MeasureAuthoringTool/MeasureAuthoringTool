7a99df8659da69debee01f0c8d18197ee6b49031
Jyoti Narang <JNarang@hcareis.com>
2013-09-09 14:17:20 +0000

    Added logger messages.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3216 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 02e51b28f876d09545cf7ff79dbe65a1948d35d1

--- src/mat/server/VSACAPIServiceImpl.java
+++ src/mat/server/VSACAPIServiceImpl.java
@@ -2,6 +2,8 @@ package mat.server;
 
 import mat.client.umls.service.VSACAPIService;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.telligen.vsac.service.VSACTicketService;
 
 
@@ -13,10 +15,13 @@ public class VSACAPIServiceImpl extends SpringRemoteServiceServlet implements VS
 	 * 
 	 */
 	private static final long serialVersionUID = -6645961609626183169L;
+	private static final Log logger = LogFactory.getLog(VSACAPIServiceImpl.class);
 
 	@Override
 	public String validateVsacUser(String userName, String password) {
+		logger.info("Start validateVsacUser =====");
 		String eightHourTicketForUser = new VSACTicketService().getTicketGrantingTicket(userName,password);
+		logger.info("End validateVsacUser ===== eightHourTicketForUser =====" + eightHourTicketForUser);
 		return eightHourTicketForUser;
 	}
 

c0f2a2a8aab27a3f6c0b2e6365c6887d721b9e0e
Jyoti Narang <JNarang@hcareis.com>
2013-09-24 18:33:16 +0000

    VsacValueSetWrapper class added.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3402 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 653239e55280ab61bbe2ab0c6e68856746b6ccaa

---
+++ src/mat/model/VSACValueSetWrapper.java
@@ -0,0 +1,18 @@
+package mat.model;
+
+import java.util.ArrayList;
+
+
+public class VSACValueSetWrapper {
+	
+	private ArrayList<MatValueSet> valueSetList;
+
+	public ArrayList<MatValueSet> getValueSetList() {
+		return valueSetList;
+	}
+
+	public void setValueSetList(ArrayList<MatValueSet> valueSetList) {
+		this.valueSetList = valueSetList;
+	}
+	
+}

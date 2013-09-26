a875bcbeea8d90b4e3ae272e25fa1dee179d266d
Jyoti Narang <JNarang@hcareis.com>
2013-09-26 13:56:39 +0000

    CheckStyles rules implemented.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3424 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: a1afef56739e3085b5a1b43158102521fee09a90

--- src/mat/model/MatConceptList.java
+++ src/mat/model/MatConceptList.java
@@ -4,14 +4,33 @@ import java.util.List;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+/**
+ * Container for holding Code System information.
+ *
+ * **/
 public class MatConceptList implements IsSerializable {
-	private List<MatConcept> conceptList;
+	/**
+	 * Container for holding Code System information.
+	 *
+	 * **/
+private List<MatConcept> conceptList;
 
-	public List<MatConcept> getConceptList() {
-		return conceptList;
-	}
-	public void setConceptList(List<MatConcept> conceptList) {
-		this.conceptList = conceptList;
-	}
+/**
+ * Getter - conceptList.
+ * @return - conceptList.
+ *
+ * **/
+public final List<MatConcept> getConceptList() {
+return conceptList;
+}
+
+/**
+ * Setter - conceptList.
+ * @param conceptLists -List of MatConcept.
+ *
+ * **/
+public final void setConceptList(final List<MatConcept> conceptLists) {
+this.conceptList = conceptLists;
+}
 
 }

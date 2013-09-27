6d7e70f2d550f241c01ec84f341e74d965332c07
Chinmay Bajikar <CBajikar@telligen.com>
2013-09-27 20:52:11 +0000

    Code Refactor,
    1) For VSACAPIService changed the method name from "getValueSetBasedOIDAndVersion" to "getValueSetByOIDAndVersion".
    
    2) Broke up "VSACAPIServiceImpl.getValueSetByOIDAndVersion" and extracted "handleVSACGroupedValueSet".
    
    3) In UMLSSessionTicket class, encapsulate the HashMap 'umlsSessionMap' and add methods like getKey, put and remove for HashMap operations
    
    
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3466 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 0cd847553cef1339eb92f8d6699f09f25b3f4247

--- src/mat/server/util/UMLSSessionTicket.java
+++ src/mat/server/util/UMLSSessionTicket.java
@@ -3,15 +3,18 @@ package mat.server.util;
 import java.util.HashMap;
 
 public class UMLSSessionTicket {
-	
+
 	private static final HashMap<String, String> umlsSessionMap = new HashMap<String, String>();
 
-	public static HashMap<String, String> getUmlssessionmap() {
-		return umlsSessionMap;
-	}
-
-	public static void put(String key, String value){
+	public static void put(String key, String value) {
 		umlsSessionMap.put(key, value);
 	}
 
+	public static void remove(String key) {
+		umlsSessionMap.remove(key);
+	}
+
+	public static String getTicket(String key) {
+		return umlsSessionMap.get(key);
+	}
 }

d9d418a174b2eb46c3fbdd5d9b40df15effd01ac
Jyoti Narang <JNarang@hcareis.com>
2013-10-02 17:12:11 +0000

    Removed unused appliedQDMList variable from VsacApiResult.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3505 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 78b3705b3d15508aa51926fea3c72e2044d7d06e

--- src/mat/client/umls/service/VsacApiResult.java
+++ src/mat/client/umls/service/VsacApiResult.java
@@ -3,7 +3,6 @@ package mat.client.umls.service;
 import java.util.ArrayList;
 
 import mat.model.MatValueSet;
-import mat.model.QualityDataSetDTO;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
@@ -11,12 +10,11 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 public class VsacApiResult implements IsSerializable {
 	public static final int UMLS_NOT_LOGGEDIN = 1;
 	public static final int OID_REQUIRED = 2;
-	
+
 	private int failureReason;
 	private boolean isSuccess;
-	
 	private ArrayList<MatValueSet> vsacResponse;
-	private ArrayList<QualityDataSetDTO> appliedQDMList;
+
 	public boolean isSuccess() {
 		return isSuccess;
 	}
@@ -35,12 +33,5 @@ public class VsacApiResult implements IsSerializable {
 	public void setVsacResponse(ArrayList<MatValueSet> vsacResponse) {
 		this.vsacResponse = vsacResponse;
 	}
-	public ArrayList<QualityDataSetDTO> getAppliedQDMList() {
-		return appliedQDMList;
-	}
-	public void setAppliedQDMList(ArrayList<QualityDataSetDTO> appliedQDMList) {
-		this.appliedQDMList = appliedQDMList;
-	}
-	
 
 }

b990a91f005d395ecd33d189c05642b6ac86e347
Jyoti Narang <JNarang@hcareis.com>
2013-10-25 17:34:04 +0000

    Added Java Doc's
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3852 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: cb4a5b3f866bd19cb3c5dc2cb61de93d098d107a

--- src/mat/client/clause/clauseworkspace/model/CellTreeNodeImpl.java
+++ src/mat/client/clause/clauseworkspace/model/CellTreeNodeImpl.java
@@ -8,120 +8,119 @@ import java.util.Map;
 import mat.client.clause.clauseworkspace.presenter.ClauseConstants;
 import mat.shared.UUIDUtilClient;
 
+/**
+ * CellTreeNodeImpl.java.
+ */
 public class CellTreeNodeImpl implements CellTreeNode {
-
-	private String name;
-
+	/**
+	 * {@link List} of child {@link CellTreeNode}.
+	 */
 	private List<CellTreeNode> childs;
-
-	private CellTreeNode parent;
-
-	private String label;
-
+	/**
+	 * {@link HashMap} of Node's extra information.
+	 */
+	private Map<String, Object> extraInformationMap = new HashMap<String, Object>();
+	/**
+	 * Node open/close status.
+	 */
 	private boolean isOpen;
-
-	private short nodeType;
-
-	Map<String, Object> extraInformationMap = new HashMap<String, Object>();
-
-	private String uuid;
-
+	/**
+	 * Valid node.
+	 */
 	private boolean isValid = true;
-
+	/**
+	 * Node Label.
+	 */
+	private String label;
+	/**
+	 * Node Name.
+	 */
+	private String name;
+	/**
+	 * Node type.
+	 */
+	private short nodeType;
+	/**
+	 * Node Parent.
+	 */
+	private CellTreeNode parent;
+	/**
+	 * Node UUID information.
+	 */
+	private String uuid;
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#appendChild(mat.
+	 * client.clause.clauseworkspace.model.CellTreeNode)
+	 */
 	@Override
-	public List<CellTreeNode> getChilds() {
-		return childs;
-	}
-
-	@Override
-	public void setChilds(List<CellTreeNode> childs) {
-		this.childs = childs;
-	}
-
-	@Override
-	public CellTreeNode appendChild(CellTreeNode child) {
+	public final CellTreeNode appendChild(CellTreeNode child) {
 		child.setParent(this);
 		if (this.getChilds() != null) {
 			this.getChilds().add(child);
 		} else {
-			List<CellTreeNode> childs = new ArrayList<CellTreeNode>();
-			childs.add(child);
-			this.setChilds(childs);
+			List<CellTreeNode> childNodes = new ArrayList<CellTreeNode>();
+			childNodes.add(child);
+			this.setChilds(childNodes);
 		}
 		isOpen = true;
 		return child;
 	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#cloneNode()
+	 */
 	@Override
-	public CellTreeNode removeChild(CellTreeNode child) {
-		if (this.getChilds() != null && this.getChilds().size() > 0) {
-			this.getChilds().remove(child);
-		}
-		return child;
-	}
-
-	@Override
-	public CellTreeNode createChild(String name, String label, short nodeType) {
-		CellTreeNode cellTreeNode = new CellTreeNodeImpl();
-		cellTreeNode.setName(name);
-		cellTreeNode.setLabel(label);
-		cellTreeNode.setNodeType(nodeType);
-		cellTreeNode.setOpen(true);
-		if (nodeType == CLAUSE_NODE) {
-			cellTreeNode.setUUID(UUIDUtilClient.uuid());
-		}
-
-		this.setOpen(true); // open parent
-		return appendChild(cellTreeNode);
-	}
-
-	@Override
-	public short getNodeType() {
-		return this.nodeType;
-	}
-
-	@Override
-	public void setNodeType(short nodeType) {
-		this.nodeType = nodeType;
-	}
-
-	@Override
-	public CellTreeNode getParent() {
-		return this.parent;
-	}
-
-	@Override
-	public void setParent(CellTreeNode parent) {
-		this.parent = parent;
-	}
-
-	@Override
-	public boolean hasChildren() {
-		return childs != null && childs.size() > 0;
-	}
-
-	@Override
-	public CellTreeNode cloneNode() {
+	public final CellTreeNode cloneNode() {
 		CellTreeNode copyModel = createCopyOfTreeModel(this);
 		if (this.getChilds() != null) {
 			createCopyChilds(this.getChilds(), copyModel);
 		}
 		return copyModel;
 	}
-
-	private void createCopyChilds(List<CellTreeNode> childs, CellTreeNode parent) {
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#createChild(java
+	 * .lang.String, java.lang.String, short)
+	 */
+	@Override
+	public final CellTreeNode createChild(String nodeName, String nodeLabel, short nodesType) {
+		CellTreeNode cellTreeNode = new CellTreeNodeImpl();
+		cellTreeNode.setName(nodeName);
+		cellTreeNode.setLabel(nodeLabel);
+		cellTreeNode.setNodeType(nodesType);
+		cellTreeNode.setOpen(true);
+		if (nodeType == CLAUSE_NODE) {
+			cellTreeNode.setUUID(UUIDUtilClient.uuid());
+		}
+		this.setOpen(true); // open parent
+		return appendChild(cellTreeNode);
+	}
+	/**
+	 * @param childNodes
+	 *            - {@link List} of {@link CellTreeNode}.
+	 * @param parentNode
+	 *            - {@link CellTreeNode}.
+	 */
+	private void createCopyChilds(List<CellTreeNode> childNodes, CellTreeNode parentNode) {
 		List<CellTreeNode> newChilds = new ArrayList<CellTreeNode>();
-		for (CellTreeNode treeNode : childs) {
+		for (CellTreeNode treeNode : childNodes) {
 			CellTreeNode child = createCopyOfTreeModel(treeNode);
-			child.setParent(parent);
+			child.setParent(parentNode);
 			newChilds.add(child);
-			parent.setChilds(newChilds);
-			if (treeNode.getChilds() != null && treeNode.getChilds().size() > 0) {
+			parentNode.setChilds(newChilds);
+			if ((treeNode.getChilds() != null) && (treeNode.getChilds().size() > 0)) {
 				createCopyChilds(treeNode.getChilds(), child);
 			}
 		}
 	}
-
+	/**
+	 * @param model
+	 *            - {@link CellTreeNode}.
+	 * @return {@link CellTreeNode}.
+	 */
 	private CellTreeNode createCopyOfTreeModel(CellTreeNode model) {
 		CellTreeNode copyModel = new CellTreeNodeImpl();
 		copyModel.setLabel(model.getLabel());
@@ -134,8 +133,9 @@ public class CellTreeNodeImpl implements CellTreeNode {
 			// "Copy/Paste in Clause Workspace Dropping Attributes".
 			// List<CellTreeNode> attributes = (List<CellTreeNode>)
 			// extraInformationMap.get("attributes");
+			@SuppressWarnings("unchecked")
 			List<CellTreeNode> attributes = (List<CellTreeNode>) model
-					.getExtraInformation("attributes");
+			.getExtraInformation("attributes");
 			// MAT-2282 : Bug fix ends.
 			if (attributes != null) {
 				List<CellTreeNode> extraAttrList = new ArrayList<CellTreeNode>();
@@ -148,7 +148,7 @@ public class CellTreeNodeImpl implements CellTreeNode {
 					extraInfoAttr.putAll(((CellTreeNodeImpl) cellTreeNode)
 							.getExtraInformationMap());
 					((CellTreeNodeImpl) attrNode)
-							.setExtraInformationMap(extraInfoAttr);
+					.setExtraInformationMap(extraInfoAttr);
 					extraAttrList.add(attrNode);
 				}
 				copyModel.setExtraInformation("attributes", extraAttrList);
@@ -158,7 +158,6 @@ public class CellTreeNodeImpl implements CellTreeNode {
 					.getExtraInformationMap());
 			((CellTreeNodeImpl) copyModel).setExtraInformationMap(extraInfos);
 		}
-
 		if (model.getNodeType() == CLAUSE_NODE) {
 			copyModel.setUUID(UUIDUtilClient.uuid());
 		} else {
@@ -166,72 +165,70 @@ public class CellTreeNodeImpl implements CellTreeNode {
 		}
 		return copyModel;
 	}
-
+	/* (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getChilds()
+	 */
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getChilds()
+	 */
 	@Override
-	public boolean isOpen() {
-		return isOpen;
+	public List<CellTreeNode> getChilds() {
+		return childs;
 	}
-
-	@Override
-	public void setOpen(boolean isOpen) {
-		this.isOpen = isOpen;
-	}
-
-	@Override
-	public String getName() {
-		return this.name;
-	}
-
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String getLabel() {
-		return this.label;
-	}
-
-	@Override
-	public void setLabel(String label) {
-		this.label = label;
-	}
-
-	@Override
-	public void setExtraInformation(String key, Object value) {
-		this.extraInformationMap.put(key, value);
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#getExtraInformation
+	 * (java.lang.String)
+	 */
 	@Override
 	public Object getExtraInformation(String key) {
-		return this.extraInformationMap.get(key);
+		return extraInformationMap.get(key);
 	}
-
 	/**
 	 * @return the extraInformationMap
 	 */
 	public Map<String, Object> getExtraInformationMap() {
 		return extraInformationMap;
 	}
-
-	/**
-	 * @param extraInformationMap
-	 *            the extraInformationMap to set
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getLabel()
 	 */
-	public void setExtraInformationMap(Map<String, Object> extraInformationMap) {
-		this.extraInformationMap = extraInformationMap;
-	}
-
 	@Override
-	public String getUUID() {
-		return this.uuid;
+	public String getLabel() {
+		return label;
 	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getName()
+	 */
 	@Override
-	public void setUUID(String uuid) {
-		this.uuid = uuid;
+	public String getName() {
+		return name;
 	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getNodeType()
+	 */
+	@Override
+	public short getNodeType() {
+		return nodeType;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getParent()
+	 */
+	@Override
+	public CellTreeNode getParent() {
+		return parent;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#getQdmAttribute()
+	 */
 	@Override
 	public String getQdmAttribute() {
 		String attrib = "";
@@ -264,67 +261,22 @@ public class CellTreeNodeImpl implements CellTreeNode {
 				String comparisonValue = (String) attributeNode
 						.getExtraInformation("comparisonValue");
 				stringBuilder.append(comparisonValue);
-
 				String unit = (String) attributeNode
 						.getExtraInformation("unit");
 				if (null != unit) {
 					stringBuilder.append(" ").append(unit);
 				}
-
 			}
 			attrib = stringBuilder.append(")").toString();
 		}
-
 		return attrib;
 	}
-
-	// TODO : this is called every time when we make a change to the Celltree,
-	// instead we can have a title variable in the CellTreeNodeImpl and set it
-	// for each functionality.
-	@Override
-	public String getTitle() {
-		String title = getName();
-		String label = getName();
-		if (label.length() > ClauseConstants.LABEL_MAX_LENGTH) {
-			label = label.substring(0, ClauseConstants.LABEL_MAX_LENGTH - 1)
-					.concat("...");
-		}
-		if (getNodeType() == CellTreeNode.ELEMENT_REF_NODE) { // checking if QDM
-																// node
-			String oid = "";
-			if (ClauseConstants.getElementLookUpNode().get(
-					getName() + "~" + getUUID()) != null) {
-				oid = ClauseConstants.getElementLookUpNode()
-						.get(getName() + "~" + getUUID()).getAttributes()
-						.getNamedItem("oid").getNodeValue(); // getting the OID
-															// for the QDM
-			}
-			int attrCount = getQdmAttributeCount();
-			if (attrCount > 1) { // if count greater than 1 just append the
-									// count to the label
-				title = name + " (" + oid + ")";
-				label = label + " (" + attrCount + ")";
-			} else if (attrCount == 1) { // if count equals one check if the
-											// length of name plus the length
-											// attribute name is greater than
-											// the max length allowed,
-											// if yes add the count as number to
-											// name and set the title as
-											// attribute name else directly add
-											// the attribute name to name
-				String qdmAttr = getQdmAttribute();
-				if ((qdmAttr.length() + name.length()) > ClauseConstants.LABEL_MAX_LENGTH) {
-					label = label + " (" + attrCount + ")";
-				} else {
-					label = getName() + qdmAttr;
-				}
-				title = name + " (" + oid + ")" + qdmAttr;
-			}
-		}
-		setLabel(label);
-		return title;
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#getQdmAttributeCount
+	 * ()
+	 */
 	@Override
 	public int getQdmAttributeCount() {
 		if (getNodeType() == ELEMENT_REF_NODE) {
@@ -335,15 +287,194 @@ public class CellTreeNodeImpl implements CellTreeNode {
 		}
 		return 0;
 	}
-
+	// TODO : this is called every time when we make a change to the Celltree,
+	// instead we can have a title variable in the CellTreeNodeImpl and set it
+	// for each functionality.
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getTitle()
+	 */
 	@Override
-	public void setValidNode(boolean isValid) {
-		this.isValid = isValid;
+	public String getTitle() {
+		String title = getName();
+		String nodeLabel = getName();
+		if (nodeLabel.length() > ClauseConstants.LABEL_MAX_LENGTH) {
+			nodeLabel = nodeLabel.substring(0, ClauseConstants.LABEL_MAX_LENGTH - 1)
+					.concat("...");
+		}
+		if (getNodeType() == CellTreeNode.ELEMENT_REF_NODE) { // checking if QDM
+			// node
+			String oid = "";
+			if (ClauseConstants.getElementLookUpNode().get(
+					getName() + "~" + getUUID()) != null) {
+				oid = ClauseConstants.getElementLookUpNode()
+						.get(getName() + "~" + getUUID()).getAttributes()
+						.getNamedItem("oid").getNodeValue(); // getting the OID
+				// for the QDM
+			}
+			int attrCount = getQdmAttributeCount();
+			if (attrCount > 1) { // if count greater than 1 just append the
+				// count to the label
+				title = name + " (" + oid + ")";
+				nodeLabel = nodeLabel + " (" + attrCount + ")";
+			} else if (attrCount == 1) { // if count equals one check if the
+				// length of name plus the length
+				// attribute name is greater than
+				// the max length allowed,
+				// if yes add the count as number to
+				// name and set the title as
+				// attribute name else directly add
+				// the attribute name to name
+				String qdmAttr = getQdmAttribute();
+				if ((qdmAttr.length() + name.length()) > ClauseConstants.LABEL_MAX_LENGTH) {
+					nodeLabel = nodeLabel + " (" + attrCount + ")";
+				} else {
+					nodeLabel = getName() + qdmAttr;
+				}
+				title = name + " (" + oid + ")" + qdmAttr;
+			}
+		}
+		setLabel(nodeLabel);
+		return title;
 	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getUUID()
+	 */
+	@Override
+	public String getUUID() {
+		return uuid;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#getValidNode()
+	 */
 	@Override
 	public boolean getValidNode() {
 		return isValid;
 	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#hasChildren()
+	 */
+	@Override
+	public boolean hasChildren() {
+		return (childs != null) && (childs.size() > 0);
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.clause.clauseworkspace.model.CellTreeNode#isOpen()
+	 */
+	@Override
+	public boolean isOpen() {
+		return isOpen;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#removeChild(mat.
+	 * client.clause.clauseworkspace.model.CellTreeNode)
+	 */
+	@Override
+	public CellTreeNode removeChild(CellTreeNode child) {
+		if ((this.getChilds() != null) && (this.getChilds().size() > 0)) {
+			this.getChilds().remove(child);
+		}
+		return child;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setChilds(java.util
+	 * .List)
+	 */
+	@Override
+	public void setChilds(List<CellTreeNode> childNodes) {
+		childs = childNodes;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setExtraInformation
+	 * (java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public void setExtraInformation(String key, Object value) {
+		extraInformationMap.put(key, value);
+	}
+	/**
+	 * @param extraInformationMap
+	 *            the extraInformationMap to set
+	 */
+	public void setExtraInformationMap(Map<String, Object> extraInformationMap) {
+		this.extraInformationMap = extraInformationMap;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setLabel(java.lang
+	 * .String)
+	 */
+	@Override
+	public void setLabel(String nodeLabel) {
+		label = nodeLabel;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setName(java.lang
+	 * .String)
+	 */
+	@Override
+	public void setName(String nodeName) {
+		name = nodeName;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setNodeType(short)
+	 */
+	@Override
+	public void setNodeType(short type) {
+		nodeType = type;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setOpen(boolean)
+	 */
+	@Override
+	public void setOpen(boolean isNodeOpen) {
+		isOpen = isNodeOpen;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setParent(mat.client
+	 * .clause.clauseworkspace.model.CellTreeNode)
+	 */
+	@Override
+	public void setParent(CellTreeNode parentNode) {
+		parent = parentNode;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setUUID(java.lang
+	 * .String)
+	 */
+	@Override
+	public void setUUID(String nodeUUID) {
+		uuid = nodeUUID;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * mat.client.clause.clauseworkspace.model.CellTreeNode#setValidNode(boolean
+	 * )
+	 */
+	@Override
+	public void setValidNode(boolean isNodeValid) {
+		isValid = isNodeValid;
+	}
 }

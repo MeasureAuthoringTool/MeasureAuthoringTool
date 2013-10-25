ff254e0d62142225a12305f85269881058cd412d
Jyoti Narang <JNarang@hcareis.com>
2013-10-25 19:21:02 +0000

    Added Java Doc's
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3886 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 5d2a527505eb51db7057f531614efb853834fe3e

--- src/mat/client/umls/service/VSACAPIService.java
+++ src/mat/client/umls/service/VSACAPIService.java
@@ -7,22 +7,63 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 /**
+ * The Interface VSACAPIService.
+ * 
  * @author jnarang
- *
  */
 @RemoteServiceRelativePath("vsacapi")
 public interface VSACAPIService extends RemoteService {
 
+	/**
+	 * Validate vsac user.
+	 * 
+	 * @param userName
+	 *            the user name
+	 * @param password
+	 *            the password
+	 * @return true, if successful
+	 */
 	boolean validateVsacUser(String userName, String password);
 
+	/**
+	 * Gets the value set by oid and version.
+	 * 
+	 * @param OID
+	 *            the oid
+	 * @param version
+	 *            the version
+	 * @return the value set by oid and version
+	 */
 	VsacApiResult getValueSetByOIDAndVersion(String OID, String version);
 
+	/**
+	 * In validate vsac user.
+	 */
 	void inValidateVsacUser();
 
+	/**
+	 * Checks if is already signed in.
+	 * 
+	 * @return true, if is already signed in
+	 */
 	boolean isAlreadySignedIn();
 
+	/**
+	 * Update vsac value sets.
+	 * 
+	 * @param measureId
+	 *            the measure id
+	 * @return the vsac api result
+	 */
 	VsacApiResult updateVSACValueSets(String measureId);
 
+	/**
+	 * Update all vsac value sets at package.
+	 * 
+	 * @param measureId
+	 *            the measure id
+	 * @return the vsac api result
+	 */
 	VsacApiResult updateAllVSACValueSetsAtPackage(String measureId);
 
 }
--- src/mat/client/umls/service/VsacApiResult.java
+++ src/mat/client/umls/service/VsacApiResult.java
@@ -7,29 +7,79 @@ import mat.model.MatValueSet;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 
+/**
+ * The Class VsacApiResult.
+ */
 public class VsacApiResult implements IsSerializable {
+	
+	/** The Constant UMLS_NOT_LOGGEDIN. */
 	public  final static int UMLS_NOT_LOGGEDIN = 1;
+	
+	/** The Constant OID_REQUIRED. */
 	public  final static int OID_REQUIRED = 2;
 
+	/** The failure reason. */
 	private int failureReason;
+	
+	/** The is success. */
 	private boolean isSuccess;
+	
+	/** The vsac response. */
 	private ArrayList<MatValueSet> vsacResponse;
 
+	/**
+	 * Checks if is success.
+	 * 
+	 * @return true, if is success
+	 */
 	public boolean isSuccess() {
 		return isSuccess;
 	}
+	
+	/**
+	 * Sets the success.
+	 * 
+	 * @param isSuccess
+	 *            the new success
+	 */
 	public void setSuccess(boolean isSuccess) {
 		this.isSuccess = isSuccess;
 	}
+	
+	/**
+	 * Gets the failure reason.
+	 * 
+	 * @return the failure reason
+	 */
 	public int getFailureReason() {
 		return failureReason;
 	}
+	
+	/**
+	 * Sets the failure reason.
+	 * 
+	 * @param failureReason
+	 *            the new failure reason
+	 */
 	public void setFailureReason(int failureReason) {
 		this.failureReason = failureReason;
 	}
+	
+	/**
+	 * Gets the vsac response.
+	 * 
+	 * @return the vsac response
+	 */
 	public ArrayList<MatValueSet> getVsacResponse() {
 		return vsacResponse;
 	}
+	
+	/**
+	 * Sets the vsac response.
+	 * 
+	 * @param vsacResponse
+	 *            the new vsac response
+	 */
 	public void setVsacResponse(ArrayList<MatValueSet> vsacResponse) {
 		this.vsacResponse = vsacResponse;
 	}

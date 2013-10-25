c698ebbf5d1113da78f51f974b27fad5f1e35f60
Jyoti Narang <JNarang@hcareis.com>
2013-10-25 17:46:18 +0000

    Added Java Doc's
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3855 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: ce9c24decd03234955eb6c6cae455b2177f225b6

--- src/mat/client/clause/clauseworkspace/presenter/PopulationClausePresenter.java
+++ src/mat/client/clause/clauseworkspace/presenter/PopulationClausePresenter.java
@@ -7,27 +7,47 @@ import mat.client.shared.MatContext;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
+/**
+ * PopulationClausePresenter extends {@link XmlTreePresenter} and implements
+ * {@link MatPresenter}.
+ */
 public class PopulationClausePresenter extends XmlTreePresenter implements MatPresenter {
-
-	SimplePanel panel = new SimplePanel();
-	MeasureServiceAsync service = MatContext.get().getMeasureService();
+	/**
+	 * Root Node for Population Tab.
+	 */
 	private static final String POPULATIONS_TAG = "populations";
-
+	/**
+	 * {@link SimplePanel} Instance.
+	 */
+	private SimplePanel panel = new SimplePanel();
+	/**
+	 * MeasureService Instance.
+	 */
+	private MeasureServiceAsync service = MatContext.get().getMeasureService();
+	/**
+	 * Default Constructor.
+	 */
 	public PopulationClausePresenter() {
 		setRootNode(POPULATIONS_TAG);
 	}
-
+	/* (non-Javadoc)
+	 * @see mat.client.MatPresenter#beforeClosingDisplay()
+	 */
+	@Override
+	public void beforeClosingDisplay() {
+	}
+	/* (non-Javadoc)
+	 * @see mat.client.MatPresenter#beforeDisplay()
+	 */
 	@Override
 	public void beforeDisplay() {
 		panel.clear();
 		loadXmlTree(panel);
 	}
-
-	@Override
-	public void beforeClosingDisplay() {
-
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.MatPresenter#getWidget()
+	 */
 	@Override
 	public Widget getWidget() {
 		return panel;

23ca7f59000df1b2f3f0820cfccd5741f211486a
Jyoti Narang <JNarang@hcareis.com>
2013-10-25 17:41:56 +0000

    Added Java Doc's
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3854 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 573fe7abb3a5c2de8b27dd472303d4ba95a9aada

--- src/mat/client/clause/clauseworkspace/presenter/MeasureObsClausePresenter.java
+++ src/mat/client/clause/clauseworkspace/presenter/MeasureObsClausePresenter.java
@@ -7,27 +7,48 @@ import mat.client.shared.MatContext;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
+/**
+ * MeasureObsClausePresenter extends {@link XmlTreePresenter} and implements
+ * {@link MatPresenter}.
+ */
 public class MeasureObsClausePresenter extends XmlTreePresenter implements MatPresenter {
-
-	SimplePanel panel = new SimplePanel();
-	MeasureServiceAsync service = MatContext.get().getMeasureService();
+	/**
+	 * Root Name for Presenter.
+	 */
 	private static final String MEASURE_OBS = "measureObservations";
-
+	/**
+	 * Panel Instance.
+	 */
+	private SimplePanel panel = new SimplePanel();
+	/**
+	 * MeasureService Instance.
+	 */
+	private MeasureServiceAsync service = MatContext.get().getMeasureService();
+	/**
+	 * Default Constructor.
+	 */
 	public MeasureObsClausePresenter() {
 		setRootNode(MEASURE_OBS);
 	}
-
+	/* (non-Javadoc)
+	 * @see mat.client.MatPresenter#beforeClosingDisplay()
+	 */
+	@Override
+	public void beforeClosingDisplay() {
+		// TODO Auto-generated method stub
+	}
+	/* (non-Javadoc)
+	 * @see mat.client.MatPresenter#beforeDisplay()
+	 */
 	@Override
 	public void beforeDisplay() {
 		panel.clear();
 		loadXmlTree(panel);
 	}
-
-	@Override
-	public void beforeClosingDisplay() {
-		// TODO Auto-generated method stub
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see mat.client.MatPresenter#getWidget()
+	 */
 	@Override
 	public Widget getWidget() {
 		return panel;

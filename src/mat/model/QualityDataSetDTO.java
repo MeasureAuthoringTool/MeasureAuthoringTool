cd642188413fa1c40158b488d600781b956c9a22
Jyoti Narang <JNarang@hcareis.com>
2013-10-29 17:44:31 +0000

    Added effective Date as attribute for QualityDataSetDTO.java.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@3940 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: faea44f4ce90e15a1d7f649f65441f75026fe7c3

--- src/mat/model/QualityDataSetDTO.java
+++ src/mat/model/QualityDataSetDTO.java
@@ -9,50 +9,12 @@ import com.google.gwt.user.client.rpc.IsSerializable;
  */
 public class QualityDataSetDTO implements IsSerializable {
 	
-
-	/** The id. */
-	private String id;
 	
-	/** The data type. */
-	private String dataType;
-	
-	/** The code list name. */
-	private String codeListName;
-	
-	/** The occurrence text. */
-	private String occurrenceText;
-	
-	/** The supp data element. */
-	private boolean suppDataElement;
-	
-	/** The oid. */
-	private String oid;
-	
-	/** The type. */
-	private String type;
-	
-	/** The taxonomy. */
-	private String taxonomy; 
-	
-	/** The code system name. */
-	private String codeSystemName;
-	
-	/** The version. */
-	private String version;
-	
-	/** The uuid. */
-	private String uuid;
-	
-	/** The is used. */
-	private boolean isUsed;
-	
-	
-		
 	/**
 	 * The Class Comparator.
 	 */
 	public static class Comparator implements java.util.Comparator<QualityDataSetDTO>, IsSerializable {
-
+		
 		/* (non-Javadoc)
 		 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
 		 */
@@ -63,45 +25,59 @@ public class QualityDataSetDTO implements IsSerializable {
 		}
 		
 	}
-
-	/**
-	 * Gets the id.
-	 * 
-	 * @return the id
+	
+	/** The code list name. */
+	private String codeListName;
+	
+	/** The code system name. */
+	private String codeSystemName;
+	
+	/** The data type. */
+	private String dataType;
+	
+	private String effectiveDate;
+	
+	/** The id. */
+	private String id;
+	
+	/** The is used. */
+	private boolean isUsed;
+	
+	/** The occurrence text. */
+	private String occurrenceText;
+	
+	/** The oid. */
+	private String oid;
+	
+	/** The supp data element. */
+	private boolean suppDataElement;
+	
+	/** The taxonomy. */
+	private String taxonomy;
+	
+	/** The type. */
+	private String type;
+	
+	/** The uuid. */
+	private String uuid;
+	
+	/** The version. */
+	private String version;
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
-	public String getId() {
-		return id;
+	@Override
+	public boolean equals (Object o) {
+		QualityDataSetDTO temp = (QualityDataSetDTO)o;
+		if (temp.getId().equals(getId())) {
+			return true;
+		}
+		return false;
 	}
-
-	/**
-	 * Sets the id.
-	 * 
-	 * @param id
-	 *            the new id
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	/**
-	 * Gets the data type.
-	 * 
-	 * @return the data type
-	 */
-	public String getDataType() {
-		return dataType;
-	}
-
-	/**
-	 * Sets the data type.
-	 * 
-	 * @param dataType
-	 *            the new data type
-	 */
-	public void setDataType(String dataType) {
-		this.dataType = dataType;
-	}
-
+	
+	
+	
 	/**
 	 * Gets the code list name.
 	 * 
@@ -110,25 +86,57 @@ public class QualityDataSetDTO implements IsSerializable {
 	public String getCodeListName() {
 		return codeListName;
 	}
-
+	
 	/**
-	 * Sets the code list name.
+	 * Gets the code system name.
 	 * 
-	 * @param codeListName
-	 *            the new code list name
+	 * @return the code system name
 	 */
-	public void setCodeListName(String codeListName) {
-		this.codeListName = codeListName;
+	public String getCodeSystemName() {
+		return codeSystemName;
 	}
 	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
+	/**
+	 * Gets the data type.
+	 * 
+	 * @return the data type
 	 */
-	public String toString() {
-		if(occurrenceText!= null && !occurrenceText.equals(""))
-			return occurrenceText + " of "+codeListName + ": " + dataType + "-" +getOid();
-		else
-			return codeListName + ": " + dataType + "-" + getOid();
+	public String getDataType() {
+		return dataType;
+	}
+	
+	/**
+	 * @return the effectiveDate
+	 */
+	public String getEffectiveDate() {
+		return effectiveDate;
+	}
+	
+	/**
+	 * Gets the id.
+	 * 
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	
+	/**
+	 * Gets the occurrence text.
+	 * 
+	 * @return the occurrence text
+	 */
+	public String getOccurrenceText() {
+		return occurrenceText;
+	}
+	
+	/**
+	 * Gets the oid.
+	 * 
+	 * @return the oid
+	 */
+	public String getOid() {
+		return oid;
 	}
 	
 	/**
@@ -140,92 +148,6 @@ public class QualityDataSetDTO implements IsSerializable {
 		return codeListName + ": " + dataType;
 	}
 	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals (Object o) {
-		QualityDataSetDTO temp = (QualityDataSetDTO)o;
-		if (temp.getId().equals(getId())) return true;
-		return false;
-	}
-
-	/**
-	 * Gets the occurrence text.
-	 * 
-	 * @return the occurrence text
-	 */
-	public String getOccurrenceText() {
-		return occurrenceText;
-	}
-
-	/**
-	 * Sets the occurrence text.
-	 * 
-	 * @param occurrenceText
-	 *            the new occurrence text
-	 */
-	public void setOccurrenceText(String occurrenceText) {
-		this.occurrenceText = occurrenceText;
-	}
-
-	/**
-	 * Checks if is supp data element.
-	 * 
-	 * @return true, if is supp data element
-	 */
-	public boolean isSuppDataElement() {
-		return suppDataElement;
-	}
-
-	/**
-	 * Sets the supp data element.
-	 * 
-	 * @param suppDataElement
-	 *            the new supp data element
-	 */
-	public void setSuppDataElement(boolean suppDataElement) {
-		this.suppDataElement = suppDataElement;
-	}
-
-	/**
-	 * Gets the oid.
-	 * 
-	 * @return the oid
-	 */
-	public String getOid() {
-		return oid;
-	}
-
-	/**
-	 * Sets the oid.
-	 * 
-	 * @param oid
-	 *            the new oid
-	 */
-	public void setOid(String oid) {
-		this.oid = oid;
-	}
-
-	/**
-	 * Sets the type.
-	 * 
-	 * @param type
-	 *            the type to set
-	 */
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	/**
-	 * Gets the type.
-	 * 
-	 * @return the type
-	 */
-	public String getType() {
-		return type;
-	}
-
 	/**
 	 * Gets the taxonomy.
 	 * 
@@ -234,26 +156,62 @@ public class QualityDataSetDTO implements IsSerializable {
 	public String getTaxonomy() {
 		return taxonomy;
 	}
-
+	
 	/**
-	 * Sets the taxonomy.
+	 * Gets the type.
 	 * 
-	 * @param taxonomy
-	 *            the new taxonomy
+	 * @return the type
 	 */
-	public void setTaxonomy(String taxonomy) {
-		this.taxonomy = taxonomy;
+	public String getType() {
+		return type;
 	}
-
+	
 	/**
-	 * Gets the code system name.
+	 * Gets the uuid.
 	 * 
-	 * @return the code system name
+	 * @return the uuid
 	 */
-	public String getCodeSystemName() {
-		return codeSystemName;
+	public String getUuid() {
+		return uuid;
 	}
-
+	
+	/**
+	 * Gets the version.
+	 * 
+	 * @return the version
+	 */
+	public String getVersion() {
+		return version;
+	}
+	
+	/**
+	 * Checks if is supp data element.
+	 * 
+	 * @return true, if is supp data element
+	 */
+	public boolean isSuppDataElement() {
+		return suppDataElement;
+	}
+	
+	/**
+	 * Checks if is used.
+	 * 
+	 * @return true, if is used
+	 */
+	public boolean isUsed() {
+		return isUsed;
+	}
+	
+	/**
+	 * Sets the code list name.
+	 * 
+	 * @param codeListName
+	 *            the new code list name
+	 */
+	public void setCodeListName(String codeListName) {
+		this.codeListName = codeListName;
+	}
+	
 	/**
 	 * Sets the code system name.
 	 * 
@@ -263,35 +221,94 @@ public class QualityDataSetDTO implements IsSerializable {
 	public void setCodeSystemName(String codeSystemName) {
 		this.codeSystemName = codeSystemName;
 	}
-
+	
 	/**
-	 * Gets the version.
+	 * Sets the data type.
 	 * 
-	 * @return the version
+	 * @param dataType
+	 *            the new data type
 	 */
-	public String getVersion() {
-		return version;
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
 	}
-
+	
 	/**
-	 * Sets the version.
-	 * 
-	 * @param version
-	 *            the new version
+	 * @param effectiveDate the effectiveDate to set
 	 */
-	public void setVersion(String version) {
-		this.version = version;
+	public void setEffectiveDate(String effectiveDate) {
+		this.effectiveDate = effectiveDate;
 	}
-
+	
 	/**
-	 * Gets the uuid.
+	 * Sets the id.
 	 * 
-	 * @return the uuid
+	 * @param id
+	 *            the new id
 	 */
-	public String getUuid() {
-		return uuid;
+	public void setId(String id) {
+		this.id = id;
 	}
-
+	
+	/**
+	 * Sets the occurrence text.
+	 * 
+	 * @param occurrenceText
+	 *            the new occurrence text
+	 */
+	public void setOccurrenceText(String occurrenceText) {
+		this.occurrenceText = occurrenceText;
+	}
+	
+	/**
+	 * Sets the oid.
+	 * 
+	 * @param oid
+	 *            the new oid
+	 */
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+	
+	/**
+	 * Sets the supp data element.
+	 * 
+	 * @param suppDataElement
+	 *            the new supp data element
+	 */
+	public void setSuppDataElement(boolean suppDataElement) {
+		this.suppDataElement = suppDataElement;
+	}
+	
+	/**
+	 * Sets the taxonomy.
+	 * 
+	 * @param taxonomy
+	 *            the new taxonomy
+	 */
+	public void setTaxonomy(String taxonomy) {
+		this.taxonomy = taxonomy;
+	}
+	
+	/**
+	 * Sets the type.
+	 * 
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	/**
+	 * Sets the used.
+	 * 
+	 * @param isUsed
+	 *            the new used
+	 */
+	public void setUsed(boolean isUsed) {
+		this.isUsed = isUsed;
+	}
+	
 	/**
 	 * Sets the uuid.
 	 * 
@@ -301,24 +318,27 @@ public class QualityDataSetDTO implements IsSerializable {
 	public void setUuid(String uuid) {
 		this.uuid = uuid;
 	}
-
+	
 	/**
-	 * Checks if is used.
+	 * Sets the version.
 	 * 
-	 * @return true, if is used
+	 * @param version
+	 *            the new version
 	 */
-	public boolean isUsed() {
-		return isUsed;
+	public void setVersion(String version) {
+		this.version = version;
 	}
-
-	/**
-	 * Sets the used.
-	 * 
-	 * @param isUsed
-	 *            the new used
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
 	 */
-	public void setUsed(boolean isUsed) {
-		this.isUsed = isUsed;
+	@Override
+	public String toString() {
+		if((occurrenceText!= null) && !occurrenceText.equals("")) {
+			return occurrenceText + " of "+codeListName + ": " + dataType + "-" +getOid();
+		} else {
+			return codeListName + ": " + dataType + "-" + getOid();
+		}
 	}
 	
 	

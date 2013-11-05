661fa1e7bb969465666e0fb4d29e10cd09b9cd55
Jyoti Narang <JNarang@hcareis.com>
2013-11-05 20:27:26 +0000

    Added 508 Aria tags
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@4039 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 7dd41518e55dd178307add1c4122389841d5e5e8

--- src/mat/client/shared/CreateMeasureWidget.java
+++ src/mat/client/shared/CreateMeasureWidget.java
@@ -2,21 +2,20 @@ package mat.client.shared;
 
 import mat.shared.ConstantMessages;
 
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /** CreateMeasureWidget.java. */
 public class CreateMeasureWidget extends Composite {
-	
 	/** Search button - {@link PrimaryButton}. */
 	private PrimaryButton createMeasure;
-	
 	/** The options. */
 	private ListBoxMVP options = new ListBoxMVP();
-	
 	/** Default Constructor. */
 	public CreateMeasureWidget() {
 		// searchInput.setWatermark("Search");
@@ -25,45 +24,45 @@ public class CreateMeasureWidget extends Composite {
 		options.getElement().getStyle().setMarginRight(10, Unit.PX);
 		options.getElement().getStyle().setMarginBottom(7, Unit.PX);
 		options.getElement().getStyle().setHeight(25, Unit.PX);
-		options.getElement().setId("options_ListBoxMVP");
+
 		createMeasure.getElement().setId("createMeasure_Button");
-		options.setName("Create");
-		DOM.setElementAttribute(options.getElement(), "id", "Create Measure");
+		options.setName("Create Measure Options");
+		DOM.setElementAttribute(options.getElement(), "id", "CreateMeasureWidget");
 		VerticalPanel topPanel = new VerticalPanel();
-		topPanel.getElement().setId("MeasureSearchFilterWidget_verticalPanel");
+		topPanel.getElement().setId("CreateMeasureWidget_verticalPanel");
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		topPanel.getElement().setId("CreateMeasureWidget_horizontalPanel");
 		loadListBoxOptions();
 		horizontalPanel.add(options);
 		horizontalPanel.add(createMeasure);
+		Label invisibleLabel = (Label) LabelBuilder.buildInvisibleLabel(new Label("CreateWidgetDisplayed"),
+				"CreateWidgetDisplayed");
+		topPanel.add(invisibleLabel);
 		topPanel.add(horizontalPanel);
 		horizontalPanel.setStylePrimaryName("searchWidget");
-		resetFilter();
-		
 		// All composites must call initWidget() in their constructors.
+		Element element = topPanel.getElement();
+		element.setAttribute("aria-role", "panel");
+		element.setAttribute("aria-labelledby", "CreateWidgetDisplayed");
+		element.setAttribute("aria-live", "assertive");
+		element.setAttribute("aria-atomic", "true");
+		element.setAttribute("aria-relevant", "all");
+		element.setAttribute("role", "alert");
 		initWidget(topPanel);
 	}
-	
 	/** @return the createMeasure */
 	public PrimaryButton getCreateMeasure() {
 		return createMeasure;
 	}
-	
 	/** @return the options */
 	public ListBoxMVP getOptions() {
 		return options;
 	}
-	
-	
 	private void loadListBoxOptions() {
 		options.addItem(ConstantMessages.DEFAULT_SELECT);
 		options.addItem(ConstantMessages.CREATE_NEW_MEASURE);
 		options.addItem(ConstantMessages.CREATE_NEW_VERSION);
 		options.addItem(ConstantMessages.CREATE_NEW_DRAFT);
-	}
-	
-	/** Method to Reset check box to default state. */
-	public final void resetFilter() {
-		
 	}
 	/**
 	 * @param createMeasure the createMeasure to set
@@ -71,13 +70,10 @@ public class CreateMeasureWidget extends Composite {
 	public void setCreateMeasure(PrimaryButton createMeasure) {
 		this.createMeasure = createMeasure;
 	}
-	
 	/**
 	 * @param options the options to set
 	 */
 	public void setOptions(ListBoxMVP options) {
 		this.options = options;
 	}
-	
-	
 }

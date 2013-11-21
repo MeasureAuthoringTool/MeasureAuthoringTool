e819e3fc6b02b79a43a8b48c028d68ae9987446e
Jyoti Narang <JNarang@hcareis.com>
2013-11-21 19:33:49 +0000

    PRIVACYPOLICY_URL changed to  http://www.cms.gov/About-CMS/Agency-Information/Aboutwebsite/Privacy-Policy.html as asked by Matt. THis is also checked in into Tag 26.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@4183 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 192cd7eecc7ff11643556aa043b4d5b25b318349

--- src/mat/client/util/ClientConstants.java
+++ src/mat/client/util/ClientConstants.java
@@ -12,26 +12,23 @@ public class ClientConstants {
 	
 	
 	
+	/** The accessibility policy url. */
+	public static String ACCESSIBILITY_POLICY_URL="https://www.emeasuretool.cms.gov/web/guest/accessibility-statement";
+	
 	/** The Constant ADMINISTRATOR. */
 	public final static String ADMINISTRATOR ="Administrator";
-	
-	/** The Constant MSIE. */
-	public final static String MSIE ="msie";
 	
 	/** The Constant ANCHOR_SIGN_OUT. */
 	public final static String ANCHOR_SIGN_OUT = "Sign Out";
 	
-	/** The accessibility policy url. */
-	public static String ACCESSIBILITY_POLICY_URL="https://www.emeasuretool.cms.gov/web/guest/accessibility-statement";
+	/** The Constant EXT_LINK_FOIA. */
+	public final static String EXT_LINK_FOIA ="https://www.cms.gov/center/freedom-of-information-act-center.html";
 	
-	/** The termsofuse url. */
-	public static String TERMSOFUSE_URL="https://www.emeasuretool.cms.gov/web/guest/terms-of-use";
+	/** The Constant EXT_LINK_HHS_Site. */
+	public final static String EXT_LINK_HHS_Site ="http://www.hhs.gov/";
 	
-	/** The privacypolicy url. */
-	public static String PRIVACYPOLICY_URL="https://www.emeasuretool.cms.gov/web/guest/privacy-policy";
-	
-	/** The userguide url. */
-	public static String USERGUIDE_URL="https://www.emeasuretool.cms.gov/documents/10179/13108/MAT+User+Guide/c50cc2fb-01ee-4b97-9cd1-2e33feca8763";
+	/** The Constant EXT_LINK_UMLS. */
+	public final static String EXT_LINK_UMLS="https://utslogin.nlm.nih.gov/cas/login/";
 	
 	/** The Constant HTML_LOGIN. */
 	public final static String HTML_LOGIN ="/Login.html";
@@ -39,63 +36,8 @@ public class ClientConstants {
 	/** The Constant HTML_MAT. */
 	public final static String HTML_MAT ="/Mat.html";
 	
-	/** The Constant EXT_LINK_HHS_Site. */
-	public final static String EXT_LINK_HHS_Site ="http://www.hhs.gov/";
-	
-	/** The Constant EXT_LINK_FOIA. */
-	public final static String EXT_LINK_FOIA ="https://www.cms.gov/center/freedom-of-information-act-center.html";
-	
-	/** The Constant EXT_LINK_UMLS. */
-	public final static String EXT_LINK_UMLS="https://utslogin.nlm.nih.gov/cas/login/";
-	
-	/** The Constant TEXT_ACCESSIBILITY_POLICY. */
-	public final static String TEXT_ACCESSIBILITY_POLICY ="Accessibility Policy";
-	
-	/** The Constant TEXT_TERMSOFUSE. */
-	public final static String TEXT_TERMSOFUSE ="Terms Of Use";
-	
-	/** The Constant TEXT_USER_GUIDE. */
-	public final static String TEXT_USER_GUIDE ="User Guide";
-	
-	/** The Constant TEXT_THE_TITLE. */
-	public final static String TEXT_THE_TITLE ="The Title";
-	
-	/** The Constant TEXT_PRIVACYPOLICY. */
-	public static final String TEXT_PRIVACYPOLICY = "Privacy Policy";
-	
-	/** The Constant TEXT_HHS_LINK. */
-	public static final String TEXT_HHS_LINK = "Link to Health and Human Services home page";
-	
-	/** The Constant TEXT_FOIA. */
-	public static final String TEXT_FOIA ="Freedom of Information Act";
-	
-	
-	/** The Constant TITLE_VALUE_SET_LIB. */
-	public final static String TITLE_VALUE_SET_LIB ="Value Set Archive";
-	
-	/** The Constant TITLE_MEASURE_LIB. */
-	public final static String TITLE_MEASURE_LIB ="Measure Library";
-	
-	/** The Constant TITLE_MEASURE_COMPOSER. */
-	public final static String TITLE_MEASURE_COMPOSER ="Measure Composer";
-	
-	/** The Constant TITLE_ADMIN. */
-	public final static String TITLE_ADMIN ="Admin";
-	
-	/** The Constant TITLE_MY_ACCOUNT. */
-	public final static String TITLE_MY_ACCOUNT="MAT Account";
-	
-	/** The Constant TITLE_UMLS. */
-	public final static String TITLE_UMLS="UMLS Account";
-	
-	/** The Constant TITLE_CMS_GOV_LOGO. */
-	public final static String TITLE_CMS_GOV_LOGO="CMS.gov";
-
-	/** The Constant TITLE_VALUE_SET_CHANGE_OWNERSHIP. */
-	public final static String TITLE_VALUE_SET_CHANGE_OWNERSHIP ="Value Set Ownership";
-	
-	/** The Constant TITLE_MEASURE_LIB_CHANGE_OWNERSHIP. */
-	public final static String TITLE_MEASURE_LIB_CHANGE_OWNERSHIP ="Measure Library Ownership";
+	/** The Constant MAINLAYOUT_ALERT_TITLE. */
+	public final static String MAINLAYOUT_ALERT_TITLE ="Informational Alert";
 	
 	/** The Constant MAINLAYOUT_LOADING_WIDGET_MSG. */
 	public final static String MAINLAYOUT_LOADING_WIDGET_MSG ="Loading Please Wait...";
@@ -103,11 +45,69 @@ public class ClientConstants {
 	/** The Constant MAINLAYOUT_SIGNOUT_WIDGET_MSG. */
 	public final static String MAINLAYOUT_SIGNOUT_WIDGET_MSG =" Signing Out Please Wait...";
 	
-	/** The Constant MAINLAYOUT_ALERT_TITLE. */
-	public final static String MAINLAYOUT_ALERT_TITLE ="Informational Alert";
+	/** The Constant MSIE. */
+	public final static String MSIE ="msie";
+	
+	/** The privacypolicy url. */
+	public static String PRIVACYPOLICY_URL="http://www.cms.gov/About-CMS/Agency-Information/Aboutwebsite/Privacy-Policy.html";
+	
+	/** The termsofuse url. */
+	public static String TERMSOFUSE_URL="https://www.emeasuretool.cms.gov/web/guest/terms-of-use";
+	
+	/** The Constant TEXT_ACCESSIBILITY_POLICY. */
+	public final static String TEXT_ACCESSIBILITY_POLICY ="Accessibility Policy";
+	
+	/** The Constant TEXT_FOIA. */
+	public static final String TEXT_FOIA ="Freedom of Information Act";
+	
+	/** The Constant TEXT_HHS_LINK. */
+	public static final String TEXT_HHS_LINK = "Link to Health and Human Services home page";
+	
+	/** The Constant TEXT_PRIVACYPOLICY. */
+	public static final String TEXT_PRIVACYPOLICY = "Privacy Policy";
+	
+	/** The Constant TEXT_TERMSOFUSE. */
+	public final static String TEXT_TERMSOFUSE ="Terms Of Use";
+	
+	
+	/** The Constant TEXT_THE_TITLE. */
+	public final static String TEXT_THE_TITLE ="The Title";
+	
+	/** The Constant TEXT_USER_GUIDE. */
+	public final static String TEXT_USER_GUIDE ="User Guide";
+	
+	/** The Constant TITLE_ADMIN. */
+	public final static String TITLE_ADMIN ="Admin";
+	
+	/** The Constant TITLE_CMS_GOV_LOGO. */
+	public final static String TITLE_CMS_GOV_LOGO="CMS.gov";
+	
+	/** The Constant TITLE_MEASURE_COMPOSER. */
+	public final static String TITLE_MEASURE_COMPOSER ="Measure Composer";
+	
+	/** The Constant TITLE_MEASURE_LIB. */
+	public final static String TITLE_MEASURE_LIB ="Measure Library";
+	
+	/** The Constant TITLE_MEASURE_LIB_CHANGE_OWNERSHIP. */
+	public final static String TITLE_MEASURE_LIB_CHANGE_OWNERSHIP ="Measure Library Ownership";
+	
+	/** The Constant TITLE_MY_ACCOUNT. */
+	public final static String TITLE_MY_ACCOUNT="MAT Account";
+	
+	/** The Constant TITLE_UMLS. */
+	public final static String TITLE_UMLS="UMLS Account";
+	
+	/** The Constant TITLE_VALUE_SET_CHANGE_OWNERSHIP. */
+	public final static String TITLE_VALUE_SET_CHANGE_OWNERSHIP ="Value Set Ownership";
+	
+	/** The Constant TITLE_VALUE_SET_LIB. */
+	public final static String TITLE_VALUE_SET_LIB ="Value Set Archive";
 	
 	/** The Constant USER_STATUS_NOT_LOGGEDIN. */
 	public final static String USER_STATUS_NOT_LOGGEDIN ="NotLoggedInYet";
+	
+	/** The userguide url. */
+	public static String USERGUIDE_URL="https://www.emeasuretool.cms.gov/documents/10179/13108/MAT+User+Guide/c50cc2fb-01ee-4b97-9cd1-2e33feca8763";
 	
 	
 	

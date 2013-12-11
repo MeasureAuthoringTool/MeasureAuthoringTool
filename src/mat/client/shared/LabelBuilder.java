cb3e832c85ffc2a2d83ebd99fccdcd1ec069cc8a
Jyoti Narang <JNarang@hcareis.com>
2013-12-11 17:00:48 +0000

    Overloaded buildInvisibleLabel method with argument String id, String labelString.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@4344 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 3de0c9b96f0b96205f8f6704a5e523bb52b18675

--- src/mat/client/shared/LabelBuilder.java
+++ src/mat/client/shared/LabelBuilder.java
@@ -8,53 +8,17 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class LabelBuilder {
 	
-	/**
-	 * Builds the label.
-	 * 
-	 * @param widget
-	 *            the widget
-	 * @param labelStr
-	 *            the label str
-	 * @return the widget
-	 */
-	public static Widget buildLabel(Widget widget, String labelStr) {
+	/** Builds the invisible label.
+	 * @param id - String id for Label.
+	 * @param labelStr - Label Text.
+	 * @return Widget. */
+	public static Widget buildInvisibleLabel(String id, String labelStr) {
 		//widget.getElement().setAttribute("id", labelStr);
-		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>" + labelStr + "</label>");
-		label.getElement().setAttribute("id", labelStr);
+		HTML label = new HTML("<label id= '" + id + "' for='" + labelStr + "'>" + labelStr + "</label>");
+		label.getElement().setAttribute("id", id);
+		label.setStyleName("invisible");
 		return label;
 	}
-	
-	/**
-	 * Builds the required label.
-	 * 
-	 * @param widget
-	 *            the widget
-	 * @param labelStr
-	 *            the label str
-	 * @return the widget
-	 */
-	public static Widget buildRequiredLabel(Widget widget, String labelStr) {
-	//	widget.getElement().setAttribute("id", labelStr);
-		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>" + labelStr + RequiredIndicator.get() + "</label>" );
-		label.getElement().setAttribute("id", labelStr);
-		return label;
-	}
-	
-	/**
-	 * Builds the label.
-	 * 
-	 * @param labelStr
-	 *            the label str
-	 * @param forVal
-	 *            the for val
-	 * @return the widget
-	 */
-	public static Widget buildLabel(String labelStr, String forVal) {
-		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + forVal + "'>" + labelStr + "</label>");
-		label.getElement().setAttribute("id", labelStr);
-		return label;
-	}
-	
 	/**
 	 * Builds the invisible label.
 	 * 
@@ -73,24 +37,50 @@ public class LabelBuilder {
 	}
 	
 	/**
-	 * Sets the invisible label.
+	 * Builds the invisible label with content.
 	 * 
 	 * @param widget
 	 *            the widget
-	 * @param label
-	 *            the label
-	 * @param labelStr
-	 *            the label str
+	 * @param id
+	 *            the id
+	 * @param content
+	 *            the content
 	 * @return the widget
 	 */
-	public static Widget setInvisibleLabel(Widget widget, HTML label, String labelStr) {
-	//	widget.getElement().setAttribute("id", labelStr);
-		label.setHTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>"+labelStr+"</label>");
-		label.getElement().setAttribute("id", labelStr);
+	public static Widget buildInvisibleLabelWithContent(Widget widget, String id, String content) {
+		//widget.getElement().setAttribute("id", labelStr);
+		HTML label = new HTML("<label id= '" +id+ "' for='" + id + "'>"+content+"</label>");
+		label.getElement().setAttribute("id", id);
 		label.setStyleName("invisible");
 		return label;
 	}
 	
+	/**
+	 * Builds the label.
+	 * 
+	 * @param labelStr
+	 *            the label str
+	 * @param forVal
+	 *            the for val
+	 * @return the widget
+	 */
+	public static Widget buildLabel(String labelStr, String forVal) {
+		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + forVal + "'>" + labelStr + "</label>");
+		label.getElement().setAttribute("id", labelStr);
+		return label;
+	}
+	
+	/** Builds the label.
+	 * 
+	 * @param widget the widget
+	 * @param labelStr the label str
+	 * @return the widget */
+	public static Widget buildLabel(Widget widget, String labelStr) {
+		// widget.getElement().setAttribute("id", labelStr);
+		HTML label = new HTML("<label id= '" + labelStr + "' for='" + labelStr + "'>" + labelStr + "</label>");
+		label.getElement().setAttribute("id", labelStr);
+		return label;
+	}
 	/**
 	 * Builds the label with embedded link.
 	 * 
@@ -124,22 +114,38 @@ public class LabelBuilder {
 	}
 	
 	/**
-	 * Builds the invisible label with content.
+	 * Builds the required label.
 	 * 
 	 * @param widget
 	 *            the widget
-	 * @param id
-	 *            the id
-	 * @param content
-	 *            the content
+	 * @param labelStr
+	 *            the label str
 	 * @return the widget
 	 */
-	public static Widget buildInvisibleLabelWithContent(Widget widget, String id, String content) {
-		//widget.getElement().setAttribute("id", labelStr);
-		HTML label = new HTML("<label id= '" +id+ "' for='" + id + "'>"+content+"</label>");
-		label.getElement().setAttribute("id", id);
+	public static Widget buildRequiredLabel(Widget widget, String labelStr) {
+		//	widget.getElement().setAttribute("id", labelStr);
+		HTML label = new HTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>" + labelStr + RequiredIndicator.get() + "</label>" );
+		label.getElement().setAttribute("id", labelStr);
+		return label;
+	}
+	
+	/**
+	 * Sets the invisible label.
+	 * 
+	 * @param widget
+	 *            the widget
+	 * @param label
+	 *            the label
+	 * @param labelStr
+	 *            the label str
+	 * @return the widget
+	 */
+	public static Widget setInvisibleLabel(Widget widget, HTML label, String labelStr) {
+		//	widget.getElement().setAttribute("id", labelStr);
+		label.setHTML("<label id= '" +labelStr+ "' for='" + labelStr + "'>"+labelStr+"</label>");
+		label.getElement().setAttribute("id", labelStr);
 		label.setStyleName("invisible");
 		return label;
 	}
-
+	
 }

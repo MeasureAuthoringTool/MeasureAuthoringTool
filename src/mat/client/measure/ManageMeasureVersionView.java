fd4504acd1ce11af8e9761e1af68beb6331437b4
Jyoti Narang <JNarang@hcareis.com>
2013-12-12 19:14:54 +0000

    Mat-2950 : Added invisibleLable with lable Text to read the content as Table summary in Applied QDM Table in order to make jaws read its a table.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@4375 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: e35095364008a1a1d8d418baabddbad6a48f044b

--- src/mat/client/measure/ManageMeasureVersionView.java
+++ src/mat/client/measure/ManageMeasureVersionView.java
@@ -5,27 +5,28 @@ package mat.client.measure;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import mat.client.CustomPager;
 import mat.client.ImageResources;
 import mat.client.measure.ManageMeasureSearchModel.Result;
 import mat.client.shared.CustomButton;
 import mat.client.shared.ErrorMessageDisplay;
 import mat.client.shared.ErrorMessageDisplayInterface;
+import mat.client.shared.LabelBuilder;
 import mat.client.shared.MatSimplePager;
 import mat.client.shared.RadioButtonCell;
 import mat.client.shared.SaveCancelButtonBar;
 import mat.client.shared.SearchWidget;
 import mat.client.shared.SpacerWidget;
 import mat.client.shared.search.SearchResults;
+import mat.client.util.CellTableUtility;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.cell.client.SafeHtmlCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.TableCaptionElement;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
@@ -48,8 +49,14 @@ import com.google.gwt.view.client.SingleSelectionModel;
  */
 public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionDisplay {
 	
+	/** CellTable Page Size. */
+	private static final int PAGE_SIZE = 25;
+	
 	/** The button bar. */
 	private SaveCancelButtonBar buttonBar = new SaveCancelButtonBar();
+	
+	/** The cell table panel. */
+	private VerticalPanel cellTablePanel = new VerticalPanel();
 	
 	/** The error messages. */
 	private ErrorMessageDisplay errorMessages = new ErrorMessageDisplay();
@@ -58,23 +65,20 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 	private FlowPanel mainPanel = new FlowPanel();
 	
 	/** The major radio. */
-	private RadioButton majorRadio = new RadioButton("group","Major");
+	private RadioButton majorRadio = new RadioButton("group", "Major");
 	
 	/** The minor radio. */
-	private RadioButton minorRadio = new RadioButton("group","Minor");
+	private RadioButton minorRadio = new RadioButton("group", "Minor");
 	
 	/** The measure search filter widget. */
 	private SearchWidget searchWidget = new SearchWidget();
 	
+	/** The selection model. */
+	private SingleSelectionModel<Result> selectionModel;
+	
 	/** Zoom Button for Showing Search Widget. */
 	private CustomButton zoomButton = (CustomButton) getImage("Search",
 			ImageResources.INSTANCE.search_zoom(), "Search");
-	
-	/** The cell table panel. */
-	private VerticalPanel cellTablePanel = new VerticalPanel(); 
-	
-	/** The selection model. */
-	private SingleSelectionModel<Result> selectionModel;
 	
 	/**
 	 * Instantiates a new manage measure version view.
@@ -85,8 +89,8 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 		mainPanel.addStyleName("leftAligned");
 		mainPanel.add(searchWidget);
 		mainPanel.add(new SpacerWidget());
-				
-		cellTablePanel.getElement().setId("cellTablePanel_VerticalPanel");	
+		
+		cellTablePanel.getElement().setId("cellTablePanel_VerticalPanel");
 		cellTablePanel.setWidth("77%");
 		mainPanel.add(cellTablePanel);
 		mainPanel.add(new SpacerWidget());
@@ -98,7 +102,7 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 		radioPanel.add(new Label("Select Version Type"));
 		radioPanel.add(new SpacerWidget());
 		radioPanel.add(majorRadio);
-		radioPanel.add(minorRadio);		
+		radioPanel.add(minorRadio);
 		mainPanel.add(radioPanel);
 		mainPanel.add(new SpacerWidget());
 		
@@ -112,6 +116,44 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 		
 	}
 	
+	/** Adds the column to table.
+	 * @param cellTable the cell table
+	 * @return the cell table */
+	private CellTable<Result> addColumnToTable(final CellTable<Result> cellTable) {
+		Column<Result, Boolean> radioButtonColumn = new Column<Result, Boolean>(new RadioButtonCell(true, true)) {
+			@Override
+			public Boolean getValue(Result result) {
+				return cellTable.getSelectionModel().isSelected(result);
+			}
+		};
+		radioButtonColumn.setFieldUpdater(new FieldUpdater<Result, Boolean>() {
+			@Override
+			public void update(int index, Result object, Boolean value) {
+				cellTable.getSelectionModel().setSelected(object, true);
+			}
+		});
+		cellTable.addColumn(radioButtonColumn, SafeHtmlUtils.fromSafeConstant("<span title='Select'"
+				+ " tabindex=\"0\">" + "Select" + "</span>"));
+		Column<Result, SafeHtml> measureNameColumn = new Column<Result, SafeHtml>(new SafeHtmlCell()) {
+			@Override
+			public SafeHtml getValue(Result object) {
+				String title = "Measure Name " + object.getName();
+				return CellTableUtility.getColumnToolTip(object.getName(), title);
+			}
+		};
+		cellTable.addColumn(measureNameColumn, SafeHtmlUtils.fromSafeConstant("<span title='Measure Name'"
+				+ " tabindex=\"0\">" + "Measure Name" + "</span>"));
+		Column<Result, SafeHtml> versionColumn = new Column<Result, SafeHtml>(new SafeHtmlCell()) {
+			@Override
+			public SafeHtml getValue(Result object) {
+				String title = "Version " + object.getVersion();
+				return CellTableUtility.getColumnToolTip(object.getVersion(), title);
+			}
+		};
+		cellTable.addColumn(versionColumn, SafeHtmlUtils.fromSafeConstant("<span title='Version'"
+				+ " tabindex=\"0\">" + "Version" + "</span>"));
+		return cellTable;
+	}
 	/* (non-Javadoc)
 	 * @see mat.client.measure.ManageMeasurePresenter.BaseDisplay#asWidget()
 	 */
@@ -131,113 +173,45 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 		Label cellTablePanelHeader = new Label("Select a Draft to create a Measure Version.");
 		cellTablePanelHeader.getElement().setId("cellTablePanelHeader_Label");
 		cellTablePanelHeader.setStyleName("recentSearchHeader");
-		cellTablePanelHeader.getElement().setAttribute("tabIndex", "0");
-		cellTablePanel.add(cellTablePanelHeader);
-		cellTablePanel.add(new SpacerWidget());
-		
 		CellTable<ManageMeasureSearchModel.Result> cellTable = new CellTable<ManageMeasureSearchModel.Result>();
 		ListDataProvider<ManageMeasureSearchModel.Result> sortProvider = new ListDataProvider<ManageMeasureSearchModel.Result>();
-		
 		List<Result> measureList = new ArrayList<Result>();
-		measureList.addAll(((ManageVersionMeasureModel)results).getDataList());	
-		cellTable.setPageSize(25);
+		measureList.addAll(((ManageVersionMeasureModel) results).getDataList());
+		cellTable.setPageSize(PAGE_SIZE);
 		cellTable.redraw();
 		cellTable.setRowCount(measureList.size(), true);
 		cellTable.setSelectionModel(getSelectionModelWithHandler());
 		sortProvider.refresh();
-		sortProvider.getList().addAll(((ManageVersionMeasureModel)results).getDataList());
+		sortProvider.getList().addAll(((ManageVersionMeasureModel) results).getDataList());
 		cellTable = addColumnToTable(cellTable);
 		sortProvider.addDataDisplay(cellTable);
 		CustomPager.Resources pagerResources = GWT.create(CustomPager.Resources.class);
-		MatSimplePager spager = new MatSimplePager(CustomPager.TextLocation.CENTER, pagerResources, false, 0, true);	
+		MatSimplePager spager = new MatSimplePager(CustomPager.TextLocation.CENTER, pagerResources, false, 0, true);
 		spager.setPageStart(0);
 		spager.setDisplay(cellTable);
-        spager.setPageSize(25);
-        spager.setToolTipAndTabIndex(spager);
-        cellTable.setWidth("100%");
-        cellTable.setColumnWidth(0, 15.0, Unit.PCT);
-        cellTable.setColumnWidth(1, 63.0, Unit.PCT);
-        cellTable.setColumnWidth(2, 22.0, Unit.PCT);
-        cellTablePanel.add(cellTable);
-        cellTablePanel.add(new SpacerWidget());
-        cellTablePanel.add(spager);
+		spager.setPageSize(PAGE_SIZE);
+		spager.setToolTipAndTabIndex(spager);
+		cellTable.setWidth("100%");
+		cellTable.setColumnWidth(0, 15.0, Unit.PCT);
+		cellTable.setColumnWidth(1, 63.0, Unit.PCT);
+		cellTable.setColumnWidth(2, 22.0, Unit.PCT);
+		com.google.gwt.dom.client.TableElement elem = cellTable.getElement().cast();
+		TableCaptionElement caption = elem.createCaption();
+		caption.appendChild(cellTablePanelHeader.getElement());
+		Label invisibleLabel = (Label) LabelBuilder
+				.buildInvisibleLabel(
+						"measureVersionSummary",
+						"In the following table Measure Name are given in the second column,"
+								+ "with the Version in the third column and options can be"
+								+ "selected from first column.");
+		cellTable.getElement().setAttribute("id", "measureVersionFromDraftCellTable");
+		cellTable.getElement().setAttribute("aria-describedby", "measureVersionSummary");
+		cellTablePanel.add(invisibleLabel);
+		cellTablePanel.add(cellTable);
+		cellTablePanel.add(new SpacerWidget());
+		cellTablePanel.add(spager);
 	}
 	
-	
-	/**
-	 * Gets the selection model with handler.
-	 *
-	 * @return the selection model with handler
-	 */
-	private SingleSelectionModel<Result> getSelectionModelWithHandler() {
-		selectionModel = new SingleSelectionModel<Result>();
-		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {			
-			@Override
-			public void onSelectionChange(SelectionChangeEvent event) {
-				getErrorMessageDisplay().clear();
-			}
-		});
-		return selectionModel;
-	}
-
-	/* (non-Javadoc)
-	 * @see mat.client.measure.ManageMeasurePresenter.VersionDisplay#getSelectedMeasure()
-	 */
-	@Override
-	public Result getSelectedMeasure() {
-		return selectionModel.getSelectedObject();
-	}
-
-	/** Adds the column to table.
-	 * 
-	 * @param cellTable the cell table
-	 * @return the cell table */
-	private CellTable<Result> addColumnToTable(final CellTable<Result> cellTable) {
-		Column<Result, Boolean> radioButtonColumn = new Column<Result, Boolean>(new RadioButtonCell(true, true)) {
-			@Override
-			public Boolean getValue(Result result) {
-				return cellTable.getSelectionModel().isSelected(result);
-			}
-		};
-		radioButtonColumn.setFieldUpdater(new FieldUpdater<Result, Boolean>() {
-			@Override
-			public void update(int index, Result object, Boolean value) {
-				cellTable.getSelectionModel().setSelected(object, true);
-			}
-		});
-		cellTable.addColumn(radioButtonColumn, SafeHtmlUtils.fromSafeConstant("<span title='Select' tabindex=\"0\">" +"Select"+ "</span>"));
-		
-		Column<Result, SafeHtml> measureNameColumn = new Column<Result, SafeHtml>(new SafeHtmlCell()) {			
-			@Override
-			public SafeHtml getValue(Result object) {
-				return getColumnValueWithToolTip("Measure Name", object.getName());
-			}
-		};
-		cellTable.addColumn(measureNameColumn, SafeHtmlUtils.fromSafeConstant("<span title='Measure Name' tabindex=\"0\">" +"Measure Name"+ "</span>"));
-		
-		Column<Result, SafeHtml> versionColumn = new Column<Result, SafeHtml>(new SafeHtmlCell()) {
-			@Override
-			public SafeHtml getValue(Result object) {
-				return getColumnValueWithToolTip("Version", object.getVersion());
-			}
-		};
-		cellTable.addColumn(versionColumn, SafeHtmlUtils.fromSafeConstant("<span title='Version' tabindex=\"0\">" +"Version"+ "</span>"));
-		
-		return cellTable;
-	}
-
-	/**
-	 * Gets the column value with tool tip.
-	 *
-	 * @param columnName the column name
-	 * @param columnValue the column value
-	 * @return the column value with tool tip
-	 */
-	private SafeHtml getColumnValueWithToolTip(String columnName, String columnValue){
-		String htmlConstant = "<span tabindex=\"0\" title='" + columnName + ": " + columnValue + "'>"+ columnValue + "</span>";
-		return new SafeHtmlBuilder().appendHtmlConstant(htmlConstant).toSafeHtml();
-	}
-
 	/* (non-Javadoc)
 	 * @see mat.client.measure.ManageMeasurePresenter.VersionDisplay#getCancelButton()
 	 */
@@ -293,7 +267,7 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 	public HasClickHandlers getSaveButton() {
 		return buttonBar.getSaveButton();
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see mat.client.measure.ManageMeasurePresenter.VersionDisplay#getSearchButton()
 	 */
@@ -301,7 +275,7 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 	public HasClickHandlers getSearchButton() {
 		return searchWidget.getSearchButton();
 	}
-
+	
 	/** Gets the measure search filter widget.
 	 * 
 	 * @return the searchWidget */
@@ -309,12 +283,36 @@ public class ManageMeasureVersionView implements ManageMeasurePresenter.VersionD
 	public SearchWidget getSearchWidget() {
 		return searchWidget;
 	}
-
+	
+	/* (non-Javadoc)
+	 * @see mat.client.measure.ManageMeasurePresenter.VersionDisplay#getSelectedMeasure()
+	 */
+	@Override
+	public Result getSelectedMeasure() {
+		return selectionModel.getSelectedObject();
+	}
+	
+	/**
+	 * Gets the selection model with handler.
+	 *
+	 * @return the selection model with handler
+	 */
+	private SingleSelectionModel<Result> getSelectionModelWithHandler() {
+		selectionModel = new SingleSelectionModel<Result>();
+		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+			@Override
+			public void onSelectionChange(SelectionChangeEvent event) {
+				getErrorMessageDisplay().clear();
+			}
+		});
+		return selectionModel;
+	}
+	
 	/* (non-Javadoc)
 	 * @see mat.client.measure.ManageMeasurePresenter.VersionDisplay#getZoomButton()
 	 */
 	@Override
 	public CustomButton getZoomButton() {
 		return zoomButton;
-	}	
+	}
 }

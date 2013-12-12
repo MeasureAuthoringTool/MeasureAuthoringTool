c9b9783bb5abc62ad6571bd1cbc9dd3c8eb41082
Jyoti Narang <JNarang@hcareis.com>
2013-12-12 19:05:45 +0000

    Mat-2950 : Added invisibleLable with lable Text to read the content as Table summary in Applied QDM Table in order to make jaws read its a table.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@4374 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: c62f1317026fdc931ca0213003f7278940a4af72

--- src/mat/client/measure/ManageMeasureDraftView.java
+++ src/mat/client/measure/ManageMeasureDraftView.java
@@ -5,27 +5,28 @@ package mat.client.measure;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import mat.client.CustomPager;
 import mat.client.ImageResources;
 import mat.client.measure.ManageMeasureSearchModel.Result;
 import mat.client.shared.CustomButton;
 import mat.client.shared.ErrorMessageDisplay;
 import mat.client.shared.ErrorMessageDisplayInterface;
+import mat.client.shared.LabelBuilder;
 import mat.client.shared.MatSimplePager;
 import mat.client.shared.RadioButtonCell;
 import mat.client.shared.SaveCancelButtonBar;
 import mat.client.shared.SearchWidget;
 import mat.client.shared.SpacerWidget;
 import mat.client.shared.search.SearchResults;
+import mat.client.util.CellTableUtility;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.cell.client.SafeHtmlCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.dom.client.TableCaptionElement;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
@@ -40,36 +41,27 @@ import com.google.gwt.view.client.SingleSelectionModel;
 
 /**
  * The Class ManageMeasureDraftView.
- * 
  * @author vandavar An view class to manage the widgets for the DRAFT creation.
  */
 public class ManageMeasureDraftView implements ManageMeasurePresenter.DraftDisplay {
-	
+	private static final int PAGE_SIZE = 25;
 	/** The button bar. */
 	private SaveCancelButtonBar buttonBar = new SaveCancelButtonBar();
-	
+	/** The cell table panel. */
+	private VerticalPanel cellTablePanel = new VerticalPanel();
 	/** The error messages. */
 	private ErrorMessageDisplay errorMessages = new ErrorMessageDisplay();
-	
 	/** The main panel. */
 	private FlowPanel mainPanel = new FlowPanel();
-	
 	/** The measure search filter widget. */
 	private SearchWidget searchWidget = new SearchWidget();
-		
+	/** The selection model. */
+	private SingleSelectionModel<Result> selectionModel;
 	/**
 	 * Zoom Button for Showing Search Widget.
 	 */
 	private CustomButton zoomButton = (CustomButton) getImage("Search",
 			ImageResources.INSTANCE.search_zoom(), "Search");
-	
-	/** The cell table panel. */
-	private VerticalPanel cellTablePanel = new VerticalPanel(); 
-	
-	/** The selection model. */
-	private SingleSelectionModel<Result> selectionModel;
-	
-	
 	/**
 	 * Instantiates a new manage measure draft view.
 	 */
@@ -78,14 +70,11 @@ public class ManageMeasureDraftView implements ManageMeasurePresenter.DraftDispl
 		mainPanel.setStylePrimaryName("contentPanel");
 		mainPanel.addStyleName("leftAligned");
 		mainPanel.add(searchWidget);
-				
-		cellTablePanel.getElement().setId("cellTablePanel_VerticalPanel");	
+		cellTablePanel.getElement().setId("cellTablePanel_VerticalPanel");
 		cellTablePanel.setWidth("77%");
 		mainPanel.add(cellTablePanel);
 		mainPanel.add(new SpacerWidget());
-		
 		mainPanel.add(errorMessages);
-		
 		SimplePanel buttonPanel = new SimplePanel();
 		buttonBar.getSaveButton().setText("Save and Continue");
 		buttonBar.getSaveButton().setTitle("Save and Continue");
@@ -94,83 +83,6 @@ public class ManageMeasureDraftView implements ManageMeasurePresenter.DraftDispl
 		buttonPanel.setWidth("100%");
 		mainPanel.add(buttonPanel);
 	}
-	
-	/* (non-Javadoc)
-	 * @see mat.client.measure.ManageMeasurePresenter.BaseDisplay#asWidget()
-	 */
-	@Override
-	public Widget asWidget() {
-		return mainPanel;
-	}
-	
-	/* (non-Javadoc)
-	 * @see mat.client.measure.ManageMeasurePresenter.DraftDisplay#buildDataTable(mat.client.shared.search.SearchResults)
-	 */
-	@Override
-	public void buildDataTable(SearchResults<Result> results) {
-		cellTablePanel.clear();
-		cellTablePanel.setStyleName("cellTablePanel");
-		Label cellTablePanelHeader = new Label("Select a Measure Version to create a Draft.");
-		cellTablePanelHeader.getElement().setId("cellTablePanelHeader_Label");
-		cellTablePanelHeader.setStyleName("recentSearchHeader");
-		cellTablePanelHeader.getElement().setAttribute("tabIndex", "0");
-		cellTablePanel.add(cellTablePanelHeader);
-		cellTablePanel.add(new SpacerWidget());
-		
-		CellTable<ManageMeasureSearchModel.Result> cellTable = new CellTable<ManageMeasureSearchModel.Result>();
-		ListDataProvider<ManageMeasureSearchModel.Result> sortProvider = new ListDataProvider<ManageMeasureSearchModel.Result>();
-		
-		List<Result> measureList = new ArrayList<Result>();
-		measureList.addAll(((ManageDraftMeasureModel)results).getDataList());	
-		cellTable.setPageSize(25);
-		cellTable.redraw();
-		cellTable.setRowCount(measureList.size(), true);
-		cellTable.setSelectionModel(getSelectionModelWithHandler());
-		sortProvider.refresh();
-		sortProvider.getList().addAll(((ManageDraftMeasureModel)results).getDataList());
-		cellTable = addColumnToTable(cellTable);
-		sortProvider.addDataDisplay(cellTable);
-		CustomPager.Resources pagerResources = GWT.create(CustomPager.Resources.class);
-		MatSimplePager spager = new MatSimplePager(CustomPager.TextLocation.CENTER, pagerResources, false, 0, true);	
-		spager.setPageStart(0);
-		spager.setDisplay(cellTable);
-        spager.setPageSize(25);
-        spager.setToolTipAndTabIndex(spager);
-        cellTable.setWidth("100%");
-        cellTable.setColumnWidth(0, 15.0, Unit.PCT);
-        cellTable.setColumnWidth(1, 63.0, Unit.PCT);
-        cellTable.setColumnWidth(2, 22.0, Unit.PCT);
-        cellTablePanel.add(cellTable);
-        cellTablePanel.add(new SpacerWidget());
-        cellTablePanel.add(spager);
-	}
-	
-	/**
-	 * Gets the selection model with handler.
-	 *
-	 * @return the selection model with handler
-	 */
-	private SingleSelectionModel<Result> getSelectionModelWithHandler() {
-		selectionModel = new SingleSelectionModel<Result>();
-		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {			
-			@Override
-			public void onSelectionChange(SelectionChangeEvent event) {
-				getErrorMessageDisplay().clear();
-			}
-		});
-		return selectionModel;
-	}
-	
-	/**
-	 * Gets the selected measure.
-	 *
-	 * @return the selected measure
-	 */
-	@Override
-	public Result getSelectedMeasure() {
-		return selectionModel.getSelectedObject();
-	}
-	
 	/**
 	 * Adds the column to table.
 	 *
@@ -190,39 +102,83 @@ public class ManageMeasureDraftView implements ManageMeasurePresenter.DraftDispl
 				cellTable.getSelectionModel().setSelected(object, true);
 			}
 		});
-		cellTable.addColumn(radioButtonColumn, SafeHtmlUtils.fromSafeConstant("<span title='Select' tabindex=\"0\">" +"Select"+ "</span>"));
-		
-		Column<Result, SafeHtml> measureNameColumn = new Column<Result, SafeHtml>(new SafeHtmlCell()) {			
+		cellTable.addColumn(radioButtonColumn, SafeHtmlUtils.fromSafeConstant("<span title='Select' tabindex=\"0\">"
+				+ "Select" + "</span>"));
+		Column<Result, SafeHtml> measureNameColumn = new Column<Result, SafeHtml>(new SafeHtmlCell()) {
 			@Override
 			public SafeHtml getValue(Result object) {
-				return getColumnValueWithToolTip("Measure Name", object.getName());
+				String title = "Measure Name " + object.getName();
+				return CellTableUtility.getColumnToolTip(object.getName(), title);
 			}
 		};
-		cellTable.addColumn(measureNameColumn, SafeHtmlUtils.fromSafeConstant("<span title='Measure Name' tabindex=\"0\">" +"Measure Name"+ "</span>"));
-		
+		cellTable.addColumn(measureNameColumn, SafeHtmlUtils.fromSafeConstant("<span title='Measure Name' tabindex=\"0\">"
+				+ "Measure Name" + "</span>"));
 		Column<Result, SafeHtml> versionColumn = new Column<Result, SafeHtml>(new SafeHtmlCell()) {
 			@Override
 			public SafeHtml getValue(Result object) {
-				return getColumnValueWithToolTip("Version", object.getVersion());
+				String title = "Version " + object.getVersion();
+				return CellTableUtility.getColumnToolTip(object.getVersion(), title);
 			}
 		};
-		cellTable.addColumn(versionColumn, SafeHtmlUtils.fromSafeConstant("<span title='Version' tabindex=\"0\">" +"Version"+ "</span>"));
-		
+		cellTable.addColumn(versionColumn, SafeHtmlUtils.fromSafeConstant("<span title='Version' tabindex=\"0\">"
+				+ "Version" + "</span>"));
 		return cellTable;
 	}
-
-	/**
-	 * Gets the column value with tool tip.
-	 *
-	 * @param columnName the column name
-	 * @param columnValue the column value
-	 * @return the column value with tool tip
+	/* (non-Javadoc)
+	 * @see mat.client.measure.ManageMeasurePresenter.BaseDisplay#asWidget()
 	 */
-	private SafeHtml getColumnValueWithToolTip(String columnName, String columnValue){
-		String htmlConstant = "<span tabindex=\"0\" title='" + columnName + ": " + columnValue + "'>"+ columnValue + "</span>";
-		return new SafeHtmlBuilder().appendHtmlConstant(htmlConstant).toSafeHtml();
+	@Override
+	public Widget asWidget() {
+		return mainPanel;
 	}
-	
+	/* (non-Javadoc)
+	 * @see mat.client.measure.ManageMeasurePresenter.DraftDisplay#buildDataTable(mat.client.shared.search.SearchResults)
+	 */
+	@Override
+	public void buildDataTable(SearchResults<Result> results) {
+		cellTablePanel.clear();
+		cellTablePanel.setStyleName("cellTablePanel");
+		Label cellTablePanelHeader = new Label("Select a Measure Version to create a Draft.");
+		cellTablePanelHeader.getElement().setId("cellTablePanelHeader_Label");
+		cellTablePanelHeader.setStyleName("recentSearchHeader");
+		CellTable<ManageMeasureSearchModel.Result> cellTable = new CellTable<ManageMeasureSearchModel.Result>();
+		ListDataProvider<ManageMeasureSearchModel.Result> sortProvider = new ListDataProvider<ManageMeasureSearchModel.Result>();
+		List<Result> measureList = new ArrayList<Result>();
+		measureList.addAll(((ManageDraftMeasureModel) results).getDataList());
+		cellTable.setPageSize(PAGE_SIZE);
+		cellTable.redraw();
+		cellTable.setRowCount(measureList.size(), true);
+		cellTable.setSelectionModel(getSelectionModelWithHandler());
+		sortProvider.refresh();
+		sortProvider.getList().addAll(((ManageDraftMeasureModel) results).getDataList());
+		cellTable = addColumnToTable(cellTable);
+		sortProvider.addDataDisplay(cellTable);
+		CustomPager.Resources pagerResources = GWT.create(CustomPager.Resources.class);
+		MatSimplePager spager = new MatSimplePager(CustomPager.TextLocation.CENTER, pagerResources, false, 0, true);
+		spager.setPageStart(0);
+		spager.setDisplay(cellTable);
+		spager.setPageSize(PAGE_SIZE);
+		spager.setToolTipAndTabIndex(spager);
+		cellTable.setWidth("100%");
+		cellTable.setColumnWidth(0, 15.0, Unit.PCT);
+		cellTable.setColumnWidth(1, 63.0, Unit.PCT);
+		cellTable.setColumnWidth(2, 22.0, Unit.PCT);
+		com.google.gwt.dom.client.TableElement elem = cellTable.getElement().cast();
+		TableCaptionElement caption = elem.createCaption();
+		caption.appendChild(cellTablePanelHeader.getElement());
+		Label invisibleLabel = (Label) LabelBuilder
+				.buildInvisibleLabel(
+						"measureDraftSummary",
+						"In the following table Measure Name are given in the second column,"
+								+ "with the Version in the third column and options can be"
+								+ "selected from first column.");
+		cellTable.getElement().setAttribute("id", "measureDraftFromVersionCellTable");
+		cellTable.getElement().setAttribute("aria-describedby", "measureDraftSummary");
+		cellTablePanel.add(invisibleLabel);
+		cellTablePanel.add(cellTable);
+		cellTablePanel.add(new SpacerWidget());
+		cellTablePanel.add(spager);
+	}
 	/* (non-Javadoc)
 	 * @see mat.client.measure.ManageMeasurePresenter.DraftDisplay#getCancelButton()
 	 */
@@ -230,7 +186,6 @@ public class ManageMeasureDraftView implements ManageMeasurePresenter.DraftDispl
 	public HasClickHandlers getCancelButton() {
 		return buttonBar.getCancelButton();
 	}
-		
 	/* (non-Javadoc)
 	 * @see mat.client.measure.ManageMeasurePresenter.BaseDisplay#getErrorMessageDisplay()
 	 */
@@ -238,7 +193,6 @@ public class ManageMeasureDraftView implements ManageMeasurePresenter.DraftDispl
 	public ErrorMessageDisplayInterface getErrorMessageDisplay() {
 		return errorMessages;
 	}
-		
 	/**
 	 * Add Image on Button with invisible text. This text will be available when
 	 * css is turned off.
@@ -283,11 +237,37 @@ public class ManageMeasureDraftView implements ManageMeasurePresenter.DraftDispl
 		return searchWidget;
 	}
 	
+	/**
+	 * Gets the selected measure.
+	 *
+	 * @return the selected measure
+	 */
+	@Override
+	public Result getSelectedMeasure() {
+		return selectionModel.getSelectedObject();
+	}
+	
+	/**
+	 * Gets the selection model with handler.
+	 *
+	 * @return the selection model with handler
+	 */
+	private SingleSelectionModel<Result> getSelectionModelWithHandler() {
+		selectionModel = new SingleSelectionModel<Result>();
+		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+			@Override
+			public void onSelectionChange(SelectionChangeEvent event) {
+				getErrorMessageDisplay().clear();
+			}
+		});
+		return selectionModel;
+	}
+	
 	/* (non-Javadoc)
 	 * @see mat.client.measure.ManageMeasurePresenter.DraftDisplay#getZoomButton()
 	 */
 	@Override
 	public CustomButton getZoomButton() {
 		return zoomButton;
-	}	
+	}
 }

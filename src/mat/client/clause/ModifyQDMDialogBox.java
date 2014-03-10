09c4d96a8b2ca60088b5d24ff92acc334eb147f3
Jyoti Narang <JNarang@hcareis.com>
2014-03-10 19:28:43 +0000

    Added ID to DialogBox
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@5045 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: c3378450b626cff4a2864a5e6b29ca7f84ec47f3

--- src/mat/client/clause/ModifyQDMDialogBox.java
+++ src/mat/client/clause/ModifyQDMDialogBox.java
@@ -2,9 +2,7 @@ package mat.client.clause;
 
 import mat.client.shared.DialogBoxWithCloseButton;
 import mat.model.QualityDataSetDTO;
-
 import org.apache.commons.lang.StringUtils;
-
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
@@ -24,7 +22,7 @@ public class ModifyQDMDialogBox {
 	
 	/** The handler registration. */
 	static HandlerRegistration handlerRegistration;
-
+	
 	/**
 	 * Creates and displays Modify QDM dialog box (pop-up).
 	 * @param widget - Widget.
@@ -38,27 +36,27 @@ public class ModifyQDMDialogBox {
 			text = text.concat(" " + modifyValueSetDTO.getOccurrenceText() + " of ");
 		}
 		text = text.concat(modifyValueSetDTO.getCodeListName() + " : " + modifyValueSetDTO.getDataType()  + " )");
-
+		
 		dialogBox.setText(text);
-		dialogBox.getElement().setId("dialogBox_DialogBox");
+		dialogBox.getElement().setId("ModifyQDMDialogBox");
 		dialogBox.setTitle("Modify Applied QDM");
 		VerticalPanel dialogContents = new VerticalPanel();
 		dialogContents.getElement().setId("dialogContents_VerticalPanel");
 		dialogContents.setWidth("70em");
 		DOM.setStyleAttribute(dialogBox.getElement(), "width", "1000px");
 		DOM.setStyleAttribute(dialogBox.getElement(), "height", "1000px");
-
+		
 		dialogBox.setWidget(dialogContents);
-
+		
 		dialogContents.add(widget);
 		dialogContents.setCellHorizontalAlignment(widget, HasHorizontalAlignment.ALIGN_LEFT);
 		dialogBox.center();
 		dialogBox.show();
-
+		
 		if (handlerRegistration != null) {
 			handlerRegistration.removeHandler();
 		}
-
+		
 		handlerRegistration = dialogBox.addCloseHandler(new CloseHandler<PopupPanel>() {
 			@Override
 			public void onClose(final CloseEvent<PopupPanel> event) {

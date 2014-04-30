e8d0f4e82b86ea812b33cf49df380f680b01410b
Hari Yadav <hyadav@hcareis.com>
2014-04-30 18:56:28 +0000

    MAT-3883 Add New Clause Pop-Up Changes
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@5451 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 18a093dffecb9036072b2d2af1a7fa4611106b72

--- src/mat/client/clause/clauseworkspace/view/EditSubTreeDialogBox.java
+++ src/mat/client/clause/clauseworkspace/view/EditSubTreeDialogBox.java
@@ -4,8 +4,14 @@ import mat.client.clause.clauseworkspace.model.CellTreeNode;
 import mat.client.clause.clauseworkspace.presenter.XmlTreeDisplay;
 import mat.client.shared.LabelBuilder;
 import mat.shared.UUIDUtilClient;
+
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
@@ -14,10 +20,20 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class EditSubTreeDialogBox.
+ */
 public class EditSubTreeDialogBox {
-	/** The dialog box. */
-	public static DialogBox dialogBox = new DialogBox(true,true);
 	
+	/** The dialog box. */
+	public static DialogBox dialogBox = new DialogBox(false,true);
+	/**
+	 * Show edit dialog box.
+	 *
+	 * @param xmlTreeDisplay the xml tree display
+	 * @param cellTreeNode the cell tree node
+	 */
 	public static void showEditDialogBox(final XmlTreeDisplay xmlTreeDisplay, final CellTreeNode cellTreeNode) {
 		dialogBox.setGlassEnabled(true);
 		dialogBox.setAnimationEnabled(true);
@@ -35,11 +51,24 @@ public class EditSubTreeDialogBox {
 		subTreeName.getElement().setId("quantity_TextBox");
 		subTreeName.setWidth("150px");
 		subTreeName.setText(existingSubTreeName);
+		subTreeName.setCursorPos(subTreeName.getText().length());
 		Label lableNewSubTreeName = (Label) LabelBuilder.buildLabel(subTreeName, "New Clause Name");
 		dialogContents.add(lableNewSubTreeName);
 		dialogContents.setCellHorizontalAlignment(lableNewSubTreeName, HasHorizontalAlignment.ALIGN_LEFT);
 		dialogContents.add(subTreeName);
 		dialogContents.setCellHorizontalAlignment(subTreeName, HasHorizontalAlignment.ALIGN_LEFT);
+		//Add a Enter press event to the text box
+		subTreeName.addKeyPressHandler(new KeyPressHandler() {
+		@Override
+		 public void onKeyPress(KeyPressEvent event) { 
+		    if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ENTER) { 
+		    	dialogBox.hide();
+		    	xmlTreeDisplay.editNode(subTreeName.getText(), subTreeName.getText());
+				xmlTreeDisplay.setDirty(true); 
+				
+		    }
+		}
+		});
 		// Add a Save button at the bottom of the dialog
 		Button save = new Button("OK", new ClickHandler() {
 			@Override
@@ -47,6 +76,7 @@ public class EditSubTreeDialogBox {
 				dialogBox.hide();
 				xmlTreeDisplay.editNode(subTreeName.getText(), subTreeName.getText());
 				xmlTreeDisplay.setDirty(true);
+				
 			}
 		});
 		save.getElement().setId("save_Button");
@@ -66,12 +96,26 @@ public class EditSubTreeDialogBox {
 		buttonPanel.add(closeButton);
 		buttonPanel.setCellHorizontalAlignment(closeButton, HasHorizontalAlignment.ALIGN_RIGHT);
 		buttonPanel.getElement().setId("buttonPanel_HorizontalPanel");
-		
 		dialogContents.add(buttonPanel);
 		dialogBox.center();
 		dialogBox.show();
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+
+		    @Override
+		    public void execute() {
+		    	subTreeName.setCursorPos(subTreeName.getText().length());
+		    	subTreeName.setFocus(true);
+		    }
+		});
+		
 	}
 	
+	/**
+	 * Show add dialog box.
+	 *
+	 * @param xmlTreeDisplay the xml tree display
+	 * @param cellTreeNode the cell tree node
+	 */
 	public static void showAddDialogBox(final XmlTreeDisplay xmlTreeDisplay, final CellTreeNode cellTreeNode) {
 		dialogBox.setGlassEnabled(true);
 		dialogBox.setAnimationEnabled(true);
@@ -84,6 +128,7 @@ public class EditSubTreeDialogBox {
 		dialogContents.getElement().setId("dialogContents_VerticalPanel");
 		dialogBox.setWidget(dialogContents);
 		final TextBox subTreeName = new TextBox();
+		subTreeName.setEnabled(true);
 		subTreeName.getElement().setId("quantity_TextBox");
 		subTreeName.setWidth("150px");
 		Label lableNewSubTreeName = (Label) LabelBuilder.buildLabel(subTreeName, "New Clause Name");
@@ -91,15 +136,31 @@ public class EditSubTreeDialogBox {
 		dialogContents.setCellHorizontalAlignment(lableNewSubTreeName, HasHorizontalAlignment.ALIGN_LEFT);
 		dialogContents.add(subTreeName);
 		dialogContents.setCellHorizontalAlignment(subTreeName, HasHorizontalAlignment.ALIGN_LEFT);
-		// Add a Save button at the bottom of the dialog
+		//Add a Enter press event to the text box
+		subTreeName.addKeyPressHandler(new KeyPressHandler() {
+		@Override
+		 public void onKeyPress(KeyPressEvent event) { 
+		    if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ENTER) { 
+		    	dialogBox.hide();
+				xmlTreeDisplay.addNode(subTreeName.getText(), subTreeName.getText()
+						, UUIDUtilClient.uuid(), CellTreeNode.SUBTREE_NODE);
+				xmlTreeDisplay.setDirty(true); 
+				
+		    }
+		}
+		});
+		//Add a OK button at the bottom of the dialog
 		Button save = new Button("OK", new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
 				dialogBox.hide();
-				xmlTreeDisplay.addNode(subTreeName.getText(), subTreeName.getText()
+				xmlTreeDisplay.addNode(subTreeName.getText()
+						, subTreeName.getText()
 						, UUIDUtilClient.uuid(), CellTreeNode.SUBTREE_NODE);
 				xmlTreeDisplay.setDirty(true);
+				
 			}
+			
 		});
 		save.getElement().setId("save_Button");
 		// Add a Close button at the bottom of the dialog
@@ -118,9 +179,18 @@ public class EditSubTreeDialogBox {
 		buttonPanel.add(closeButton);
 		buttonPanel.setCellHorizontalAlignment(closeButton, HasHorizontalAlignment.ALIGN_RIGHT);
 		buttonPanel.getElement().setId("buttonPanel_HorizontalPanel");
-		
 		dialogContents.add(buttonPanel);
 		dialogBox.center();
 		dialogBox.show();
+		Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+
+		    @Override
+		    public void execute() {
+		    	subTreeName.setCursorPos(subTreeName.getText().length());
+		    	subTreeName.setFocus(true);
+		    }
+		});
+		
 	}
+	
 }

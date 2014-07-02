c2285196416d91e78e03c41a19968b9c166af671
Hari Yadav <hyadav@hcareis.com>
2014-07-02 23:06:28 +0000

    MAT-4381 fixed issue for Birthdate and expired
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@6169 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: cfac86afc68b25849c933551ddbd2251daf0ae4c

--- src/mat/server/clause/QDSAttributesServiceImpl.java
+++ src/mat/server/clause/QDSAttributesServiceImpl.java
@@ -1,8 +1,11 @@
 package mat.server.clause;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import mat.client.clause.QDSAttributesService;
 import mat.dao.DataTypeDAO;
@@ -87,5 +90,44 @@ public class QDSAttributesServiceImpl extends SpringRemoteServiceServlet impleme
 		} 
 		return checkIfDataTypeIsPresent;
 	}
+	
+	/* (non-Javadoc)
+	 * @see mat.client.clause.QDSAttributesService#getDatatypeList(java.util.List)
+	 */
+	@Override
+	public Map<String, List<String>> getDatatypeList(List<String> dataTypeList){
+		
+		DataTypeDAO dataTypeDAO = (DataTypeDAO)context.getBean("dataTypeDAO");
+		Map<String, List<String>> dataTypeListMap = new HashMap<String, List<String>>();
+		
+		for(String dataType: dataTypeList){
+			DataType qdmDataType = dataTypeDAO.findByDataTypeName(dataType);
+			if(qdmDataType!=null){
+			List<String> qdsAttributeList = getAllQDMAttributesbyDataType(dataType);
+			dataTypeListMap.put(dataType, qdsAttributeList);
+			}
+		}
+		
+		
+	
+		return dataTypeListMap;
+		
+	}
+	
+	/**
+	 * Gets the all qdm attributesby data type.
+	 *
+	 * @param dataType the data type
+	 * @return the all qdm attributesby data type
+	 */
+	private List<String> getAllQDMAttributesbyDataType(String dataType){
+		List<QDSAttributes> qdsAttributeList = getAllAttributesByDataType(dataType);
+		List<String> qdsAttributes = new ArrayList<String>();
+		for(QDSAttributes qdsAttribtue: qdsAttributeList){
+			qdsAttributes.add(qdsAttribtue.getName());
+		}
+		return qdsAttributes;
+	}
+	
 
 }

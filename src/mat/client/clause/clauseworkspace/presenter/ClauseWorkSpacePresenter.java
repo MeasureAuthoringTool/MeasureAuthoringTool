1cea20ff58064d8c17d640508a6794a9d1081490
Hari Yadav <hyadav@hcareis.com>
2014-09-09 14:10:48 +0000

    fix the AddClause Button Issue on clauseWorkSpace
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@6952 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: e62b6ba265f1457c812a0ad35c51d9faf623e937

--- src/mat/client/clause/clauseworkspace/presenter/ClauseWorkSpacePresenter.java
+++ src/mat/client/clause/clauseworkspace/presenter/ClauseWorkSpacePresenter.java
@@ -126,98 +126,121 @@ public class ClauseWorkSpacePresenter extends XmlTreePresenter implements MatPre
 	 *
 	 * @param xml            the new qdm elements map
 	 */
-	private void setQdmElementsAndSubTreeLookUpMap(final String xml) {	
+	private void setQdmElementsAndSubTreeLookUpMap(final String xml) {
 		PopulationWorkSpaceConstants.elementLookUpDataTypeName = new TreeMap<String, String>();
 		PopulationWorkSpaceConstants.elementLookUpName = new TreeMap<String, String>();
-		PopulationWorkSpaceConstants.elementLookUpNode = new TreeMap<String, Node>();
-		//PopulationWorkSpaceConstants.subTreeLookUpName = new LinkedHashMap<String, String>();
-		PopulationWorkSpaceConstants.subTreeLookUpNode = new LinkedHashMap<String, Node>();				
-		Document document = XMLParser.parse(xml);
-				
-		NodeList nodeList = document.getElementsByTagName("elementLookUp");	
+		PopulationWorkSpaceConstants.elementLookUpNode = new TreeMap<String, Node>();		
 		List<Entry<String, String>> sortedClauses = new LinkedList<Map.Entry<String, String>>(
 				PopulationWorkSpaceConstants.subTreeLookUpName.entrySet());
+		PopulationWorkSpaceConstants.subTreeLookUpNode = new LinkedHashMap<String, Node>();
+		Document document = XMLParser.parse(xml);
+
+		NodeList nodeList = document.getElementsByTagName("elementLookUp");		
 		if ((null != nodeList) && (nodeList.getLength() > 0)) {
 			NodeList qdms = nodeList.item(0).getChildNodes();
-			
-			
+
 			for (int i = 0; i < qdms.getLength(); i++) {
 				if ("qdm".equals(qdms.item(i).getNodeName())) {
-						NamedNodeMap namedNodeMap = qdms.item(i).getAttributes();
-						String isSupplementData = namedNodeMap.getNamedItem("suppDataElement").getNodeValue();
-						if (isSupplementData.equals("false")) { //filter supplementDataElements from elementLookUp
-								String name = namedNodeMap.getNamedItem("name").getNodeValue();
-								
-								// Prod Issue fixed : qdm name has trailing spaces which is reterived frm VSAC.
-								//So QDM attribute dialog box is throwing error in FF.
-								//To fix that spaces are removed from start and end.
-								name = name.trim();
-								String uuid = namedNodeMap.getNamedItem("uuid").getNodeValue();
-								//name = name.replaceAll("^\\s+|\\s+$", "");
-								if (namedNodeMap.getNamedItem("instance") != null) {
-									name = namedNodeMap.getNamedItem("instance").getNodeValue() + " of " + name;
-								}
-								if (namedNodeMap.getNamedItem("datatype") != null) {
-									String dataType = namedNodeMap.getNamedItem("datatype").getNodeValue().trim();
-									name = name + " : " + dataType;
-									PopulationWorkSpaceConstants.elementLookUpDataTypeName.put(uuid, dataType);
-								}
-								PopulationWorkSpaceConstants.elementLookUpNode.put(name + "~" + uuid, qdms.item(i));
-								PopulationWorkSpaceConstants.elementLookUpName.put(uuid, name);
-							}
+					NamedNodeMap namedNodeMap = qdms.item(i).getAttributes();
+					String isSupplementData = namedNodeMap.getNamedItem(
+							"suppDataElement").getNodeValue();
+					if (isSupplementData.equals("false")) { // filter
+															// supplementDataElements
+															// from
+															// elementLookUp
+						String name = namedNodeMap.getNamedItem("name")
+								.getNodeValue();
+
+						// Prod Issue fixed : qdm name has trailing spaces which
+						// is reterived frm VSAC.
+						// So QDM attribute dialog box is throwing error in FF.
+						// To fix that spaces are removed from start and end.
+						name = name.trim();
+						String uuid = namedNodeMap.getNamedItem("uuid")
+								.getNodeValue();
+						// name = name.replaceAll("^\\s+|\\s+$", "");
+						if (namedNodeMap.getNamedItem("instance") != null) {
+							name = namedNodeMap.getNamedItem("instance")
+									.getNodeValue() + " of " + name;
 						}
+						if (namedNodeMap.getNamedItem("datatype") != null) {
+							String dataType = namedNodeMap
+									.getNamedItem("datatype").getNodeValue()
+									.trim();
+							name = name + " : " + dataType;
+							PopulationWorkSpaceConstants.elementLookUpDataTypeName
+									.put(uuid, dataType);
+						}
+						PopulationWorkSpaceConstants.elementLookUpNode.put(name
+								+ "~" + uuid, qdms.item(i));
+						PopulationWorkSpaceConstants.elementLookUpName.put(
+								uuid, name);
 					}
 				}
-				
-				
-				NodeList subTreesNodeList = document.getElementsByTagName("subTreeLookUp");
-				if ((null != subTreesNodeList) && (subTreesNodeList.getLength() > 0)) {
-					NodeList subTree = subTreesNodeList.item(0).getChildNodes();
-					for (Entry<String, String> entry1 : sortedClauses) {
-					for (int i = 0; i < subTree.getLength(); i++) {
-						if ("subTree".equals(subTree.item(i).getNodeName())) {
-							NamedNodeMap namedNodeMap = subTree.item(i).getAttributes();
-							
-							String name = namedNodeMap.getNamedItem("displayName").getNodeValue();
-							/*if (namedNodeMap.getNamedItem("instance") != null) {
-								String occurrText = "Occurrence " + namedNodeMap.getNamedItem("instance").getNodeValue().toString() + " of";
-								name = occurrText + " " + name;
-							}*/
-							// SubTree name Might have trailing spaces.
-							name = name.trim();
-							//name = name.replaceAll("^\\s+|\\s+$", "");
-							String uuid = namedNodeMap.getNamedItem("uuid").getNodeValue();
-							if(uuid.equalsIgnoreCase(entry1.getKey())){
-								PopulationWorkSpaceConstants.subTreeLookUpNode.put(entry1.getValue() + "~" + entry1.getKey(), subTree.item(i));
-								break;
-							}
-							
-							
+			}
+		}
+
+		NodeList subTreesNodeList = document
+				.getElementsByTagName("subTreeLookUp");
+		if ((null != subTreesNodeList) && (subTreesNodeList.getLength() > 0)) {
+			NodeList subTree = subTreesNodeList.item(0).getChildNodes();
+			for (Entry<String, String> entry1 : sortedClauses) {
+				for (int i = 0; i < subTree.getLength(); i++) {
+					if ("subTree".equals(subTree.item(i).getNodeName())) {
+						NamedNodeMap namedNodeMap = subTree.item(i)
+								.getAttributes();
+
+						String name = namedNodeMap.getNamedItem("displayName")
+								.getNodeValue();
+						/*
+						 * if (namedNodeMap.getNamedItem("instance") != null) {
+						 * String occurrText = "Occurrence " +
+						 * namedNodeMap.getNamedItem
+						 * ("instance").getNodeValue().toString() + " of"; name
+						 * = occurrText + " " + name; }
+						 */
+						// SubTree name Might have trailing spaces.
+						name = name.trim();
+						// name = name.replaceAll("^\\s+|\\s+$", "");
+						String uuid = namedNodeMap.getNamedItem("uuid")
+								.getNodeValue();
+						if (uuid.equalsIgnoreCase(entry1.getKey())) {
+							PopulationWorkSpaceConstants.subTreeLookUpNode.put(
+									entry1.getValue() + "~" + entry1.getKey(),
+									subTree.item(i));
+							break;
 						}
+
 					}
-					}
-					
 				}
-				
-				System.out.println("PopulationWorkSpaceConstants.subTreeLookUpName: 576879809   "+PopulationWorkSpaceConstants.subTreeLookUpName);
+			}
+
+		}
+
+		System.out
+				.println("PopulationWorkSpaceConstants.subTreeLookUpName: 576879809   "
+						+ PopulationWorkSpaceConstants.subTreeLookUpName);
 		List<String> dataTypeList = new ArrayList<String>();
-		dataTypeList.addAll(PopulationWorkSpaceConstants.getElementLookUpDataTypeName().values());
-		attributeService.getDatatypeList(dataTypeList, new AsyncCallback<Map<String, List<String>>>() {
-			
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-				
-			}
-			@Override
-			public void onSuccess(Map<String, List<String>> datatypeMap) {
-				System.out.println("Data type map:" + datatypeMap);
-				PopulationWorkSpaceConstants.setDatatypeMap(datatypeMap);
-				
-			}
-		});
-		
-		
+		dataTypeList.addAll(PopulationWorkSpaceConstants
+				.getElementLookUpDataTypeName().values());
+		attributeService.getDatatypeList(dataTypeList,
+				new AsyncCallback<Map<String, List<String>>>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onSuccess(Map<String, List<String>> datatypeMap) {
+						System.out.println("Data type map:" + datatypeMap);
+						PopulationWorkSpaceConstants
+								.setDatatypeMap(datatypeMap);
+
+					}
+				});
+
 	}
 	
 	/* (non-Javadoc)

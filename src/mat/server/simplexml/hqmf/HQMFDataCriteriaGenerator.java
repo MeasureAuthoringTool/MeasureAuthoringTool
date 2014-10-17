bbf87ace58474ee156535108b0a6ac4359abf3cd
Chinmay Bajikar <CBajikar@telligen.com>
2014-10-17 20:25:50 +0000

    When we have a negation rationale attribute configured with a "Check if Present", the HQMF generator throws an exception. Fixed.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@7379 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 2eda6beacdcfe4d7be994c5ab8e562a5fc799100

--- src/mat/server/simplexml/hqmf/HQMFDataCriteriaGenerator.java
+++ src/mat/server/simplexml/hqmf/HQMFDataCriteriaGenerator.java
@@ -2,6 +2,7 @@ package mat.server.simplexml.hqmf;
 
 import java.io.StringWriter;
 import java.io.Writer;
+
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
@@ -11,9 +12,11 @@ import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPathExpressionException;
+
 import mat.model.clause.MeasureExport;
 import mat.server.util.XmlProcessor;
 import mat.shared.UUIDUtilClient;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -348,7 +351,7 @@ public class HQMFDataCriteriaGenerator implements Generator {
 	private void generateCheckIfPresentAttribEntries(
 			XmlProcessor dataCriteriaXMLProcessor,
 			XmlProcessor simpleXmlprocessor) throws XPathExpressionException {
-		String xPathForAttributeUse = "/measure/subTreeLookUp/subTree//elementRef/attribute[@mode = 'Check if Present']";
+		String xPathForAttributeUse = "/measure/subTreeLookUp/subTree//elementRef/attribute[@mode = 'Check if Present'][@name != 'negation rationale']";
 		NodeList usedAttributeNodeList = simpleXmlprocessor.findNodeList(simpleXmlprocessor.getOriginalDoc(), xPathForAttributeUse);
 		
 		if(usedAttributeNodeList == null){

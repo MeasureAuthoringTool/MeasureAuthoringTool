d86da5019ef8d8c45f507b494ff90266faaad440
Hari Yadav <hyadav@hcareis.com>
2014-12-04 23:24:21 +0000

    Added method getHumanReadable()
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@7887 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: ac920f8813d4913bf8f96fa761db97a1263ad78c

--- src/mat/server/simplexml/hqmf/HQMFGenerator.java
+++ src/mat/server/simplexml/hqmf/HQMFGenerator.java
@@ -1,6 +1,13 @@
 package mat.server.simplexml.hqmf;
 
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.Node;
+
+
 import mat.model.clause.MeasureExport;
+import mat.server.simplexml.HumanReadableGenerator;
+import mat.server.util.XmlProcessor;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -24,11 +31,44 @@ public class HQMFGenerator implements Generator {
 			
 			String dataCriteriaXML = new HQMFDataCriteriaGenerator().generate(me);
 			hqmfXML= appendToHQMF(dataCriteriaXML, hqmfXML);
+			getHumanReadable(me);
+			
 		} catch(Exception e){
 			LOG.error("Unable to generate human readable. Exception Stack Strace is as followed : ");
 			e.printStackTrace();
 		}
 		return hqmfXML;
+	}
+
+	private void getHumanReadable(MeasureExport me) {
+		HQMFDataCriteriaElementGenerator hQMFDataElementGenerator= new HQMFDataCriteriaElementGenerator();
+		String humanReadableHTML = HumanReadableGenerator.generateHTMLForMeasure(me.getMeasure().getId(), me.getSimpleXML());
+		//humanReadableHTML = humanReadableHTML.substring(humanReadableHTML.indexOf("<style type="),humanReadableHTML.indexOf("</style>")+"</style>".length());
+		humanReadableHTML = humanReadableHTML.substring(humanReadableHTML.indexOf(" <body>"),humanReadableHTML.indexOf("</body>")+"</body>".length());		
+		XmlProcessor humanReadableProcessor = new XmlProcessor(humanReadableHTML);
+		me.setHumanReadableProcessor(humanReadableProcessor);	
+		try{
+			XmlProcessor processor = me.getHumanReadableProcessor();
+			
+			Node mainNode = me.getHumanReadableProcessor().findNode(
+					processor.getOriginalDoc(),
+					"/body");
+			hQMFDataElementGenerator.clean(mainNode);
+		Node qDMDataElementsNode = me.getHumanReadableProcessor().findNode(
+				processor.getOriginalDoc(),
+				"/body/h3[a[text()='Data criteria (QDM Data Elements)']]");
+		
+		Node childNode = qDMDataElementsNode.getNextSibling();
+		Node duplicateNode = childNode.cloneNode(true);
+		childNode.getParentNode().appendChild(duplicateNode);
+		me.setHumanReadableProcessor(processor);		
+		
+		}  catch (XPathExpressionException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 	private String appendToHQMF(String dataCriteriaXML, String hqmfXML) {

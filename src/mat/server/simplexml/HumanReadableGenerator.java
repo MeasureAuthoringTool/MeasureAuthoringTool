09a1b57c60e28476f9472edaf8c6b8c8e4095893
Ravi Tadepally <rtadepally@hcareis.com>
2014-12-09 20:51:22 +0000

    Fixed MAT-5280 Satisfies All/Any issue in Human Readable Initializing List<String> lhsID at the time of generatingHTMLForMeasure
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@7953 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 64800b326023511bfc2c3311820fa946d8220502

--- src/mat/server/simplexml/HumanReadableGenerator.java
+++ src/mat/server/simplexml/HumanReadableGenerator.java
@@ -1502,7 +1502,7 @@ public class HumanReadableGenerator {
 			org.jsoup.nodes.Document humanReadableHTMLDocument = HeaderHumanReadableGenerator
 					.generateHeaderHTMLForMeasure(simpleXmlStr);
 			XmlProcessor simpleXMLProcessor = resolveSubTreesInPopulations(simpleXmlStr);
-			
+			lhsID = new ArrayList<String>();
 			if (simpleXMLProcessor == null) {
 				org.jsoup.nodes.Document htmlDocument = createBaseHumanReadableDocument();
 				Element bodyElement = htmlDocument.body();

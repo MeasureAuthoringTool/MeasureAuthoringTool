bd2fcb342fa4820ac8488871e29285302340c10b
Chinmay Bajikar <CBajikar@telligen.com>
2015-01-28 22:21:02 +0000

    Re-factored the code for normalizing clause names. made the needed changes for this junit test.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@8421 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: baa785077b3a34818cdb62bbd0ed4e83c36446d3

--- test/mat/server/service/impl/MeasureLibraryServiceImpSpaceNormalizationlTest.java
+++ test/mat/server/service/impl/MeasureLibraryServiceImpSpaceNormalizationlTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import javax.xml.xpath.XPathExpressionException;
 
 import mat.client.clause.clauseworkspace.model.MeasureXmlModel;
-import mat.server.MeasureLibraryServiceImpl;
+import mat.server.util.XmlProcessor;
 
 import org.junit.Test;
 
@@ -15,15 +15,13 @@ public class MeasureLibraryServiceImpSpaceNormalizationlTest {
 	@Test
 	public void test1() {
 		MeasureXmlModel measureXmlModel = new MeasureXmlModel();
-		MeasureLibraryServiceImpl measureLibraryServiceImpl = new MeasureLibraryServiceImpl();
 		measureXmlModel.setXml("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?><subTree displayName=\"C a          d          e       t\" uuid=\"348C1486-CA35-44B9-AE8A-4E3B7AC32091\" qdmVariable=\"false\"/>");
-		String clauseName = "Sp a c e s";
 		try {
-			String someXml = measureLibraryServiceImpl.clauseNameNormalizeSpaces(measureXmlModel, clauseName);
+			String someXml = XmlProcessor.normalizeNodeForSpaces(measureXmlModel.getXml(), "//subTree/@displayName");
 			measureXmlModel.setXml(someXml);
-			assertTrue(measureXmlModel.getXml().contains("displayName=\"Sp a c e s\""));
+			assertTrue(measureXmlModel.getXml().contains("displayName=\"C a d e t\""));
 			
-		} catch (XPathExpressionException e) {
+		} catch (XPathExpressionException e) { 
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -32,11 +30,9 @@ public class MeasureLibraryServiceImpSpaceNormalizationlTest {
 	@Test
 	public void test2() {
 		MeasureXmlModel measureXmlModel = new MeasureXmlModel();
-		MeasureLibraryServiceImpl measureLibraryServiceImpl = new MeasureLibraryServiceImpl();
 		measureXmlModel.setXml("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?><subTree displayName=\"C a          d          e       t\" uuid=\"348C1486-CA35-44B9-AE8A-4E3B7AC32091\" qdmVariable=\"false\"/>");
-		String clauseName = "C a d e t";
 		try {
-			String someXml = measureLibraryServiceImpl.clauseNameNormalizeSpaces(measureXmlModel, clauseName);
+			String someXml = XmlProcessor.normalizeNodeForSpaces(measureXmlModel.getXml(), "//subTree/@displayName");
 			measureXmlModel.setXml(someXml);
 			assertTrue(measureXmlModel.getXml().contains("displayName=\"C a d e t\""));
 			

982e22af21d29a4a95dcf7bc7597c42f6a918323
Ravi Tadepally <rtadepally@hcareis.com>
2015-03-10 14:55:37 +0000

    Added VSACVersionMapping.xml for castor Mapping
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@8718 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: bb30ca54230dc15db8f6a68298fe58167383fc8d

---
+++ src/mat/model/VSACVersion.java
@@ -0,0 +1,33 @@
+package mat.model;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class VSACVersion.
+ */
+public class VSACVersion  implements IsSerializable {
+
+	/** The name. */
+	private String name;
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}

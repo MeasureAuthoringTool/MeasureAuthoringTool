a05b8e8d587098899e8eaa6b6cb8d8977272ac2d
Jyoti Narang <JNarang@hcareis.com>
2015-03-27 16:15:08 +0000

    Added MatConstants
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@8848 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 01b8db600a12b129bdfca0ecf75a559dc2283114

---
+++ src/mat/shared/MatConstants.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package mat.shared;
+
+/**
+ * This holds constants used across MAT.
+ * These constants are available on client as well as server side.
+ */
+public interface MatConstants {
+}

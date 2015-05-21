3303e75e9dc784ff2246860948427be0c3536dfd
Jyoti Narang <JNarang@hcareis.com>
2015-05-21 15:15:01 +0000

    Default Expansion Identifier is picked from MAT.Properties using Spring Bean.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@9263 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: a29905f13d56eca05772f2e1dd1b8ab1d0b97b1b

---
+++ src/mat/server/service/VSACApiService.java
@@ -0,0 +1,28 @@
+package mat.server.service;
+
+import mat.client.umls.service.VsacApiResult;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface MeasureLibraryService.
+ */
+public interface VSACApiService {
+	void inValidateVsacUser(String sessionId);
+	
+	boolean isAlreadySignedIn(String sessionId);
+	
+	VsacApiResult getAllVersionListByOID(String oid, String sessionId);
+	
+	VsacApiResult updateVSACValueSets(String measureId, String defaultExpId, String sessionId);
+	
+	boolean validateVsacUser(String userName, String password, String sessionId);
+	
+	VsacApiResult getMostRecentValueSetByOID(String oid, String expansionId, String sessionId);
+	
+	VsacApiResult getValueSetByOIDAndVersionOrExpansionId(String oid, String version, String expansionId, String sessionId);
+	
+	VsacApiResult updateAllVSACValueSetsAtPackage(String measureId, String sessionId);
+	
+	VsacApiResult getAllExpIdentifierList(String sessionId);
+}

12744d5b2a7603fb51b23602f3b5234bfd392883
Janet Landry <jlandry@hcareis.com>
2015-09-15 16:52:24 +0000

    Take out all message changes until they decide all the changes they want to make.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@10095 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: b93db36797be61aa291b126c269f3df457d0678b

--- src/mat/client/shared/MessageDelegate.java
+++ src/mat/client/shared/MessageDelegate.java
@@ -238,8 +238,8 @@ public class MessageDelegate {
 	/** The lhs rhs required. */
 	private final String LHS_RHS_REQUIRED = " LHS and RHS are required for Timings, Relationships and Satisfies functions.";
 	
-	/** The at least one child required. */
-	private final String ATLEAST_ONE_CHILD_REQUIRED = "Measure logic is incorrect. Functions must contain at least one child node.";
+	/** The atleast one child required. */
+	private final String ATLEAST_ONE_CHILD_REQUIRED = " Functions must contain at least one child node.";
 	
 	/** The at least three children are required. */
 	private final String AT_LEAST_TWO_CHILDREN_REQUIRED = "Union, Intersection, and Datetimediff must contain at least two or more child nodes.";

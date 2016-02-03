7076dc11c5da17aa6691cdc0766a2a1ec8fc748a
Ravi Tadepally <rtadepally@hcareis.com>
2016-02-03 16:06:49 +0000

    Merge Code for CQL- Added Liquibase script for CQLDATA Table and added DAO changes for CQL.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@11176 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: dd19c7c9b73c0267e774c5a84c69bdc824533962

---
+++ src/mat/dao/clause/CQLDAO.java
@@ -0,0 +1,10 @@
+package mat.dao.clause;
+
+import mat.dao.IDAO;
+import mat.model.clause.CQLData;
+
+public interface CQLDAO extends IDAO<CQLData, String>{
+
+	CQLData findForCQL(String measureId);
+
+}

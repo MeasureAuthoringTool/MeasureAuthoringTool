a17768b667f208f8b314d0535ab39205718eb98a
Jyoti Narang <JNarang@hcareis.com>
2016-02-24 20:33:05 +0000

    MAT 6762 : Function UI - Added Add Argument Modal.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@11456 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8c1c224289fc3c39cccec49449a2ff1c026a71d4

--- src/mat/client/clause/cqlworkspace/AddFunctionArgumentDialogBox.java
+++ src/mat/client/clause/cqlworkspace/AddFunctionArgumentDialogBox.java
@@ -1,86 +1,139 @@
 package mat.client.clause.cqlworkspace;
 
 import java.util.List;
-import mat.client.shared.LabelBuilder;
 import mat.client.shared.ListBoxMVP;
 import mat.model.cql.CQLGrammarDataType;
 import mat.model.cql.CQLModel;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import org.gwtbootstrap3.client.ui.Button;
+import org.gwtbootstrap3.client.ui.ButtonToolBar;
+import org.gwtbootstrap3.client.ui.Column;
+import org.gwtbootstrap3.client.ui.Container;
+import org.gwtbootstrap3.client.ui.Label;
+import org.gwtbootstrap3.client.ui.Modal;
+import org.gwtbootstrap3.client.ui.ModalBody;
+import org.gwtbootstrap3.client.ui.ModalFooter;
+import org.gwtbootstrap3.client.ui.ModalSize;
+import org.gwtbootstrap3.client.ui.Row;
+import org.gwtbootstrap3.client.ui.TextArea;
+import org.gwtbootstrap3.client.ui.constants.ButtonDismiss;
+import org.gwtbootstrap3.client.ui.constants.ButtonSize;
+import org.gwtbootstrap3.client.ui.constants.ButtonType;
+import org.gwtbootstrap3.client.ui.constants.ColumnSize;
+import org.gwtbootstrap3.client.ui.constants.LabelType;
+import org.gwtbootstrap3.client.ui.constants.ModalBackdrop;
+
+
 
 public class AddFunctionArgumentDialogBox {
-	
-	public static DialogBox dialogBox = new DialogBox(true,false);
 	private static List<String> argumentDataTypeList = CQLGrammarDataType.getDataTypeName();
 	
-	public static void showComparisonDialogBox(CQLModel currentModel){
+	public static  void showComparisonDialogBox(CQLModel currentModel){
 		
-		dialogBox.setGlassEnabled(true);
-		dialogBox.setAnimationEnabled(true);
-		dialogBox.setText("Add Argument");
-		dialogBox.setTitle("Add Argument");
-		dialogBox.getElement().setAttribute("id", "AddArgumentDialogBox");
-		final VerticalPanel dialogContents = new VerticalPanel();
-		dialogBox.setWidget(dialogContents);
-		dialogContents.clear();
-		dialogContents.setWidth("21em");
-		dialogContents.setSpacing(5);
-		dialogContents.getElement().setId("dialogContents_VerticalPanel");
+		Modal dialogModal = new Modal();
+		dialogModal.setTitle("Argument");
+		dialogModal.setClosable(true);
+		dialogModal.setFade(true);
+		dialogModal.setDataBackdrop(ModalBackdrop.STATIC);
+		dialogModal.setDataKeyboard(true);
+		dialogModal.setId("AddEditArgument_Modal");
+		dialogModal.setSize(ModalSize.SMALL);
 		
-		final ListBoxMVP listAllDataTypes = new ListBoxMVP();
+		ModalBody modalBody = new ModalBody();
+		modalBody.setHeight("250px");
+		Container modalContainer = new Container();
+		Row rowOne = new Row();
+		Row rowTwo = new Row();
+		Row rowThird = new Row();
+		Row rowFourth = new Row();
+		Row rowFifth = new Row();
+		Row rowSixth = new Row();
+		Column rowOneColOne = new Column(ColumnSize.SM_5);
+		Column rowTwoColOne = new Column(ColumnSize.SM_5);
+		Column rowThirdColOne = new Column(ColumnSize.SM_5);
+		Column rowFourthColOne = new Column(ColumnSize.SM_5);
+		Column rowFifthColOne = new Column(ColumnSize.SM_5);
+		Column rowSixthColOne = new Column(ColumnSize.SM_5);
+		
+		Label labelDataTypeLabel = new Label(LabelType.INFO,"Available Datatypes");
+		labelDataTypeLabel.setId("Available_Datatypes");
+		labelDataTypeLabel.setText("Available Datatypes");
+		labelDataTypeLabel.getElement().setAttribute("style", "font-size:75%;");
+		
+		ListBoxMVP listAllDataTypes = new ListBoxMVP();
+		listAllDataTypes.setWidth("150px");
+		listAllDataTypes.addItem("-- Select--");
 		for (int i = 0; i < argumentDataTypeList.size(); i++) {
 			listAllDataTypes.addItem(argumentDataTypeList.get(i));
 		}
 		
-		final HorizontalPanel hPanel = new HorizontalPanel();
-		hPanel.clear();
-		dialogContents.add(hPanel);
 		
-		Label labelListBoxTimingOrFunction = (Label) LabelBuilder.buildLabel(
-				listAllDataTypes, "Available Datatype");
-		dialogContents.add(labelListBoxTimingOrFunction);
-		dialogContents.setCellHorizontalAlignment(labelListBoxTimingOrFunction,
-				HasHorizontalAlignment.ALIGN_LEFT);
-		dialogContents.add(listAllDataTypes);
-		dialogContents.setCellHorizontalAlignment(listAllDataTypes,
-				HasHorizontalAlignment.ALIGN_LEFT);
+		Label labelArgumentName = new Label(LabelType.INFO,"Argument Name");
+		labelArgumentName.setText("Argument Name");
+		labelArgumentName.getElement().setAttribute("style", "font-size:75%;");
 		
-		Button save = new Button("OK", new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				hPanel.clear();
-				dialogContents.clear();
-				dialogBox.hide();
-			}
-		});
+		TextArea argumentNameTextArea = new TextArea();
+		argumentNameTextArea.setPlaceholder("Enter Argument Name");
+		argumentNameTextArea.getElement().setAttribute("style","width:250px;height:25px");
 		
-		Button closeButton = new Button("Cancel", new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				dialogContents.clear();
-				dialogBox.hide();
-				
-			}
-		});
-		closeButton.getElement().setId("closeButton_Button");
-		HorizontalPanel buttonPanel = new HorizontalPanel();
-		buttonPanel.setSpacing(10);
-		buttonPanel.add(save);
-		buttonPanel.setCellHorizontalAlignment(save,
-				HasHorizontalAlignment.ALIGN_RIGHT);
-		buttonPanel.add(closeButton);
-		buttonPanel.setCellHorizontalAlignment(closeButton,
-				HasHorizontalAlignment.ALIGN_RIGHT);
-		buttonPanel.getElement().setId("buttonPanel_HorizontalPanel");
-		dialogContents.add(buttonPanel);
-		dialogBox.center();
-		dialogBox.show();
+		Label labelSelectItem = new Label(LabelType.INFO, "Select Item");
+		labelSelectItem.setText("Select Item");
+		labelSelectItem.getElement().setAttribute("style", "font-size:75%;");
+		
+		ListBoxMVP listSelectItem = new ListBoxMVP();
+		listSelectItem.setWidth("150px");
+		listSelectItem.setEnabled(false);
+		
+		rowOneColOne.add(labelDataTypeLabel);
+		rowTwoColOne.add(listAllDataTypes);
+		rowThirdColOne.add(labelArgumentName);
+		rowFourthColOne.add(argumentNameTextArea);
+		rowFifthColOne.add(labelSelectItem);
+		rowSixthColOne.add(listSelectItem);
+		
+		rowOne.add(rowOneColOne);
+		rowTwo.add(rowTwoColOne);
+		rowTwo.setMarginTop(15.00);
+		rowThird.add(rowThirdColOne);
+		rowThird.setMarginTop(15.00);
+		rowFourth.add(rowFourthColOne);
+		rowFourth.setMarginTop(15.00);
+		rowFifth.add(rowFifthColOne);
+		rowFifth.setMarginTop(15.00);
+		rowSixth.add(rowSixthColOne);
+		rowSixth.setMarginTop(15.00);
+		rowSixth.setMarginBottom(15.00);
+		
+		modalContainer.add(rowOne);
+		modalContainer.add(rowTwo);
+		modalContainer.add(rowThird);
+		modalContainer.add(rowFourth);
+		modalContainer.add(rowFifth);
+		modalContainer.add(rowSixth);
+		
+		modalBody.add(modalContainer);
+		ModalFooter modalFooter = new ModalFooter();
+		
+		ButtonToolBar buttonToolBar = new ButtonToolBar();
+		Button addButton = new Button();
+		addButton.setText("Add");
+		addButton.setTitle("Add");
+		addButton.setType(ButtonType.PRIMARY);
+		addButton.setSize(ButtonSize.SMALL);
+		
+		Button closeButton = new Button();
+		closeButton.setText("Close");
+		closeButton.setTitle("Close");
+		closeButton.setType(ButtonType.DANGER);
+		closeButton.setSize(ButtonSize.SMALL);
+		closeButton.setDataDismiss(ButtonDismiss.MODAL);
+		buttonToolBar.add(addButton);
+		buttonToolBar.add(closeButton);
+		modalFooter.add(buttonToolBar);
+		dialogModal.add(modalBody);
+		dialogModal.add(modalFooter);
+		
+		dialogModal.show();
+		
 	}
 	
 }

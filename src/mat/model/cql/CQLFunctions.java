f2cea08725d67b8f3bc0ba04ac7106278c6fc9a5
Ravi Tadepally <rtadepally@hcareis.com>
2016-02-26 23:20:41 +0000

    Trimming out White spaces for FuntionName at the time of set and get functionality as we do for Parameters and Definitions
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@11523 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 45e5df3fce0eb7752acb6dd04ca2b6d708118638

--- src/mat/model/cql/CQLFunctions.java
+++ src/mat/model/cql/CQLFunctions.java
@@ -3,39 +3,113 @@ package mat.model.cql;
 import java.util.List;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CQLFunctions.
+ */
 public class CQLFunctions implements IsSerializable {
+	
+	/** The id. */
 	private String id;
+	
+	/** The function name. */
 	private String functionName;
+	
+	/** The function logic. */
 	private String functionLogic;
+	
+	/** The argument. */
 	private List<CQLFunctionArgument> argument;
+	
+	/** The context. */
 	private String context;
+	
+	/**
+	 * Gets the function name.
+	 *
+	 * @return the function name
+	 */
 	public String getFunctionName() {
-		return functionName;
+		return functionName.replaceAll(" ", "").trim();
 	}
+	
+	/**
+	 * Sets the function name.
+	 *
+	 * @param functionName the new function name
+	 */
 	public void setFunctionName(String functionName) {
-		this.functionName = functionName;
+		this.functionName = functionName.replaceAll(" ", "").trim();
 	}
+	
+	/**
+	 * Gets the function logic.
+	 *
+	 * @return the function logic
+	 */
 	public String getFunctionLogic() {
 		return functionLogic;
 	}
+	
+	/**
+	 * Sets the function logic.
+	 *
+	 * @param functionLogic the new function logic
+	 */
 	public void setFunctionLogic(String functionLogic) {
 		this.functionLogic = functionLogic;
 	}
+	
+	/**
+	 * Gets the argument list.
+	 *
+	 * @return the argument list
+	 */
 	public List<CQLFunctionArgument> getArgumentList() {
 		return argument;
 	}
+	
+	/**
+	 * Sets the argument list.
+	 *
+	 * @param argumentList the new argument list
+	 */
 	public void setArgumentList(List<CQLFunctionArgument> argumentList) {
 		this.argument = argumentList;
 	}
+	
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public String getId() {
 		return id;
 	}
+	
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
+	
+	/**
+	 * Gets the context.
+	 *
+	 * @return the context
+	 */
 	public String getContext() {
 		return context;
 	}
+	
+	/**
+	 * Sets the context.
+	 *
+	 * @param context the new context
+	 */
 	public void setContext(String context) {
 		this.context = context;
 	}

7906de36dc62738a496b286ce8e7c73b25b0dfa8
Jyoti Narang <JNarang@hcareis.com>
2016-02-29 16:04:06 +0000

    Added attribute Name for Model Object data type.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@11524 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8876340cc7471366f9079c7fee5cd1dac4ec15db

--- src/mat/model/cql/CQLFunctionArgument.java
+++ src/mat/model/cql/CQLFunctionArgument.java
@@ -6,6 +6,7 @@ public class CQLFunctionArgument implements IsSerializable {
 	private String id;
 	private String argumentName;
 	private String argumentType;
+	private String attributeName;
 	
 	public String getArgumentName() {
 		return argumentName;
@@ -24,5 +25,11 @@ public class CQLFunctionArgument implements IsSerializable {
 	}
 	public void setId(String id) {
 		this.id = id;
+	}
+	public String getAttributeName() {
+		return attributeName;
+	}
+	public void setAttributeName(String attributeName) {
+		this.attributeName = attributeName;
 	}
 }

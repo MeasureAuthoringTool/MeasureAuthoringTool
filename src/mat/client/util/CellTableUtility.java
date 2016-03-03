5b3e7044964be9f0313233344f51fb7c95e5624c
Ravi Tadepally <rtadepally@hcareis.com>
2016-03-03 20:01:43 +0000

    Added fix to escape specials characters like "<" and ">" inside html tag inside CellTable.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@11580 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 99a7a21d530ea873759ee22ca675273886677493

--- src/mat/client/util/CellTableUtility.java
+++ src/mat/client/util/CellTableUtility.java
@@ -1,14 +1,34 @@
 package mat.client.util;
 
+import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CellTableUtility.
+ */
 public class CellTableUtility {
+	
+	 /** The Constant AMP_RE. */
+ 	private static final RegExp AMP_RE = RegExp.compile("&", "g");
+	  
+  	/** The Constant GT_RE. */
+  	private static final RegExp GT_RE = RegExp.compile(">", "g");
+	  
+  	/** The Constant LT_RE. */
+  	private static final RegExp LT_RE = RegExp.compile("<", "g");
+	  
+  	/** The Constant SQUOT_RE. */
+  	private static final RegExp SQUOT_RE = RegExp.compile("\'", "g");
+	  
+  	/** The Constant QUOT_RE. */
+  	private static final RegExp QUOT_RE = RegExp.compile("\"", "g");
 	/** Gets the column tool tip.
 	 * @param title the title
 	 * @return the column tool tip */
 	public static SafeHtml getColumnToolTip(String title) {
-		String htmlConstant = "<html>" + "<head> </head> <Body><span title=\" " + title + "\">" + title + "</span></body>" + "</html>";
+		String htmlConstant = "<html>" + "<head> </head> <body><span title=\" " + title + "\">" + title + "</span></body>" + "</html>";
 		return new SafeHtmlBuilder().appendHtmlConstant(htmlConstant).toSafeHtml();
 	}
 	/** Gets the column tool tip.
@@ -16,9 +36,36 @@ public class CellTableUtility {
 	 * @param title the title
 	 * @return the column tool tip */
 	public static SafeHtml getColumnToolTip(String columnText, String title) {
-		String htmlConstant = "<html>" + "<head> </head> <Body><span tabIndex = \"0\" title=\" " + title + "\">" + columnText
+		String htmlConstant = "<html>" + "<head> </head> <body><span tabIndex = \"0\" title=\" " + title + "\">" + columnText
 				+ "</span></body>"
 				+ "</html>";
 		return new SafeHtmlBuilder().appendHtmlConstant(htmlConstant).toSafeHtml();
 	}
+	
+	
+	/**
+	 * Html escape method to escape special characters like 
+	 * "&", "<", ">", "/", "\", "'" inside Html tag.
+	 *
+	 * @param s the s
+	 * @return the string
+	 */
+	public static String htmlEscape(String s) {
+	    if (s.indexOf("&") != -1) {
+	      s = AMP_RE.replace(s, "&amp;");
+	    }
+	    if (s.indexOf("<") != -1) {
+	      s = LT_RE.replace(s, "&lt;");
+	    }
+	    if (s.indexOf(">") != -1) {
+	      s = GT_RE.replace(s, "&gt;");
+	    }
+	    if (s.indexOf("\"") != -1) {
+	      s = QUOT_RE.replace(s, "&quot;");
+	    }
+	    if (s.indexOf("'") != -1) {
+	      s = SQUOT_RE.replace(s, "&#39;");
+	    }
+	    return s;
+	  }
 }

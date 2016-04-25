a3fb85b565bdb3432090ff283629c93b18ec6345
Chinmay Bajikar <CBajikar@telligen.com>
2016-04-25 00:24:26 +0000

    MAT-6927 : CQL Translate HQMF and CQL file into a Human Readable when viewed in the Population Workspace. Removed unwanted code.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@11954 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 95dcbcf1ba1591856691cb82d8b3e00c7db176fe

--- src/mat/server/cqlparser/MATCQLParser.java
+++ src/mat/server/cqlparser/MATCQLParser.java
@@ -2,7 +2,6 @@ package mat.server.cqlparser;
 
 import mat.model.cql.parser.CQLFileObject;
 
-import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -27,42 +26,5 @@ public class MATCQLParser {
     	
     	return matcqlListener.getCqlFileObject();
 	}
-		
-    public static void main(String[] args) throws Exception {
-    	
-    	//cqlLexer lexer = new cqlLexer(new ANTLRFileStream("CMS146v2_QDM.cql")); 
-    	cqlLexer lexer = new cqlLexer(new ANTLRFileStream("test_valueset_define.cql"));
-    	//cqlLexer lexer = new cqlLexer(new ANTLRInputStream(""));
-    	CommonTokenStream tokens = new CommonTokenStream(lexer);
-    	CQLErrorListener cqlErrorListener = new CQLErrorListener();
-    	cqlParser parser = new cqlParser(tokens);
-    	parser.setBuildParseTree(true);
-    	parser.addErrorListener(cqlErrorListener);
-    	parser.addParseListener(new MATCQLListener(new MATCQLParser()));
-    	//ParseTree parseTree = parser.logic();
-    	ParserRuleContext tree = parser.logic();
-    	//int childCount = tree.getChildCount();
-//    	for(int i=0;i<childCount;i++){
-//    		System.out.println(tree.getChild(i).getPayload().getClass().getName());
-//    	}
-    	//System.out.println("child count:"+childCount);
-    	tree.inspect(parser);
-    	
-//    	cqlBaseVisitor<String> visitor = new cqlBaseVisitor<String>();
-//    	visitor.visit(parseTree);
-//    	
-    	System.out.println(parser.getNumberOfSyntaxErrors());
-    	System.out.println(cqlErrorListener.getErrors());
-    	
-    	//ExpressionDefinitionContext expressionDefinitionContext = parser.expressionDefinition();
-    	//expressionDefinitionContext.children.get(0).getText();
-    	
-//    	LibraryDefinitionContext libraryDefinitionContext = parser.libraryDefinition();
-//    	String text = libraryDefinitionContext.children.get(0).getText();
-//    	System.out.println(text);
-    	
-    	
-    }
-	
-	
+
 }

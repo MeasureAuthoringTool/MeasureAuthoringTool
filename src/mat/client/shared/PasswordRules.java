cca18b0ed4ad406415e771f3874bc520a0d26c23
Ravi Tadepally <rtadepally@hcareis.com>
2016-05-05 15:57:38 +0000

    Added "aria-describedby" attribute to New password TextBox Widget for 508 User Story MAT-7208
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@12018 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 6a41164d48fff3418f6ebd4b1c8a4fde9671bf98

--- src/mat/client/shared/PasswordRules.java
+++ src/mat/client/shared/PasswordRules.java
@@ -2,9 +2,9 @@ package mat.client.shared;
 
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextArea;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -19,46 +19,42 @@ public class PasswordRules extends Composite {
 		FlowPanel fPanel = new FlowPanel();
 		fPanel.getElement().setId("fPanel_FlowPanel");
 		Label rules = new Label("Password Rules");
+		rules.getElement().setId("passwordRulesLabel");
 		rules.addStyleName("bold");
-		
-		FocusPanel rulesLabelFocusPanel = new FocusPanel(rules);
-		rulesLabelFocusPanel.setTitle("Password Rules");
-		
-		
-		HTML desc = new HTML("Passwords must not consist of a single dictionary word with letters, numbers and symbols. <br><br>" +
-		                     " Previous 6 passwords cannot be reused. <br><br>"+ " A Password needs to be at least one day old before you can change it again. <br><br>"+
-				            " Passwords must be between 8 and 16 characters and contain at least one of each of  the following:" 
-							);
+		HTML desc = new HTML(
+				"Passwords must not consist of a single dictionary word with letters, numbers and symbols. <br><br>"
+						+ " Previous 6 passwords cannot be reused. <br><br>"
+						+ " A Password needs to be at least one day old before you can change it again. <br><br>"
+						+ " Passwords must be between 8 and 16 characters and contain at least one of each of  the following:");
 		desc.addStyleName("passwordRulesDescription");
-		
-		FocusPanel descFocusPanel = new FocusPanel(desc);
-		descFocusPanel.setTitle("Passwords must not consist of a single dictionary word with letters, numbers and symbols." +
-                " Previous 6 passwords cannot be reused."+ " A Password needs to be at least one day old before you can change it again."+
-	            " Passwords must be between 8 and 16 characters and contain at least one of each of  the following:" 
-				);
-		
-		HTML b1 = new HTML("<img src='images/bullet.png'/> Upper case character");
-		FocusPanel b1FocusPanel = new FocusPanel(b1);
-		b1FocusPanel.setTitle("Upper case character.");
-		
-		HTML b2 = new HTML("<img src='images/bullet.png'/> Lower case character");
-		FocusPanel b2FocusPanel = new FocusPanel(b2);
-		b2FocusPanel.setTitle("or Lower case character.");
-		
-		HTML b3 = new HTML("<img src='images/bullet.png'/> One of the following special characters % &nbsp; # &nbsp; * &nbsp; + &nbsp; , &nbsp; ; &nbsp; = &nbsp; ? &nbsp; _");
-		FocusPanel b3FocusPanel = new FocusPanel(b3);
-		b3FocusPanel.setTitle("or One of the following special characters % # * + , ; = ? _");
-		
+		desc.getElement().setId("descLabel");
+		HTML b1 = new HTML(
+				"<img src='images/bullet.png'/> Upper case character");
+		b1.getElement().setId("b1Label");
+		HTML b2 = new HTML(
+				"<img src='images/bullet.png'/> Lower case character");
+		b2.getElement().setId("b2Label");
+		HTML b3 = new HTML(
+				"<img src='images/bullet.png'/> One of the following special characters % &nbsp; # &nbsp; * &nbsp; + &nbsp; , &nbsp; ; &nbsp; = &nbsp; ? &nbsp; _");
+		b3.getElement().setId("b3Label");
 		HTML b4 = new HTML("<img src='images/bullet.png'/> Numeric character");
-		FocusPanel b4FocusPanel = new FocusPanel(b4);
-		b4FocusPanel.setTitle("or Numeric character.");
-		
-		fPanel.add(rulesLabelFocusPanel);
-		fPanel.add(descFocusPanel);
-		fPanel.add(b1FocusPanel);
-		fPanel.add(b2FocusPanel);
-		fPanel.add(b3FocusPanel);
-		fPanel.add(b4FocusPanel);
+		b4.getElement().setId("b4Label");
+
+		fPanel.add(rules);
+		fPanel.add(desc);
+		fPanel.add(b1);
+		fPanel.add(b2);
+		fPanel.add(b3);
+		fPanel.add(b4);
 		initWidget(fPanel);
+	}
+
+	public static TextArea getInvisibleTextAreaFor508() {
+
+		TextArea textArea508 = new TextArea();
+		textArea508.setVisible(false);
+
+		textArea508.setText("Password rules blah blah");
+		return textArea508;
 	}
 }

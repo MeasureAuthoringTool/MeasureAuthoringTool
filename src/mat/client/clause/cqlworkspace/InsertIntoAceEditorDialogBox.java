69dc88654d7a167b79a936d77905dce934c9581d
Ravi Tadepally <rtadepally@hcareis.com>
2016-06-29 18:12:57 +0000

    MAT-7461 Fix white space issues when using Insert functionality and creating Value Sets for the CQL Library -Removing white space while inserting into ace Editor -Added space after colon ":" while inserting Applied QDM Elements into Definitions and Functions
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@12439 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: eb9057b00bca090fb6c96b68069300679ffd0d8e

--- src/mat/client/clause/cqlworkspace/InsertIntoAceEditorDialogBox.java
+++ src/mat/client/clause/cqlworkspace/InsertIntoAceEditorDialogBox.java
@@ -212,12 +212,7 @@ public class InsertIntoAceEditorDialogBox {
 									int columnIndex = editor.getCursorPosition().getColumn();
 									System.out.println(columnIndex);
 									//convertToCamelCase(attributeNameToBeInserted);
-									if(columnIndex == 0){
-										editor.insertAtCursor(convertToCamelCase(attributeNameToBeInserted));
-									}
-									else{
-										editor.insertAtCursor(" " + convertToCamelCase(attributeNameToBeInserted));
-									}
+									editor.insertAtCursor(convertToCamelCase(attributeNameToBeInserted));
 									editor.focus();
 									dialogModal.hide();
 								}
@@ -245,15 +240,10 @@ public class InsertIntoAceEditorDialogBox {
 										String[] str = itemNameToBeInserted.split("\\.");
 										StringBuilder sb = new StringBuilder();
 										sb = sb.append("[\"" + str[1] + "\"");
-										sb = sb.append(":\"").append(str[0] + "\"]");
+										sb = sb.append(": \"").append(str[0] + "\"]");
 										itemNameToBeInserted = sb.toString();
 									}
-									if(columnIndex == 0){
-										editor.insertAtCursor(itemNameToBeInserted);
-									}
-									else{
-										editor.insertAtCursor(" " + itemNameToBeInserted);
-									}
+									editor.insertAtCursor(itemNameToBeInserted);
 									editor.focus();
 									dialogModal.hide();
 								}

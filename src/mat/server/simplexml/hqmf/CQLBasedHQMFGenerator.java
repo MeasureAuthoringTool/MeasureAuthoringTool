163a2a7a82335cc0b46548a9ab45bc93bc065ec8
Chinmay Bajikar <CBajikar@telligen.com>
2016-08-17 04:30:10 +0000

    Human-readable narrative for data criteria causing CQL-HQMF generation to fail. Fixed.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@12830 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: ebca6cafff9500556ece6d8291238f1202cee67a

--- src/mat/server/simplexml/hqmf/CQLBasedHQMFGenerator.java
+++ src/mat/server/simplexml/hqmf/CQLBasedHQMFGenerator.java
@@ -52,7 +52,7 @@ public class CQLBasedHQMFGenerator implements Generator {
 			XmlProcessor hqmfProcessor = new XmlProcessor(hqmfXML);
 			me.setHQMFXmlProcessor(hqmfProcessor);
 			
-			//generateNarrative(me);
+			generateNarrative(me);
 			hqmfXML = finalCleanUp(me);
 			
 			// TODO Export more Stuff
@@ -131,7 +131,7 @@ public class CQLBasedHQMFGenerator implements Generator {
 		try{
 			Node mainNode = humanReadableProcessor.getOriginalDoc().getFirstChild();
 			XmlProcessor.clean(mainNode);
-			//generateDataCritNarrative(me, humanReadableProcessor);
+			generateDataCritNarrative(me, humanReadableProcessor);
 			generatePopulationCritNarrative(me, humanReadableProcessor);
 			
 		}  catch (XPathExpressionException e) {
@@ -205,7 +205,7 @@ public class CQLBasedHQMFGenerator implements Generator {
 		for(Node popNode : popNodeList ){
 			divNode.appendChild(popNode);
 		}
-		Node narrativeListNode = getNarrativeListNode(divNode, me.getHQMFXmlProcessor(), humanReadableProcessor);
+		Node narrativeListNode = getPopulationNarrativeListNode(divNode, me.getHQMFXmlProcessor(), humanReadableProcessor);
 		if(narrativeListNode != null){
 			dataCritItemNode.appendChild(narrativeListNode);
 		}		
@@ -230,6 +230,46 @@ public class CQLBasedHQMFGenerator implements Generator {
 		return elementsNodeList;
 	}
 	
+	/**
+	 * Generate data crit narrative.
+	 *
+	 * @param me the me
+	 * @param humanReadableProcessor the human readable processor
+	 * @throws XPathExpressionException the x path expression exception
+	 */
+	private void generateDataCritNarrative(MeasureExport me,
+			XmlProcessor humanReadableProcessor)
+					throws XPathExpressionException {
+		
+		//Get narrative for Data Criteria (QDM Variable) section
+		//Node dataCritQDMVarNode = generateNarrativeItem(me, humanReadableProcessor, "Data Criteria (QDM Variables)");
+		
+		//Get narrative for Data Criteria (QDM Data Elements) section.
+		Node dataCritQDMNode = generateNarrativeItem(me, humanReadableProcessor, "Data Criteria (QDM Data Elements)");
+		
+		//Get narrative for Supplemental Data Elements section
+		Node dataCritSuppNode = generateNarrativeItem(me, humanReadableProcessor, "Supplemental Data Elements");
+		
+		//Get narrative for Risk Adjustment Data Elements section
+		Node riskAdjustmentSuppNode = generateNarrativeItem(me, humanReadableProcessor, "Risk Adjustment Variables");
+		
+		XmlProcessor hqmfProcessor = me.getHQMFXmlProcessor();
+		String xPathForDataCriteriaSection = "//dataCriteriaSection/text";
+		Node dataCritTextNode = hqmfProcessor.findNode(hqmfProcessor.getOriginalDoc(), xPathForDataCriteriaSection);
+		
+		Element xmlNode = hqmfProcessor.getOriginalDoc().createElement("xml");
+		Element itemNode = hqmfProcessor.getOriginalDoc().createElement("item");
+		Element listNode = hqmfProcessor.getOriginalDoc().createElement("list");
+		
+		//listNode.appendChild(dataCritQDMVarNode);
+		listNode.appendChild(dataCritQDMNode);
+		listNode.appendChild(dataCritSuppNode);
+		listNode.appendChild(riskAdjustmentSuppNode);
+		
+		itemNode.appendChild(listNode);
+		xmlNode.appendChild(itemNode);
+		dataCritTextNode.appendChild(xmlNode);
+	}
 	
 	/**
 	 * Generate narrative item.
@@ -254,13 +294,63 @@ public class CQLBasedHQMFGenerator implements Generator {
 		if((divNode != null) && "div".equals(divNode.getNodeName())){
 			if(divNode.hasChildNodes()){
 				Node ulNode = divNode.getFirstChild();
-				Node narrativeListNode = getNarrativeListNode(ulNode, me.getHQMFXmlProcessor(), humanReadableProcessor);
+				Node narrativeListNode = getNarrativeListNode(ulNode, me.getHQMFXmlProcessor());
 				if(narrativeListNode != null){
 					dataCritItemNode.appendChild(narrativeListNode);
 				}
 			}
 		}
 		return dataCritItemNode;
+	}
+	
+	/**
+	 * Gets the narrative list node.
+	 *
+	 * @param humanReadableNode the human readable node
+	 * @param xmlProcessor the xml processor
+	 * @return the narrative list node
+	 */
+	private Node getNarrativeListNode(Node humanReadableNode, XmlProcessor xmlProcessor) {		
+		Node narrativeListNode = null;
+		String nodeName = humanReadableNode.getNodeName();
+		if(humanReadableNode.getNodeType() == humanReadableNode.TEXT_NODE){
+			narrativeListNode = xmlProcessor.getOriginalDoc().createTextNode(humanReadableNode.getNodeValue());
+		}
+		else if("ul".equals(nodeName)){
+			narrativeListNode = xmlProcessor.getOriginalDoc().createElement("list");
+			NodeList childNodeList = humanReadableNode.getChildNodes();
+			
+			if(childNodeList != null){
+				for(int i=0;i<childNodeList.getLength();i++){
+					Node child = getNarrativeListNode(childNodeList.item(i), xmlProcessor);
+					if(child != null){
+						narrativeListNode.appendChild(child);
+					}
+				}
+			}
+		}else if("li".equals(nodeName)){
+			narrativeListNode = xmlProcessor.getOriginalDoc().createElement("item");
+			
+			if(humanReadableNode.hasChildNodes()){
+				NodeList childNodeList = humanReadableNode.getChildNodes();
+				for(int i=0;i<childNodeList.getLength();i++){
+					Node child = getNarrativeListNode(childNodeList.item(i), xmlProcessor);
+					if(child != null){
+						narrativeListNode.appendChild(child);
+					}
+				}
+			}else{
+				narrativeListNode.setTextContent(humanReadableNode.getTextContent());
+			}
+		}else if("b".equals(nodeName)){
+			narrativeListNode = xmlProcessor.getOriginalDoc().createElement("content");
+			((Element)narrativeListNode).setAttribute("styleCode", "Bold");
+			narrativeListNode.setTextContent(humanReadableNode.getTextContent());
+		}else if("i".equals(nodeName)){
+			narrativeListNode = xmlProcessor.getOriginalDoc().createElement("caption");
+			narrativeListNode.setTextContent(humanReadableNode.getTextContent());
+		}
+		return narrativeListNode;
 	}
 	
 	/**
@@ -350,7 +440,7 @@ public class CQLBasedHQMFGenerator implements Generator {
 	}*/
 	
 	
-	private Node getNarrativeListNode(Node humanReadableNode, XmlProcessor xmlProcessor, XmlProcessor humanReadableProcessor){
+	private Node getPopulationNarrativeListNode(Node humanReadableNode, XmlProcessor xmlProcessor, XmlProcessor humanReadableProcessor){
 		Node narrativeListNode = xmlProcessor.getOriginalDoc().createElement("list");
 		
 		String searchText = "//ul/li/div//li/label/strong";

f07642784a59b71b55d9689a39d4a6c59d394466
Chinmay Bajikar <CBajikar@telligen.com>
2016-09-21 01:01:39 +0000

    Pilot test for MAT-7807
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@13092 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 47a8831f1a4af3ee2eb3f195247d693ec50059dc

--- src/edu/ycp/cs/dh/acegwt/public/ace/mode-cql.js
+++ src/edu/ycp/cs/dh/acegwt/public/ace/mode-cql.js
@@ -7,47 +7,67 @@ var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 var SqlHighlightRules = function() {
 
 	var keywords = (
-	        "library|version|using|include|called|public|private|parameter|default|codesystem|valueset|codesystems|define|function|with|such that|without|" +
-	        "in|from|where|return|all|distinct|sort|by|asc|desc|is|not|cast|as|between|difference|contains|and|or|xor|union|intersection|year|month|day|hour|minute|" +
-	        "second|millisecond|when|then|or before|or after|or more|or less|context|using QDM|Interval<DateTime>|context Patient|context Population"
+	        "-|!|(|)|*|,|.|/|:|[|]|^|{|}|~|+|<|=|>|after|all|and|as|asc|ascending|before|begins|begun|between|Boolean|by|called|case|cast|Code|codesystem|codesystems|" +
+	        "collapse|Concept|contains|context|convert|date|DateTime|day|days|Decimal|default|define|desc|descending|difference|display|distinct|div|duration|during|" +
+	        "else|end|ends|except|exists|false|flatten|from|function|hour|hours|if|in|include|included in|includes|Integer|intersect|Interval|is|less|let|library|List|" +
+	        "maximum|meets|millisecond|milliseconds|minimum|minute|minutes|mod|month|months|more|not|null|occurs|of|or|or after|or before|or less|or more|overlaps|parameter|" +
+	        "predecessor|predecessor of|private|properly|properly included in|properly includes|public|QDM|Quantity|return|same|second|seconds|singleton|singleton from|sort|" +
+	        "sort by|start|starts|String|successor|such|such that|Sum|that|then|Time|time|timezone|to|true|Tuple|union|using|valueset|version|week|weeks|when|where|width|with|" +
+	        "within|without|xor|year|years"
 	    );
 	    
 	    var matTimingKeywords = (
-	    		"after|before|during|ends|includes|included in|meets|overlaps|same|starts|within|start of|such that"
+	    		"after|after end|after start|before|before end|before start|during|ends|ends after|ends after end|ends after start|ends before|ends before end|ends before start|" +
+	    		"ends during|ends properly during|ends properly within|ends properly within end|ends properly within start|ends same as|ends same as end|ends same as start|" +
+	    		"ends same or after|ends same or after end|ends same or after start|ends same or before|ends same or before end|ends same or before start|ends within|ends within end|" +
+	    		"ends within start|included in|includes|includes end|includes start|meets|meets after|meets before|overlaps|overlaps after|overlaps after|overlaps before|" +
+	    		"overlaps before|properly during|properly included in|properly includes|properly includes end|properly includes start|properly within|properly within end|" +
+	    		"properly within start|same as|same as|same as end|same as start|same or after|same or after end|same or after start|same or before|same or before end|" +
+	    		"same or before start|starts|starts after|starts after end|starts after start|starts before|starts before end|starts before start|starts during|starts properly during|" +
+	    		"starts properly within|starts properly within end|starts properly within start|starts same as|starts same as end|starts same as start|starts same or after|" +
+	    		"starts same or after end|starts same or after start|starts same or before|starts same or before end|starts same or before start|starts within|starts within end|" +
+	    		"starts within start|within"
 	    );
 	    
 	    var matFunctionKeywords = (
-	    		"Age At|Avg|Count|Datetimediff|Fifth|First|Fourth|Max|Median|Min|Most Recent|Satisfies All|Satisfies Any|Second|Sum|Third"
+	    		"Abs()|AgeInDays()|AgeInDaysAt()|AgeInHours()|AgeInHoursAt()|AgeInMinutes()|AgeInMinutesAt()|AgeInMonths()|AgeInMonthsAt()|AgeInSeconds()|" +
+	    		"AgeInSecondsAt()|AgeInYears()|AgeInYearsAt()|AllTrue()|AnyTrue()|Avg()|CalculateAgeInDays()|CalculateAgeInDaysAt()|CalculateAgeInHours()|" +
+	    		"CalculateAgeInHoursAt()|CalculateAgeInMinutes()|CalculateAgeInMinutesAt()|CalculateAgeInMonths()|CalculateAgeInMonthsAt()|CalculateAgeInSeconds()|" +
+	    		"CalculateAgeInSecondsAt()|CalculateAgeInYears()|CalculateAgeInYearsAt()|Ceiling()|Coalesce()|Count()|DateTime()|Exp()|First()|Floor()|IndexOf()|Last()|" +
+	    		"Length()|Ln()|Log()|Max()|Median()|Min()|Mode()|Now()|PopulationStdDev()|PopulationVariance()|Round()|StdDev()|Sum()|Time()|TimeOfDay()|Today()|Truncate()|Variance()"
 	    );
 
 	    var builtinConstants = (
 	        
 	    	//basic constants	
-	    	'true|false|null|QDM|Patient|Population|MeasurementPeriod|' + 
+	    	'Patient|Population|MeasurementPeriod|' + 
 	    	
 	        //data type constants
-	        '"Patient Characteristic Clinical Trial Participant"|"Patient Characteristic Expired"|"Patient Characteristic Birthdate"|' +
-	        '"Timing Element"|"Encounter, Active"|"Diagnostic Study, Recommended"|"Substance, Recommended"|"Procedure, Recommended"|"Physical Exam, Recommended"|"Laboratory Test, Recommended"|' +
-	        '"Intervention, Recommended"|"Functional Status, Recommended"|"Encounter, Recommended"|"Device, Recommended"|"Transfer To"|"Transfer From"|"Substance, Order"|"Substance, Intolerance"|' +
-	        '"Substance, Allergy"|"Substance, Adverse Event"|"Substance, Administered"|"Risk Category Assessment"|"Procedure, Performed"|"Procedure, Order"|"Procedure, Intolerance"|"Procedure, Adverse Event"|' +
-	        '"Communication: From Provider to Patient"|"Physical Exam, Performed"|"Physical Exam, Order"|"Communication: From Patient to Provider"|"Medication, Order"|"Medication, Intolerance"|' +
-	        '"Medication, Dispensed"|"Medication, Allergy"|"Medication, Adverse Effects"|"Communication: From Provider to Provider"|"Medication, Administered"|"Medication, Active"|' +
-	        '"Laboratory Test, Performed"|"Laboratory Test, Order"|"Laboratory Test, Intolerance"|"Laboratory Test, Adverse Event"|"Intervention, Performed"|"Intervention, Order"|' +
-	        '"Care Goal"|"Intervention, Intolerance"|"Intervention, Adverse Event"|"Provider Characteristic"|"Patient Characteristic"|"Functional Status, Performed"|"Functional Status, Order"|' +
-	        '"Encounter, Performed"|"Encounter, Order"|"Provider Care Experience"|"Diagnostic Study, Performed"|"Diagnostic Study, Order"|"Diagnostic Study, Intolerance"|"Diagnostic Study, Adverse Event"|' +
-	        '"Device, Order"|"Device, Intolerance"|"Device, Applied"|"Device, Allergy"|"Diagnosis"|"Immunization, Intolerance"|"Immunization, Allergy"|"Immunization, Order"|"Immunization, Administered"|' +
-	        '"Device, Adverse Event"|"Medication, Discharge"|"Patient Characteristic Race"|"Patient Characteristic Ethnicity"|"Patient Characteristic Sex"|"Patient Characteristic Payer"|"Patient Care Experience"|' +
+	        '"Adverse Event"|"Allergy/Intolerance"|"Assessment, Performed"|"Assessment, Not Performed"|"Assessment, Recommended"|"Assessment, Not Recommended"|"Patient Care Experience"|' +
+	    	'"Provider Care Experience"|"Care Goal"|"Communication: From Patient to Provider"|"Communication: From Patient to Provider, Not Done"|"Communication: From Provider to Patient"|' +
+	    	'"Communication: From Provider to Patient, Not Done"|"Communication: From Provider to Provider"|"Communication: From Provider to Provider, Not Done"|"Diagnosis"|"Device, Applied"|' +
+	    	'"Device, Not Applied"|"Device, Not Ordered"|"Device, Order"|"Device, Not Recommeded"|"Device, Recommeded"|"Diagnositic Study, Order"|"Diagnositic Study, Not Ordered"|' +
+	    	'"Diagnositic Study, Performed"|"Diagnositic Study, Not Performed"|"Diagnositic Study, Recommended"|"Diagnositic Study, Not Recommended"|"Encounter, Active"|"Encounter, Order"|' +
+	    	'"Encounter, Not Ordered"|"Encounter, Performed"|"Encounter, Not Performed"|"Encounter, Recommended"|"Encounter, Not Recommended"|"Family History"|"Immunization, Administered"|' +
+	    	'"Immunization, Not Administered"|"Immunization, Order"|"Immunization, Not Ordered"|"Patient Characteristic"|"Patient Characteristic Birthdate"|' +
+	    	'"Patient Characteristic Clinical Trial Participant"|"Patient Characteristic Ethnicity"|"Patient Characteristic Expired"|"Patient Characteristic Payer"|"Patient Characteristic Race"|' +
+	    	'"Patient Characteristic Sex"|"Provider Characteristic"|"Intervention, Not Ordered"|"Intervention, Order"|"Intervention, Not Performed"|"Intervention, Performed"|' +
+	    	'"Intervention, Not Recommended"|"Intervention, Recommended"|"Laboratory Test, Order"|"Laboratory Test, Not Ordered"|"Laboratory Test, Performed"|"Laboratory Test, Not Performed"|' +
+	    	'"Laboratory Test, Recommended"|"Laboratory Test, Not Recommended"|"Medication, Active"|"Medication, Administered"|"Medication, Not Administered"|"Medication, Discharge"|' +
+	    	'"Medication, Not Discharged"|"Medication, Dispensed"|"Medication, Not Dispensed"|"Medication, Order"|"Medication, Not Ordered"|"Physical Exam, Order"|"Physical Exam, Not Ordered"|' +
+	    	'"Physical Exam, Performed"|"Physical Exam, Not Performed"|"Physical Exam, Recommended"|"Physical Exam, Not Recommended"|"Procedure, Order"|"Procedure, Not Ordered"|' +
+	    	'"Procedure, Performed"|"Procedure, Not Performed"|"Procedure, Recommended"|"Procedure, Not Recommended"|"Substance, Administered"|"Substance, Not Administered"|"Substance, Order"|' +
+	    	'"Substance, Not Ordered"|"Substance, Recommended"|"Substance, Not Recommended"|"Symptom"|' +
 	        
 	        //data type attribute constants
-	        '"anatomical structure"|"negation rationale"|"number"|"ordinality"|"patient preference"|"provider preference"|"radiation dosage"|"radiationduration"|"reaction"|"reason"|"refills"|' +
-	        '"cumulative medication duration"|"result"|"route"|"severity"|"start datetime"|"signed datetime"|"status"|"stop datetime"|"dose"|"Health Record Field"|"admission datetime"|"discharge datetime"|' +
-	        '"environment"|"discharge status"|"incision datetime"|"laterality"|"length of stay"|"removal datetime"|"facility location"|"facility location arrival datetime"|"facility location departure datetime"|' +
-	        '"radiation duration"|"related to"|"recorder"|"source"|"frequency"|"hospital location"|"method"'
+	        'activeDatetime|admissionSource|anatomicalApproachSite|anatomicalLocationSite|authorTime|birthDatetime|cause|code|diagnosis|dischargeDisposition|dosage|expiredDatetime|' +
+	        'facilityLocation|frequency|id|incisionDatetime|lengthOfStay|locationPeriod|method|negationRationale|ordinality|prevalencePeriod|principalDiagnosis|radiationDosage|' +
+	        'radiationDuration|reason|recorder|referenceRange|refills|relatedTo|relationship|relevantPeriod|reporter|result|resultDatetime|route|severity|status|supply|targetOutcome|type'
 	        
 	    );
 
 	    var builtinFunctions = (
-	        'date|time|timezone|starts|ends|occurs|overlaps|Interval|Tuple|List|DateTime|AgeInYearsAt'
+	        'date|time|timezone|starts|ends|occurs|overlaps|Interval|Tuple|List|DateTime'
 	    );
 
     var keywordMapper = this.createKeywordMapper({

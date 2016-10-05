e85c7f6539fe45ba1cceb0c8579ec3b146098f01
Ravi Tadepally <rtadepally@hcareis.com>
2016-10-05 16:53:42 +0000

    Fixed Issues on Extension values in templates xml for HQMF
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@13191 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8a6c87a3435e9c27392cd0041a6671776690950c

--- src/mat/server/simplexml/hqmf/Generator.java
+++ src/mat/server/simplexml/hqmf/Generator.java
@@ -125,6 +125,8 @@ public interface Generator extends MatConstants {
 	/** The Constant VERSION_4_3_ID. */
 	public static final String VERSION_4_3_ID = "2015-09-30";
 	
+	public static final String VERSION_PATIENT_CHARACTERISTIC_SEX="2014-11-24";
+	
 	public static final String POPULATION_CRITERIA_EXTENSION = "2015-12-01";
 	
 	/** The Constant VALUE_SET. */

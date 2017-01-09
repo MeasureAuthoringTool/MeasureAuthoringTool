61d5707f992b2e7e5a0e9e8856a906ed96d67b39
Jyoti Narang <JNarang@hcareis.com>
2017-01-09 17:41:34 +0000

    Added DTO for CQLLibrary
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@13905 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: f8690925aab7fdbdd596c449effd2fdd4829bee3

---
+++ src/mat/model/cql/CQLLibraryDataSetObject.java
@@ -0,0 +1,117 @@
+package mat.model.cql;
+
+import java.sql.Timestamp;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import mat.model.LockedUserInfo;
+
+public class CQLLibraryDataSetObject implements IsSerializable{
+	private String id;
+	private String cqlName;
+	private String version;
+	private boolean isDraft;
+	private Timestamp finalizedDate;
+	private String measureSetId;
+	private boolean isLocked;
+	private LockedUserInfo lockedUserInfo;
+	private String ownerEmailAddress;
+	private boolean isFamily;
+	private String releaseVersion;
+	private String ownerFirstName;
+	private String ownerLastName;
+	private String cqlText;
+	private String measureId;
+	
+	public boolean isLocked() {
+		return isLocked;
+	}
+	public void setLocked(boolean isLocked) {
+		this.isLocked = isLocked;
+	}
+	public LockedUserInfo getLockedUserInfo() {
+		return lockedUserInfo;
+	}
+	public void setLockedUserInfo(LockedUserInfo lockedUserInfo) {
+		this.lockedUserInfo = lockedUserInfo;
+	}
+	
+	public boolean isFamily() {
+		return isFamily;
+	}
+	public void setFamily(boolean isFamily) {
+		this.isFamily = isFamily;
+	}
+	public String getReleaseVersion() {
+		return releaseVersion;
+	}
+	public void setReleaseVersion(String releaseVersion) {
+		this.releaseVersion = releaseVersion;
+	}
+	public String getOwnerFirstName() {
+		return ownerFirstName;
+	}
+	public void setOwnerFirstName(String ownerFirstName) {
+		this.ownerFirstName = ownerFirstName;
+	}
+	public String getOwnerLastName() {
+		return ownerLastName;
+	}
+	public void setOwnerLastName(String ownerLastName) {
+		this.ownerLastName = ownerLastName;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	public boolean isDraft() {
+		return isDraft;
+	}
+	public void setDraft(boolean isDraft) {
+		this.isDraft = isDraft;
+	}
+	public Timestamp getFinalizedDate() {
+		return finalizedDate;
+	}
+	public void setFinalizedDate(Timestamp finalizedDate) {
+		this.finalizedDate = finalizedDate;
+	}
+	public String getMeasureSetId() {
+		return measureSetId;
+	}
+	public void setMeasureSetId(String measureSetId) {
+		this.measureSetId = measureSetId;
+	}
+	public String getOwnerEmailAddress() {
+		return ownerEmailAddress;
+	}
+	public void setOwnerEmailAddress(String ownerEmailAddress) {
+		this.ownerEmailAddress = ownerEmailAddress;
+	}
+	public String getCqlText() {
+		return cqlText;
+	}
+	public void setCqlText(String cqlText) {
+		this.cqlText = cqlText;
+	}
+	public String getCqlName() {
+		return cqlName;
+	}
+	public void setCqlName(String cqlName) {
+		this.cqlName = cqlName;
+	}
+	public String getMeasureId() {
+		return measureId;
+	}
+	public void setMeasureId(String measureId) {
+		this.measureId = measureId;
+	}
+}

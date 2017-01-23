713a78b83641c23d72c082781dc4617005834223
Jack Meyer <jmeyer@telligen.com>
2017-01-23 16:44:31 +0000

    MAT-8399 Change title from QDM Elements to Value Sets/Codes
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14031 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 9763da15f4dfe867932ab0e8658866b81db92399

--- src/mat/client/clause/cqlworkspace/CQLQDMAppliedView.java
+++ src/mat/client/clause/cqlworkspace/CQLQDMAppliedView.java
@@ -468,7 +468,7 @@ public class CQLQDMAppliedView implements HasSelectionHandlers<Boolean>{
 		qdmElementsHeader.setStyleName("measureGroupingTableHeader");
 		qdmElementsHeader.getElement().setAttribute("tabIndex", "0");
 		
-		HTML searchHeaderText = new HTML("<strong>Value Sets/Codes</strong>");
+		HTML searchHeaderText = new HTML("<strong>Applied Value Sets/Codes</strong>");
 		qdmElementsHeader.add(searchHeaderText);
 		cellTablePanel.add(qdmElementsHeader);
 		if ((appliedValueSetList != null)

1ded7c8fa4f948680e6452236d5100b3330bc966
Jyoti Narang <JNarang@hcareis.com>
2017-01-25 19:02:23 +0000

    Moved General Information view out from CQLWorkSpaceView for more re usability
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14064 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 869d29bd6c538d084770344f58d80c770227379c

--- src/mat/client/clause/cqlworkspace/CQLWorkSpaceView.java
+++ src/mat/client/clause/cqlworkspace/CQLWorkSpaceView.java
@@ -104,6 +104,10 @@ import mat.shared.GetUsedCQLArtifactsResult;
 /**
  * The Class CQLWorkSpaceView.
  */
+/**
+ * @author jnarang
+ *
+ */
 public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	
 	/** The available qds attribute list. */
@@ -414,6 +418,8 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	
 	private CQLIncludeLibraryView inclView ;
 	
+	private CQLGeneralInformationView generalInformationView;
+	
 	//private AnchorListItem includeLibrary;
 	
 	/**
@@ -507,6 +513,7 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	 * Instantiates a new CQL work space view.
 	 */
 	public CQLWorkSpaceView() {
+		generalInformationView = new CQLGeneralInformationView();
 		qdmView = new CQLQDMAppliedView();
 		inclView = new CQLIncludeLibraryView();
 		defineAceEditor.startEditor();
@@ -559,14 +566,7 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	 */
 	@Override
 	public void buildCQLFileView() {
-		setCurrentSelectedFunctionObjId(null);
-		setCurrentSelectedDefinitionObjId(null);
-		setCurrentSelectedParamerterObjId(null);
-		setCurrentSelectedIncLibraryObjId(null);
-		if (getFunctionArgumentList().size() > 0) {
-			getFunctionArgumentList().clear();
-		}
-		getFunctionArgNameMap().clear();
+		unsetEachSectionSelectedObject();
 		mainFlowPanel.clear();
 		
 		VerticalPanel cqlViewVP = new VerticalPanel();
@@ -1000,134 +1000,32 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	 */
 	@Override
 	public void buildGeneralInformation() {
+		unsetEachSectionSelectedObject();
+		mainFlowPanel.clear();
+		mainFlowPanel.add(generalInformationView.asWidget());
+		
+	}
+
+   private void unsetEachSectionSelectedObject() {
 		setCurrentSelectedDefinitionObjId(null);
 		setCurrentSelectedParamerterObjId(null);
 		setCurrentSelectedFunctionObjId(null);
 		setCurrentSelectedIncLibraryObjId(null);
+		getFunctionArgumentList().clear();
 		getFunctionArgNameMap().clear();
 		if (getFunctionArgumentList().size() > 0) {
 			getFunctionArgumentList().clear();
 		}
-		VerticalPanel generalInfoTopPanel = new VerticalPanel();
-		
-		Label libraryNameLabel = new Label(LabelType.INFO, "CQL Library Name");
-		
-		libraryNameLabel.getElement().setAttribute("style", "font-size:90%;margin-left:15px;background-color:#0964A2;");
-		libraryNameLabel.setWidth("150px");
-		
-		MatTextBox libraryNameValue = new MatTextBox();
-		libraryNameValue.getElement().setAttribute("style", "margin-left:15px;width:250px;height:25px;");
-		libraryNameValue.setText(createCQLLibraryName(MatContext.get().getCurrentMeasureName()));
-		libraryNameValue.setReadOnly(true);
-		
-		Label libraryVersionLabel = new Label(LabelType.INFO, "Version");
-		
-		libraryVersionLabel.getElement().setAttribute("style", "font-size:90%;margin-left:15px;background-color:#0964A2;");
-		libraryVersionLabel.setWidth("150px");
-		
-		MatTextBox libraryVersionValue = new MatTextBox();
-		libraryVersionValue.getElement().setAttribute("style", "margin-left:15px;width:250px;height:25px;");
-		
-		String measureVersion = MatContext.get().getCurrentMeasureVersion();
-		
-		measureVersion = measureVersion.replaceAll("Draft ", "").trim();
-		if(measureVersion.startsWith("v")){
-			measureVersion = measureVersion.substring(1);
-		}
-		
-		libraryVersionValue.setText(measureVersion);
-		libraryVersionValue.setReadOnly(true);
-		
-		Label usingModeLabel = new Label(LabelType.INFO, "Using Model");
-		MatTextBox usingModelValue = new MatTextBox();
-		usingModeLabel.getElement().setAttribute("style", "font-size:90%;margin-left:15px;background-color:#0964A2;");
-		usingModeLabel.setWidth("150px");
-		usingModelValue.getElement().setAttribute("style", "margin-left:15px;width:250px;height:25px;");
-		usingModelValue.setText("QDM");
-		usingModelValue.setReadOnly(true);
-		
-		Label modelVersionLabel = new Label(LabelType.INFO, "Version");
-		
-		modelVersionLabel.getElement().setAttribute("style", "font-size:90%;margin-left:15px;background-color:#0964A2;");
-		modelVersionLabel.setWidth("150px");
-		MatTextBox modelVersionValue = new MatTextBox();
-		modelVersionValue.getElement().setAttribute("style", "margin-left:15px;width:250px;height:25px;");
-		//modelVersionValue.setText("5.0");
-		modelVersionValue.setReadOnly(true);
-		modelVersionValue.setText("5.0.2");
-		
-		generalInfoTopPanel.add(new SpacerWidget());
-		// messagePanel.add(successMessageAlert);
-		generalInfoTopPanel.add(new SpacerWidget());
-		
-		generalInfoTopPanel.add(libraryNameLabel);
-		generalInfoTopPanel.add(new SpacerWidget());
-		generalInfoTopPanel.add(libraryNameValue);
-		generalInfoTopPanel.add(new SpacerWidget());
-		
-		generalInfoTopPanel.add(libraryVersionLabel);
-		generalInfoTopPanel.add(new SpacerWidget());
-		generalInfoTopPanel.add(libraryVersionValue);
-		generalInfoTopPanel.add(new SpacerWidget());
-		
-		generalInfoTopPanel.add(usingModeLabel);
-		generalInfoTopPanel.add(new SpacerWidget());
-		generalInfoTopPanel.add(usingModelValue);
-		generalInfoTopPanel.add(new SpacerWidget());
-		
-		generalInfoTopPanel.add(modelVersionLabel);
-		generalInfoTopPanel.add(new SpacerWidget());
-		generalInfoTopPanel.add(modelVersionValue);
-		generalInfoTopPanel.add(new SpacerWidget());
-		
-		VerticalPanel vp = new VerticalPanel();
-		vp.setStyleName("cqlRightContainer");
-		vp.setWidth("715px");
-		generalInfoTopPanel.setWidth("700px");
-		generalInfoTopPanel.setStyleName("marginLeft15px");
-		vp.add(generalInfoTopPanel);
-		
-		mainFlowPanel.clear();
-		mainFlowPanel.add(vp);
-		
 	}
 	
-	/**
-	 * This method will take a String and remove all non-alphabet/non-numeric characters 
-	 * except underscore ("_") characters.
-	 * @param originalString
-	 * @return cleanedString
-	 */
-	private String createCQLLibraryName(String originalString) {
-		originalString = originalString.replaceAll(" ", "");
-		String cleanedString = "";
-				
-		for(int i=0;i<originalString.length();i++){
-			char c = originalString.charAt(i);
-			int intc = (int)c;
-			if(c == '_' || (intc >= 48 && intc <= 57) || (intc >= 65 && intc <= 90) || (intc >= 97 && intc <= 122)){
-				
-				if(!(cleanedString.isEmpty() && Character.isDigit(c))){
-					cleanedString = cleanedString + "" + c;
-				}
-				
-			} 
-			
-		}
-		
-		return cleanedString;
-	}
 
 	@Override
 	public void buildAppliedQDM() {
 		qdmView.resetVSACValueSetWidget();
 		qdmView.setWidgetToDefault();
 		resetMessageDisplay();
-		setCurrentSelectedDefinitionObjId(null);
-		setCurrentSelectedParamerterObjId(null);
-		setCurrentSelectedFunctionObjId(null);
-		setCurrentSelectedIncLibraryObjId(null);
-		
+
+		unsetEachSectionSelectedObject();
 		VerticalPanel appliedQDMTopPanel = new VerticalPanel();
 		
 		appliedQDMTopPanel.add(qdmView.asWidget());
@@ -1147,15 +1045,7 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	
 	@Override
 	public void buildIncludesView() {
-		setCurrentSelectedDefinitionObjId(null);
-		setCurrentSelectedParamerterObjId(null);
-		setCurrentSelectedFunctionObjId(null);
-		setCurrentSelectedIncLibraryObjId(null);
-		getFunctionArgNameMap().clear();
-		//setIsPageDirty(false);
-		if (getFunctionArgumentList().size() > 0) {
-			getFunctionArgumentList().clear();
-		}
+		unsetEachSectionSelectedObject();
 		mainFlowPanel.clear();
 		resetMessageDisplay();
 		
@@ -1182,12 +1072,7 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	 * Method to create Right Hand side Nav bar in CQL Workspace.
 	 */
 	private void buildLeftHandNavNar() {
-		setCurrentSelectedDefinitionObjId(null);
-		setCurrentSelectedParamerterObjId(null);
-		setCurrentSelectedFunctionObjId(null);
-		setCurrentSelectedIncLibraryObjId(null);
-		getFunctionArgumentList().clear();
-		getFunctionArgNameMap().clear();
+		unsetEachSectionSelectedObject();
 		rightHandNavPanel.clear();
 		NavPills navPills = new NavPills();
 		navPills.setStacked(true);
@@ -1579,14 +1464,7 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	@SuppressWarnings("static-access")
 	@Override
 	public void buildParameterLibraryView() {
-		setCurrentSelectedDefinitionObjId(null);
-		setCurrentSelectedParamerterObjId(null);
-		setCurrentSelectedFunctionObjId(null);
-		setCurrentSelectedIncLibraryObjId(null);
-		if (getFunctionArgumentList().size() > 0) {
-			getFunctionArgumentList().clear();
-		}
-		getFunctionArgNameMap().clear();
+		unsetEachSectionSelectedObject();
 		//setIsPageDirty(false);
 		mainFlowPanel.clear();
 		VerticalPanel parameterVP = new VerticalPanel();
@@ -1784,15 +1662,7 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	@SuppressWarnings("static-access")
 	@Override
 	public void buildDefinitionLibraryView() {
-		setCurrentSelectedDefinitionObjId(null);
-		setCurrentSelectedParamerterObjId(null);
-		setCurrentSelectedFunctionObjId(null);
-		setCurrentSelectedIncLibraryObjId(null);
-		getFunctionArgNameMap().clear();
-		//setIsPageDirty(false);
-		if (getFunctionArgumentList().size() > 0) {
-			getFunctionArgumentList().clear();
-		}
+		unsetEachSectionSelectedObject();
 		mainFlowPanel.clear();
 		VerticalPanel definitionVP = new VerticalPanel();
 		HorizontalPanel definitionFP = new HorizontalPanel();
@@ -2049,14 +1919,7 @@ public class CQLWorkSpaceView implements CQLWorkSpacePresenter.ViewDisplay {
 	@SuppressWarnings("static-access")
 	@Override
 	public void buildFunctionLibraryView() {
-		setCurrentSelectedDefinitionObjId(null);
-		setCurrentSelectedParamerterObjId(null);
-		setCurrentSelectedFunctionObjId(null);
-		setCurrentSelectedIncLibraryObjId(null);
-		getFunctionArgNameMap().clear();
-		if (getFunctionArgumentList().size() > 0) {
-			getFunctionArgumentList().clear();
-		}
+		unsetEachSectionSelectedObject();
 		mainFlowPanel.clear();
 		
 		VerticalPanel funcVP = new VerticalPanel();

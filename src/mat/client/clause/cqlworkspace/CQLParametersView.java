05605300c0d155d46c01ce99810286ff2457b9f4
Jyoti Narang <JNarang@hcareis.com>
2017-01-25 21:19:35 +0000

    Moved Parameter Section view out from CQLWorkSpaceView for more re usability.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14065 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 751c87ba9b306f34e742a4d60c94ce603444f4c8

---
+++ src/mat/client/clause/cqlworkspace/CQLParametersView.java
@@ -0,0 +1,150 @@
+/**
+ * 
+ */
+package mat.client.clause.cqlworkspace;
+
+import org.gwtbootstrap3.client.ui.Label;
+import org.gwtbootstrap3.client.ui.constants.LabelType;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import edu.ycp.cs.dh.acegwt.client.ace.AceEditor;
+import edu.ycp.cs.dh.acegwt.client.ace.AceEditorMode;
+import edu.ycp.cs.dh.acegwt.client.ace.AceEditorTheme;
+import mat.client.shared.CQLButtonToolBar;
+import mat.client.shared.SpacerWidget;
+import mat.client.util.MatTextBox;
+
+/**
+ * @author jnarang
+ *
+ */
+public class CQLParametersView {
+
+	/**
+	 * TextArea parameterNameTxtArea.
+	 */
+	private MatTextBox parameterNameTxtArea = new MatTextBox();
+
+	/** The parameter ace editor. */
+	private AceEditor parameterAceEditor = new AceEditor();
+
+	private CQLButtonToolBar parameterButtonBar = new CQLButtonToolBar("parameter");
+
+	VerticalPanel vp = new VerticalPanel();
+
+	public CQLParametersView() {
+		// TODO Auto-generated constructor stub
+		// buildView();
+		parameterAceEditor.startEditor();
+	}
+
+	@SuppressWarnings("static-access")
+	private void buildView() {
+		VerticalPanel parameterVP = new VerticalPanel();
+		HorizontalPanel parameterFP = new HorizontalPanel();
+
+		Label parameterLabel = new Label(LabelType.INFO, "Parameter");
+		parameterLabel.setMarginTop(5);
+		parameterLabel.setId("Parameter_Label");
+		parameterNameTxtArea.setText("");
+		// parameterNameTxtArea.setPlaceholder("Enter Parameter Name here.");
+		parameterNameTxtArea.setSize("260px", "25px");
+		parameterNameTxtArea.getElement().setId("parameterNameField");
+		parameterNameTxtArea.setName("parameterName");
+		parameterLabel.setText("Parameter");
+
+		SimplePanel paramAceEditorPanel = new SimplePanel();
+		paramAceEditorPanel.setSize("685px", "510px");
+		// parameterAceEditor.startEditor();
+		parameterAceEditor.setText("");
+		System.out.println("In buildParameterLibraryView setText to ace editor.");
+		parameterAceEditor.setMode(AceEditorMode.CQL);
+		parameterAceEditor.setTheme(AceEditorTheme.ECLIPSE);
+		parameterAceEditor.getElement().getStyle().setFontSize(14, Unit.PX);
+		parameterAceEditor.setSize("675px", "500px");
+		parameterAceEditor.setAutocompleteEnabled(true);
+		parameterAceEditor.addAutoCompletions();
+		parameterAceEditor.setUseWrapMode(true);
+		parameterAceEditor.clearAnnotations();
+		parameterAceEditor.removeAllMarkers();
+		parameterAceEditor.redisplay();
+		parameterAceEditor.getElement().setAttribute("id", "Parameter_AceEditorID");
+		paramAceEditorPanel.add(parameterAceEditor);
+		paramAceEditorPanel.getElement().setAttribute("id", "SimplePanel_Parameter_AceEditor");
+		paramAceEditorPanel.setStyleName("cqlRightContainer");
+
+		parameterNameTxtArea.getElement().setAttribute("style", "width:250px;height:25px;margin-top:5px;");
+
+		parameterButtonBar.getInsertButton().setVisible(false);
+		parameterButtonBar.getTimingExpButton().setVisible(false);
+		parameterButtonBar.getCloseButton().setVisible(false);
+		parameterVP.add(new SpacerWidget());
+		parameterVP.add(parameterLabel);
+		parameterVP.add(new SpacerWidget());
+		parameterVP.add(parameterNameTxtArea);
+		parameterVP.add(new SpacerWidget());
+		parameterVP.add(parameterButtonBar);
+		parameterVP.add(paramAceEditorPanel);
+		parameterVP.add(new SpacerWidget());
+		parameterVP.setStyleName("topping");
+		parameterFP.add(parameterVP);
+		parameterFP.setStyleName("cqlRightContainer");
+		
+		vp.clear();
+		vp.setStyleName("cqlRightContainer");
+		vp.setWidth("700px");
+		vp.setHeight("500px");
+		parameterFP.setWidth("700px");
+		parameterFP.setStyleName("marginLeft15px");
+
+		vp.add(new SpacerWidget());
+		vp.add(parameterFP);
+		vp.setHeight("675px");
+	}
+
+	public MatTextBox getParameterNameTxtArea() {
+		return parameterNameTxtArea;
+	}
+
+	public AceEditor getParameterAceEditor() {
+		return parameterAceEditor;
+	}
+
+	public void setParameterAceEditor(AceEditor parameterAceEditor) {
+		this.parameterAceEditor = parameterAceEditor;
+	}
+
+	public CQLButtonToolBar getParameterButtonBar() {
+		return parameterButtonBar;
+	}
+
+	public VerticalPanel asWidget() {
+		resetAll();
+		buildView();
+		return vp;
+	}
+
+	public void resetAll() {
+		getParameterNameTxtArea().setText("");
+		getParameterAceEditor().setText("");
+	}
+
+	public void setParameterWidgetReadOnly(boolean isEditable) {
+
+		getParameterNameTxtArea().setEnabled(isEditable);
+		getParameterAceEditor().setReadOnly(!isEditable);
+		System.out.println(
+				"in setParameterWidgetReadOnly: setting Ace Editor read only flag. read only = " + !isEditable);
+		getParameterButtonBar().getSaveButton().setEnabled(isEditable);
+		getParameterButtonBar().getDeleteButton().setEnabled(isEditable);
+		getParameterButtonBar().getInsertButton().setEnabled(isEditable);
+	}
+
+	public void hideParamAceEditorAutoCompletePopUp() {
+		getParameterAceEditor().detach();
+	}
+}

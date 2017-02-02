faac7baa62b5cf9e6d4c0f7ec0683ed5f2f20e64
Jyoti Narang <JNarang@hcareis.com>
2017-02-02 20:19:29 +0000

    MAT-8425 : Stand alone CQL libraries, create folder widget on CQL Library tab - Code re factoring , common part for two parts are taken out of if/else. - Each element for this widget is given unique id's for different views.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14140 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8706d2dc64d67fb36981e137cfbb500acb6442f8

--- src/mat/client/shared/CreateNewItemWidget.java
+++ src/mat/client/shared/CreateNewItemWidget.java
@@ -7,7 +7,6 @@ import org.gwtbootstrap3.client.ui.constants.ButtonType;
 import org.gwtbootstrap3.client.ui.constants.Pull;
 
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -23,78 +22,56 @@ public class CreateNewItemWidget extends Composite {
 	private ListBox options = new ListBox();
 
 	/** Default Constructor. */
-	@SuppressWarnings("deprecation")
 	public CreateNewItemWidget(String forView) {
-		
-		VerticalPanel topPanel = new VerticalPanel();
-		if (forView.equalsIgnoreCase("measureLib")) {
-			createItemButton = new Button("Create");
-			createItemButton.setType(ButtonType.PRIMARY);
-			createItemButton.setSize(ButtonSize.SMALL);
-			createItemButton.setTitle("Create");
-			createItemButton.setPull(Pull.RIGHT);
-			createItemButton.setMarginLeft(5.0);
-			createItemButton.setHeight("30px");
-			options.setWidth("200px");
-			options.setHeight("32px");
-			createItemButton.getElement().setId("createMeasure_Button");
-			options.setName("Create Measure Options");
-			DOM.setElementAttribute(options.getElement(), "id", "CreateMeasureWidget");
-			topPanel.getElement().setId("CreateMeasureWidget_verticalPanel");
-			HorizontalPanel horizontalPanel = new HorizontalPanel();
-			topPanel.getElement().setId("CreateMeasureWidget_horizontalPanel");
-			loadListBoxOptions(forView);
-			horizontalPanel.add(options);
-			horizontalPanel.add(createItemButton);
-			Label invisibleLabel = (Label) LabelBuilder.buildInvisibleLabel(
-					new Label("CreateWidgetDisplayedSelectOptionsFromComboBox"),
-					"CreateWidgetDisplayedSelectOptionsFromComboBox");
-			topPanel.add(invisibleLabel);
-			topPanel.add(horizontalPanel);
-			horizontalPanel.setStylePrimaryName("createNewItemWidget");
-			// All composites must call initWidget() in their constructors.
-			Element element = topPanel.getElement();
-			element.setAttribute("aria-role", "panel");
-			element.setAttribute("aria-labelledby", "CreateWidgetDisplayedSelectOptionsFromComboBox");
-			element.setAttribute("aria-live", "assertive");
-			element.setAttribute("aria-atomic", "true");
-			element.setAttribute("aria-relevant", "all");
-			element.setAttribute("role", "alert");
-		} else {
-			createItemButton = new Button("Create");
-			createItemButton.setType(ButtonType.PRIMARY);
-			createItemButton.setSize(ButtonSize.SMALL);
-			createItemButton.setTitle("Create");
-			createItemButton.setPull(Pull.RIGHT);
-			createItemButton.setMarginLeft(5.0);
-			createItemButton.setHeight("30px");
-			options.setWidth("200px");
-			options.setHeight("32px");
 
-			createItemButton.getElement().setId("createCQL_Button");
+		VerticalPanel topPanel = new VerticalPanel();
+		createItemButton = new Button("Create");
+		createItemButton.setType(ButtonType.PRIMARY);
+		createItemButton.setSize(ButtonSize.SMALL);
+		createItemButton.setTitle("Create");
+		createItemButton.setPull(Pull.RIGHT);
+		createItemButton.setMarginLeft(5.0);
+		createItemButton.setHeight("30px");
+		options.setWidth("200px");
+		options.setHeight("32px");
+		createItemButton.getElement().setId("createItem_Button_" + forView);
+		options.setId("CreateItemWidget_" + forView);
+		topPanel.getElement().setId("CreateItemWidget_verticalPanel_" + forView);
+		topPanel.getElement().setId("CreateItemeWidget_horizontalPanel_" + forView);
+		// Set Id's based on view.
+		if (forView.equalsIgnoreCase("measureLib")) {
+			options.setName("Create Measure Options");
+			/*
+			 * DOM.setElementAttribute(options.getElement(), "id",
+			 * "CreateMeasureWidget");
+			 */
+
+		} else {
 			options.setName("Create CQL Options");
-			DOM.setElementAttribute(options.getElement(), "id", "CreateCQLWidget");
-			topPanel.getElement().setId("CreateCQLWidget_verticalPanel");
-			HorizontalPanel horizontalPanel = new HorizontalPanel();
-			topPanel.getElement().setId("CreateCQLWidget_horizontalPanel");
-			loadListBoxOptions(forView);
-			horizontalPanel.add(options);
-			horizontalPanel.add(createItemButton);
-			Label invisibleLabel = (Label) LabelBuilder.buildInvisibleLabel(
-					new Label("CreateWidgetDisplayedSelectOptionsFromComboBox"),
-					"CreateWidgetDisplayedSelectOptionsFromComboBox");
-			topPanel.add(invisibleLabel);
-			topPanel.add(horizontalPanel);
-			horizontalPanel.setStylePrimaryName("createNewItemWidget");
-			// All composites must call initWidget() in their constructors.
-			Element element = topPanel.getElement();
-			element.setAttribute("aria-role", "panel");
-			element.setAttribute("aria-labelledby", "CreateWidgetDisplayedSelectOptionsFromComboBox");
-			element.setAttribute("aria-live", "assertive");
-			element.setAttribute("aria-atomic", "true");
-			element.setAttribute("aria-relevant", "all");
-			element.setAttribute("role", "alert");
+			/*
+			 * DOM.setElementAttribute(options.getElement(), "id",
+			 * "CreateCQLWidget");
+			 */
 		}
+
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		loadListBoxOptions(forView);
+		horizontalPanel.add(options);
+		horizontalPanel.add(createItemButton);
+		Label invisibleLabel = (Label) LabelBuilder.buildInvisibleLabel(
+				new Label("CreateWidgetDisplayedSelectOptionsFromComboBox"),
+				"CreateWidgetDisplayedSelectOptionsFromComboBox");
+		topPanel.add(invisibleLabel);
+		topPanel.add(horizontalPanel);
+		horizontalPanel.setStylePrimaryName("createNewItemWidget");
+		// All composites must call initWidget() in their constructors.
+		Element element = topPanel.getElement();
+		element.setAttribute("aria-role", "panel");
+		element.setAttribute("aria-labelledby", "CreateWidgetDisplayedSelectOptionsFromComboBox");
+		element.setAttribute("aria-live", "assertive");
+		element.setAttribute("aria-atomic", "true");
+		element.setAttribute("aria-relevant", "all");
+		element.setAttribute("role", "alert");
 		initWidget(topPanel);
 	}
 
@@ -124,7 +101,7 @@ public class CreateNewItemWidget extends Composite {
 			options.addItem(ConstantMessages.CREATE_NEW_MEASURE);
 			options.addItem(ConstantMessages.CREATE_NEW_VERSION);
 			options.addItem(ConstantMessages.CREATE_NEW_DRAFT);
-		} else if(forView.equalsIgnoreCase("cqlLib")){
+		} else if (forView.equalsIgnoreCase("cqlLib")) {
 			options.addItem(ConstantMessages.DEFAULT_SELECT);
 			options.addItem(ConstantMessages.CREATE_NEW_CQL);
 			options.addItem(ConstantMessages.CREATE_NEW_CQL_VERSION);

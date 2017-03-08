08b21cdbd4c20e7dadd3a77869fc907d9062a141
Ravi Tadepally <rtadepally@hcareis.com>
2017-03-08 19:36:56 +0000

    Text boxes text is reset to blank every time general Information Nav pill is clicked. This is done to clear the stale data from previous library or measure.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14468 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 873b61176316f5d2dbf25421d51f68c192046532

--- src/mat/client/clause/cqlworkspace/CQLGeneralInformationView.java
+++ src/mat/client/clause/cqlworkspace/CQLGeneralInformationView.java
@@ -47,10 +47,7 @@ public class CQLGeneralInformationView {
 	 * Builds the view.
 	 */
 	private void buildView() {
-		libraryNameValue.setText("");
-		libraryVersionValue.setText("");
-		usingModelValue.setText("");
-		modelVersionValue.setText("");
+		
 		VerticalPanel generalInfoTopPanel = new VerticalPanel();
 		
 		Label libraryNameLabel = new Label(LabelType.INFO, "CQL Library Name");

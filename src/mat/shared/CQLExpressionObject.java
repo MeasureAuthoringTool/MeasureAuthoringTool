814e529c5e27ed21e861b257db5de9167690be24
Jyoti Narang <JNarang@hcareis.com>
2017-03-30 14:28:49 +0000

    MAT-8536 : Server side validation for birthdate and dead codes only paired with appropriate patient characteristic MAT-8520 : Successfully package measures CQL files using includes and create outputs - Added code for assimilating each expression's used items in CQLExpressionObject which is invoked in CQLFilter. With this we can go to each expression and find used valueset/parameter/function/code/codesystem/definitions/valueset and datatype combinations used.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14637 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: a306e58cf06e3668e4d0b31cea520a64179da37d

---
+++ src/mat/shared/CQLExpressionObject.java
@@ -0,0 +1,113 @@
+package mat.shared;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class CQLExpressionObject implements IsSerializable {
+	
+	public CQLExpressionObject(String type) {
+		// TODO Auto-generated constructor stub
+		this.type = type;
+	}
+	 /**
+     * The used expressions list
+     */
+    private List<String> usedExpressions = new ArrayList<String>();
+
+    /**
+     * The used functions list
+     */
+    private List<String> usedFunctions = new ArrayList<String>();;
+
+    /**
+     * The used cql valuesets
+     */
+    private List<String> usedValuesets = new ArrayList<String>();;
+
+    /**
+     * THe used parameters list
+     */
+    private List<String> usedParameters = new ArrayList<String>();;
+
+    
+    /**
+     * The used code systems list
+     */
+    private List<String> usedCodeSystems = new ArrayList<String>();;
+    
+    /**
+     * The used codes list
+     */
+    private List<String> usedCodes = new ArrayList<String>();
+    private Map<String, List<String>> valueSetDataTypeMap = new HashMap<String,List<String>>();
+    
+    String type;
+
+	public List<String> getUsedExpressions() {
+		return usedExpressions;
+	}
+
+	public void setUsedExpressions(List<String> usedExpressions) {
+		this.usedExpressions = usedExpressions;
+	}
+
+	public List<String> getUsedFunctions() {
+		return usedFunctions;
+	}
+
+	public void setUsedFunctions(List<String> usedFunctions) {
+		this.usedFunctions = usedFunctions;
+	}
+
+	public List<String> getUsedValuesets() {
+		return usedValuesets;
+	}
+
+	public void setUsedValuesets(List<String> usedValuesets) {
+		this.usedValuesets = usedValuesets;
+	}
+
+	public List<String> getUsedParameters() {
+		return usedParameters;
+	}
+
+	public void setUsedParameters(List<String> usedParameters) {
+		this.usedParameters = usedParameters;
+	}
+
+	public List<String> getUsedCodeSystems() {
+		return usedCodeSystems;
+	}
+
+	public void setUsedCodeSystems(List<String> usedCodeSystems) {
+		this.usedCodeSystems = usedCodeSystems;
+	}
+
+	public List<String> getUsedCodes() {
+		return usedCodes;
+	}
+
+	public void setUsedCodes(List<String> usedCodes) {
+		this.usedCodes = usedCodes;
+	}
+
+	public Map<String, List<String>> getValueSetDataTypeMap() {
+		return valueSetDataTypeMap;
+	}
+
+	public void setValueSetDataTypeMap(Map<String, List<String>> valueSetDataTypeMap) {
+		this.valueSetDataTypeMap = valueSetDataTypeMap;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
---
+++ src/mat/shared/CQLObject.java
@@ -0,0 +1,46 @@
+package mat.shared;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class CQLObject implements IsSerializable {
+
+	List<CQLExpressionObject> cqlDefinitionObjectList = new ArrayList<CQLExpressionObject>();
+	List<CQLExpressionObject> cqlFunctionObjectList = new ArrayList<CQLExpressionObject>();
+	List<CQLExpressionObject> cqlParameterObjectList = new ArrayList<CQLExpressionObject>();
+
+	public List<CQLExpressionObject> getCqlDefinitionObjectList() {
+		return cqlDefinitionObjectList;
+	}
+
+	public void setCqlDefinitionObjectList(List<CQLExpressionObject> cqlDefinitionObjectList) {
+		this.cqlDefinitionObjectList = cqlDefinitionObjectList;
+	}
+
+	public List<CQLExpressionObject> getCqlFunctionObjectList() {
+		return cqlFunctionObjectList;
+	}
+
+	public void setCqlFunctionObjectList(List<CQLExpressionObject> cqlFunctionObjectList) {
+		this.cqlFunctionObjectList = cqlFunctionObjectList;
+	}
+
+	public List<CQLExpressionObject> getCqlParameterObjectList() {
+		return cqlParameterObjectList;
+	}
+
+	public void setCqlParameterObjectList(List<CQLExpressionObject> cqlParameterObjectList) {
+		this.cqlParameterObjectList = cqlParameterObjectList;
+	}
+
+	public List<CQLExpressionObject> getAllExpressionList() {
+		List<CQLExpressionObject> allExpressionList = new ArrayList<CQLExpressionObject>();
+		allExpressionList.addAll(this.getCqlDefinitionObjectList());
+		allExpressionList.addAll(this.getCqlFunctionObjectList());
+		allExpressionList.addAll(this.getCqlParameterObjectList());
+
+		return allExpressionList;
+	}
+}

d2b94bd2c7a5d87667d68963f5583ab723657aec
Chinmay Bajikar <CBajikar@telligen.com>
2017-04-07 06:33:40 +0000

    Indentation chage for CQL Body
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14711 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8cb32c463ffe6e4aaa84f62a4bf4cd53f6548f9e

--- src/mat/server/simplexml/cql/MATCssCQLUtil.java
+++ src/mat/server/simplexml/cql/MATCssCQLUtil.java
@@ -355,7 +355,7 @@ public class MATCssCQLUtil {
 			".cql-definition-body {\r\n" + 
 			"	width:800px;\r\n" + 
 			"	display: block;\r\n" + 
-			"	text-indent: 20px;\r\n" + 
+			//"	text-indent: 20px;\r\n" + 
 			"}" + 
 			"li {"
 			+ "  padding-left: 15px;"

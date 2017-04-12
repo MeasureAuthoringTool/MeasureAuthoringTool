99a41c14c6aeeb3455baceabf9b69b6e56b394a3
Jyoti Narang <JNarang@hcareis.com>
2017-04-12 19:26:34 +0000

    POC- Added Progress Bar for demo.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@14751 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: fa4fe133c9e85114e9e3fd96f202c567cda85695

--- src/mat/client/MainLayout.java
+++ src/mat/client/MainLayout.java
@@ -199,7 +199,7 @@ public abstract class MainLayout {
 		progress.add(bar);
 		progress.setId("LoadingPanel");
 		getLoadingPanel().add(progress);
-		int delay = 1500;
+		int delay = 100;
 		
 		if(delay > 0){
 			

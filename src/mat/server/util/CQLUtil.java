4cde9d6d540f97492b966ada82554d7f1cd90a2a
Ravi Tadepally <rtadepally@hcareis.com>
2017-07-06 22:28:24 +0000

    MAT- 8675 added functionality to filter out child "Measurement Period" from the insert icon and the shortcut key CQL Workspace.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@15449 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 462d2f85a1c2f4758ff2bc64eb1fe8eab62d76a3

--- src/mat/server/util/CQLUtil.java
+++ src/mat/server/util/CQLUtil.java
@@ -619,7 +619,7 @@ public class CQLUtil {
 			addNamesToList(alias, xmlProcessor, "//cqlLookUp/definitions/definition[@supplDataElement=\"false\"]/@name", cqlModel.getIncludedDefNames());
 			addNamesToList(alias, xmlProcessor, "//cqlLookUp/functions/function/@name", cqlModel.getIncludedFuncNames());
 			addNamesToList(alias, xmlProcessor, "//cqlLookUp/valuesets/valueset[@suppDataElement=\"false\"]/@name", cqlModel.getIncludedValueSetNames());
-			addNamesToList(alias, xmlProcessor, "//cqlLookUp/parameters/parameter/@name", cqlModel.getIncludedParamNames());
+			addNamesToList(alias, xmlProcessor, "//cqlLookUp/parameters/parameter[readOnly=\"false\"]/@name", cqlModel.getIncludedParamNames());
 			addNamesToList(alias, xmlProcessor, "//cqlLookUp/codes/code[@readOnly=\"false\"]/@codeName", cqlModel.getIncludedCodeNames());
 
 		}

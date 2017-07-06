08f2dfcf70ad60a8fae422c0914bfe690dbf4d95
Jyoti Narang <JNarang@hcareis.com>
2017-07-06 21:12:32 +0000

    MAT-8704 : Codes as Attributes - Attribute builder - Modified CQL_INSERT_AS_CODE constant value to "as Code = " for Codes
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@15443 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 8b45e079c0da2a606dfab9c656f98174bc855730

--- src/mat/client/clause/cqlworkspace/CQLWorkSpaceConstants.java
+++ src/mat/client/clause/cqlworkspace/CQLWorkSpaceConstants.java
@@ -190,8 +190,9 @@ public class CQLWorkSpaceConstants {
 	public static final String CQL_ATTRIBUTE_RESULT = "result";
 	public static final String CQL_ATTRIBUTE_TARGET_OUTCOME = "targetOutcome";
 	public static final String CQL_INSERT_AS_CODE_IN = " as Code in ";
-	public static final String CQL_INSERT_AS_CODE = " as Code ";
+	public static final String CQL_INSERT_AS_CODE = " as Code = ";
 	public static final String CQL_INSERT_IN = " in ";
+	public static final String CQL_EQUALS = " = ";
 	public static final String CQL_DRAFT = "Draft";
 	public static final String CQL_CODE = "CODE:";
 	

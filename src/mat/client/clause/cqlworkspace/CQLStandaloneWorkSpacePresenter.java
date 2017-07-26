922eab0a8c261a41a39f58f888a1860449135aa6
Jyoti Narang <JNarang@hcareis.com>
2017-07-26 22:08:49 +0000

    MAT-8715 : Updated value set Modify Search header title. Message is substring at 60th character. Loading progress bar fixed.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@15673 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: b0f4fa031467f6c32ac3b4f13099c4d374c14942

--- src/mat/client/clause/cqlworkspace/CQLStandaloneWorkSpacePresenter.java
+++ src/mat/client/clause/cqlworkspace/CQLStandaloneWorkSpacePresenter.java
@@ -4197,6 +4197,10 @@ public class CQLStandaloneWorkSpacePresenter implements MatPresenter {
 				isModified = true;
 				modifyValueSetDTO = result;
 				String displayName = result.getCodeListName();
+				// Substring at 60th character length.
+				if(displayName.length() >=60){
+					displayName = displayName.substring(0, 59);
+				}
 				HTML searchHeaderText = new HTML("<strong>Modify value set ( " + displayName + ")</strong>");
 				searchDisplay.getValueSetView().getSearchHeader().clear();
 				searchDisplay.getValueSetView().getSearchHeader().add(searchHeaderText);

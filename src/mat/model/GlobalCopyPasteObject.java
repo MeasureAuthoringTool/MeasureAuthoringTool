7a7771b2997c575b9756295c41d745e7b8fcef53
Jyoti Narang <JNarang@hcareis.com>
2017-11-13 17:56:22 +0000

    MAT-8886 : Paste validations and messaging - Value Sets - removed call to un-used CodeSystem List, currentMeasureId, currentLibraryId ( declaration, getter/setter)  as it is not used in further calls.
    git-svn-id: svn+ssh://subversion.telligen.com/var/www/html/repos/cmat/trunk/Development/mat@16592 de2f59c9-771b-4d1d-97da-b950f46c8601
    
    Former-commit-id: 7cf6b44fbd18c9642e6b68c1935778e0e7c823a2

--- src/mat/model/GlobalCopyPasteObject.java
+++ src/mat/model/GlobalCopyPasteObject.java
@@ -6,108 +6,14 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 import mat.model.cql.CQLCode;
-import mat.model.cql.CQLCodeSystem;
 import mat.model.cql.CQLQualityDataSetDTO;
 
 public class GlobalCopyPasteObject implements IsSerializable{
-	
-	//private List<QualityDataSetDTO> copiedQDMList = new ArrayList<QualityDataSetDTO>();
 	
 	private List<CQLQualityDataSetDTO> copiedValueSetList = new ArrayList<CQLQualityDataSetDTO>();
 	
 	private List<CQLCode> copiedCodeList = new ArrayList<CQLCode>();
 	
-	private List<CQLCodeSystem> copiedCodeSystemList = new  ArrayList<CQLCodeSystem>();
-	
-	private String currentMeasureId;
-	
-	private String currentLibraryId;
-	
-//	private List<MatValueSetTransferObject> matValueSetList;
-	/**
-	 * @return the copiedQDMList
-	 */
-	/*public List<QualityDataSetDTO> getCopiedQDMList() {
-		return copiedQDMList;
-	}
-	
-	*//**
-	 * @param copiedQDMList the copiedQDMList to set
-	 *//*
-	public void setCopiedQDMList(List<QualityDataSetDTO> copiedQDMList) {
-		this.copiedQDMList = copiedQDMList;
-	}*/
-	/**
-	 * @return the currentMeasureId
-	 */
-	public String getCurrentMeasureId() {
-		return currentMeasureId;
-	}
-	/**
-	 * @param currentMeasureId the currentMeasureId to set
-	 */
-	public void setCurrentMeasureId(String currentMeasureId) {
-		this.currentMeasureId = currentMeasureId;
-	}
-	/*public List<MatValueSetTransferObject> getMatValueSetList() {
-		return matValueSetList;
-	}
-	public void setMatValueSetList(List<MatValueSetTransferObject> matValueSetList) {
-		this.matValueSetList = matValueSetList;
-	}*/
-	/**
-	 * 
-	 */
-	/*public void setMatValueSetListFromQDS(String defaultExpansionProfile) {
-		matValueSetList = new ArrayList<MatValueSetTransferObject>();
-		for (QualityDataSetDTO dataSetDTO : copiedQDMList) {
-			MatValueSetTransferObject mvsto = new MatValueSetTransferObject();
-			mvsto.setDatatype(dataSetDTO.getDataType());
-			if (!dataSetDTO.getOid().equalsIgnoreCase(ConstantMessages.USER_DEFINED_QDM_OID)) {
-				MatValueSet currentMatValueSet = new MatValueSet();
-				currentMatValueSet.setDisplayName(dataSetDTO.getCodeListName());
-				if (!((defaultExpansionProfile != null) && (defaultExpansionProfile != ""))) {
-					if ((dataSetDTO.getExpansionIdentifier() != null) && (dataSetDTO.getExpansionIdentifier() != "")) {
-						mvsto.setExpansionProfile(true);
-						currentMatValueSet.setExpansionProfile(dataSetDTO.getExpansionIdentifier());
-					} else {
-						mvsto.setExpansionProfile(false);
-					}
-					if (dataSetDTO.getVersion().equalsIgnoreCase("1.0")) {
-						mvsto.setVersionDate(false);
-					} else {
-						mvsto.setVersionDate(true);
-						currentMatValueSet.setVersion(dataSetDTO.getVersion());
-					}
-				} else {
-					mvsto.setExpansionProfile(true);
-					currentMatValueSet.setExpansionProfile(defaultExpansionProfile);
-				}
-				if (dataSetDTO.getTaxonomy().equalsIgnoreCase(ConstantMessages.GROUPING_CODE_SYSTEM)) {
-					currentMatValueSet.setType(ConstantMessages.GROUPING_CODE_SYSTEM);
-				} else {
-					currentMatValueSet.setType(dataSetDTO.getTaxonomy());
-					MatConceptList conceptList = new MatConceptList();
-					MatConcept concept = new MatConcept();
-					concept.setCodeSystemName(dataSetDTO.getTaxonomy());
-					List<MatConcept> concepts = new ArrayList<MatConcept>();
-					concepts.add(concept);
-					conceptList.setConceptList(concepts);
-					currentMatValueSet.setConceptList(conceptList);
-				}
-				currentMatValueSet.setID(dataSetDTO.getOid());
-				mvsto.setMatValueSet(currentMatValueSet);
-				mvsto.setMeasureId(MatContext.get().getCurrentMeasureId());
-				if (dataSetDTO.getOccurrenceText() != null) {
-					mvsto.setSpecificOccurrence(true);
-				}
-			} else {
-				mvsto.setUserDefinedText(dataSetDTO.getCodeListName());
-			}
-			matValueSetList.add(mvsto);
-		}
-	}*/
-
 	public List<CQLQualityDataSetDTO> getCopiedValueSetList() {
 		return copiedValueSetList;
 	}
@@ -122,22 +28,6 @@ public class GlobalCopyPasteObject implements IsSerializable{
 
 	public void setCopiedCodeList(List<CQLCode> copiedCodeList) {
 		this.copiedCodeList = copiedCodeList;
-	}
-
-	public List<CQLCodeSystem> getCopiedCodeSystemList() {
-		return copiedCodeSystemList;
-	}
-
-	public void setCopiedCodeSystemList(List<CQLCodeSystem> copiedCodeSystemList) {
-		this.copiedCodeSystemList = copiedCodeSystemList;
-	}
-
-	public String getCurrentLibraryId() {
-		return currentLibraryId;
-	}
-
-	public void setCurrentLibraryId(String currentLibraryId) {
-		this.currentLibraryId = currentLibraryId;
 	}
 
 }

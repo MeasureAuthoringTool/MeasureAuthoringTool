643efd35899afc4a1858d407dc7395b456c8882e
Jyoti Narang <jnarang@hcareis.com>
2017-12-11 12:44:08 -0600

    MAT-8910 : GUI for Stratification - Modified PopulationDataModel.java , moved strata to constant variable.
    Former-commit-id: d1991179d2be948182388282ac9dd4a02e0dfb4d

--- src/mat/client/clause/cqlworkspace/model/PopulationDataModel.java
+++ src/mat/client/clause/cqlworkspace/model/PopulationDataModel.java
@@ -14,6 +14,7 @@ import mat.client.shared.MatContext;
 
 public class PopulationDataModel {
 	
+	private static final String TAGNAME_STRATA = "strata";
 	private static final String TAGNAME_NAME = "name";
 	private static final String TAGNAME_CQL_LOOK_UP = "cqlLookUp";
 	private static final String TAGNAME_DEFINITION = "definition";
@@ -152,7 +153,7 @@ public class PopulationDataModel {
 	}
 	
 	private void extractStratifications(Document document) {
-		NodeList strataNodeList = document.getElementsByTagName("strata"); 
+		NodeList strataNodeList = document.getElementsByTagName(TAGNAME_STRATA); 
 		
 		if(strataNodeList == null || strataNodeList.getLength() == 0) {
 			return;

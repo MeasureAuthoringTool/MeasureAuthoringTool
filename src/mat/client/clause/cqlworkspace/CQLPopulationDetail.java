72df6ee6188a13048683f022c6f2931220b0d58b
Chinmay Bajikar <CBajikar@telligen.com>
2017-12-12 15:48:53 -0600

    Fixed build issue
    Former-commit-id: 1208476d11e2e86de62b1c867340a1d55b048fa2

--- src/mat/client/clause/cqlworkspace/CQLPopulationDetail.java
+++ src/mat/client/clause/cqlworkspace/CQLPopulationDetail.java
@@ -1,11 +1,12 @@
 package mat.client.clause.cqlworkspace;
 
+import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.gwt.FlowPanel;
 
 import mat.client.clause.cqlworkspace.model.PopulationsObject;
 
 public interface CQLPopulationDetail {
-	public void addButtonClicked();
+	public Button addButtonClicked();
 	void displayPopulationDetail(FlowPanel mainFlowPanel);
 	public PopulationsObject getPopulationsObject();
 	public void setObserver(CQLPopulationObserver observer); 

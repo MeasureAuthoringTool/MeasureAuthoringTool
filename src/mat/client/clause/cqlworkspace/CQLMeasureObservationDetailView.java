2425e5ad74af658f9d992f27f388ff7ccf208697
Chinmay Bajikar <CBajikar@telligen.com>
2017-12-12 15:20:31 -0600

    MAT-8912:Human readable for measure observation not working for some scenarios. Refactor for CQLPopulationDetailView. Former-commit-id: 2ddc68b8ffe2bdce35170173bdc094c558eaf020

--- src/mat/client/clause/cqlworkspace/CQLMeasureObservationDetailView.java
+++ src/mat/client/clause/cqlworkspace/CQLMeasureObservationDetailView.java
@@ -50,129 +50,7 @@ public class CQLMeasureObservationDetailView implements CQLPopulationDetail{
 				populationsObject.getPopulationName() , populationsObject.getDisplayName(), "Save", "Add New");
 		
 		for (int i = 0; i < popClauses.size(); i++) {
-
-			PopulationClauseObject populationClauseObject = popClauses.get(i);
-					
-			// set the name of the Initial Population clause.
-			FocusPanel nameFocusPanel = new FocusPanel();
-			FormLabel nameLabel = new FormLabel();
-			nameLabel.setText(populationClauseObject.getDisplayName());
-			nameLabel.setTitle(populationClauseObject.getDisplayName());
-			nameLabel.setId("nameLabel" + i);
-			nameFocusPanel.add(nameLabel);
-
-			populationGrid.setWidget(i, 0, nameFocusPanel);
-			populationGrid.getCellFormatter().setWidth(i, 0, "230px");
-			
-			//set a listbox with all agg function names in it.
-			ListBox aggFuncListBox = new ListBox();
-			aggFuncListBox.setSize("140px", "30px");			
-			aggFuncListBox.addItem("--Select--", "");
-			aggFuncListBox.setTitle("Select Aggregate Function");
-			aggFuncListBox.setId("definitionList_" + populationClauseObject.getDisplayName());
-
-
-			//Add all Aggregate function names to aggFuncListBox 
-			PopulationWorkSpaceConstants.AggfuncNames.forEach(name -> aggFuncListBox.addItem(name, name));
-			
-			SelectElement selectAggElement = SelectElement.as(aggFuncListBox.getElement());
-			com.google.gwt.dom.client.NodeList<OptionElement> options = selectAggElement.getOptions();
-		    for (int j = 0; j < options.getLength(); j++) {
-		        options.getItem(j).setTitle(options.getItem(j).getText());
-		    }
-		    
-		    // select a definition name in the listbox
- 			for (int j = 0; j < aggFuncListBox.getItemCount(); j++) {
- 				String functionName = aggFuncListBox.getItemText(j);
- 				if (functionName.equals(populationClauseObject.getAggFunctionName())) {
- 					aggFuncListBox.setItemSelected(j, true);
- 					break;
- 				}
- 			}
-			
-			populationGrid.setWidget(i, 1, aggFuncListBox);
-
-			// Set a listbox with all function names in it.
-			ListBox functionListBox = new ListBox();
-			functionListBox.setSize("140px", "30px");			
-			functionListBox.addItem("--Select Function--", "");
-			functionListBox.setTitle("Select Function List");
-			functionListBox.setId("definitionList_" + populationClauseObject.getDisplayName());
-
-			populationDataModel.getFunctionNameList()
-							.forEach(
-										functionExpressionObject -> functionListBox.addItem(functionExpressionObject.getName(), functionExpressionObject.getUuid())
-									);
-			
-			SelectElement selectFuncElement = SelectElement.as(functionListBox.getElement());
-			com.google.gwt.dom.client.NodeList<OptionElement> funcOptions = selectFuncElement.getOptions();
-		    for (int j = 0; j < funcOptions.getLength(); j++) {
-		    	funcOptions.getItem(j).setTitle(funcOptions.getItem(j).getText());
-		    }
-			
-			// select a definition name in the listbox
-			for (int j = 0; j < functionListBox.getItemCount(); j++) {
-				String functionName = functionListBox.getItemText(j);
-				if (functionName.equals(populationClauseObject.getCqlExpressionDisplayName())) {
-					functionListBox.setItemSelected(j, true);
-					break;
-				}
-			}
-
-			populationGrid.setWidget(i, 2, functionListBox);
-
-			// button for Delete
-			Button deleteButton = new Button("Delete", IconType.TRASH, new ClickHandler() {
-
-				@Override
-				public void onClick(ClickEvent event) {
-					observer.onDeleteClick(functionListBox.getSelectedItemText());
-				}
-			} );
-			deleteButton.setType(ButtonType.LINK);
-			deleteButton.getElement().setId("deleteButton_" + populationClauseObject.getDisplayName());
-			deleteButton.setTitle("Delete");			
-			deleteButton.getElement().setAttribute("aria-label", "Delete");			
-			deleteButton.setIconSize(IconSize.LARGE);
-			deleteButton.setColor("#0964A2");
-			
-			populationGrid.setWidget(i, 3, deleteButton);
-
-			// button for View Human Readable
-			Button viewHRButton = new Button("View", IconType.BINOCULARS, new ClickHandler() {
-
-				@Override
-				public void onClick(ClickEvent event) {
-					
-					PopulationClauseObject population = new PopulationClauseObject(populationClauseObject);
-					
-					if(!functionListBox.getSelectedItemText().equals("--Select Function--")) {
-						population.setCqlExpressionDisplayName(functionListBox.getSelectedItemText());
-					}else {
-						population.setCqlExpressionDisplayName("");
-					}
-					
-					if(!aggFuncListBox.getSelectedItemText().equals("--Select--")) {
-						population.setAggFunctionName(aggFuncListBox.getSelectedItemText());
-					}else {
-						population.setAggFunctionName("");
-					}
-					
-					population.setCqlExpressionUUID(functionListBox.getSelectedValue());
-					
-					observer.onViewHRClick(population);
-				}
-			});
-			viewHRButton.setType(ButtonType.LINK);
-			viewHRButton.getElement().setId("viewHRButton_" + populationClauseObject.getDisplayName());
-			viewHRButton.setTitle("View Human Readable");
-			viewHRButton.setSize("20px", "30px");
-			viewHRButton.getElement().setAttribute("aria-label", "View Human Readable");
-			viewHRButton.setIcon(IconType.BINOCULARS);			
-			viewHRButton.setColor("black");
-						
-			populationGrid.setWidget(i, 4, viewHRButton);
-
+			populateGrid(popClauses, populationGrid, i);
 		}
 
 		ScrollPanel scrollPanel = new ScrollPanel(populationGrid);
@@ -190,6 +68,131 @@ public class CQLMeasureObservationDetailView implements CQLPopulationDetail{
 		mainFlowPanel.add(scrollPanel);
 		mainFlowPanel.add(new SpacerWidget());
 		mainFlowPanel.add(new SpacerWidget());
+	}
+
+	private void populateGrid(List<PopulationClauseObject> popClauses, Grid populationGrid, int i) {
+		PopulationClauseObject populationClauseObject = popClauses.get(i);
+				
+		// set the name of the Initial Population clause.
+		FocusPanel nameFocusPanel = new FocusPanel();
+		FormLabel nameLabel = new FormLabel();
+		nameLabel.setText(populationClauseObject.getDisplayName());
+		nameLabel.setTitle(populationClauseObject.getDisplayName());
+		nameLabel.setId("nameLabel" + i);
+		nameFocusPanel.add(nameLabel);
+
+		populationGrid.setWidget(i, 0, nameFocusPanel);
+		populationGrid.getCellFormatter().setWidth(i, 0, "230px");
+		
+		//set a listbox with all agg function names in it.
+		ListBox aggFuncListBox = new ListBox();
+		aggFuncListBox.setSize("140px", "30px");			
+		aggFuncListBox.addItem("--Select--", "");
+		aggFuncListBox.setTitle("Select Aggregate Function");
+		aggFuncListBox.setId("definitionList_" + populationClauseObject.getDisplayName());
+
+
+		//Add all Aggregate function names to aggFuncListBox 
+		PopulationWorkSpaceConstants.AggfuncNames.forEach(name -> aggFuncListBox.addItem(name, name));
+		
+		SelectElement selectAggElement = SelectElement.as(aggFuncListBox.getElement());
+		com.google.gwt.dom.client.NodeList<OptionElement> options = selectAggElement.getOptions();
+		for (int j = 0; j < options.getLength(); j++) {
+		    options.getItem(j).setTitle(options.getItem(j).getText());
+		}
+		
+		// select a definition name in the listbox
+		for (int j = 0; j < aggFuncListBox.getItemCount(); j++) {
+			String functionName = aggFuncListBox.getItemText(j);
+			if (functionName.equals(populationClauseObject.getAggFunctionName())) {
+				aggFuncListBox.setItemSelected(j, true);
+				break;
+			}
+		}
+		
+		populationGrid.setWidget(i, 1, aggFuncListBox);
+
+		// Set a listbox with all function names in it.
+		ListBox functionListBox = new ListBox();
+		functionListBox.setSize("140px", "30px");			
+		functionListBox.addItem("--Select--", "");
+		functionListBox.setTitle("Select Function List");
+		functionListBox.setId("definitionList_" + populationClauseObject.getDisplayName());
+
+		populationDataModel.getFunctionNameList().forEach(
+									functionExpressionObject -> functionListBox.addItem(functionExpressionObject.getName(), functionExpressionObject.getUuid())
+								);
+		
+		SelectElement selectFuncElement = SelectElement.as(functionListBox.getElement());
+		com.google.gwt.dom.client.NodeList<OptionElement> funcOptions = selectFuncElement.getOptions();
+		for (int j = 0; j < funcOptions.getLength(); j++) {
+			funcOptions.getItem(j).setTitle(funcOptions.getItem(j).getText());
+		}
+		
+		// select a definition name in the listbox
+		for (int j = 0; j < functionListBox.getItemCount(); j++) {
+			String functionName = functionListBox.getItemText(j);
+			if (functionName.equals(populationClauseObject.getCqlExpressionDisplayName())) {
+				functionListBox.setItemSelected(j, true);
+				break;
+			}
+		}
+
+		populationGrid.setWidget(i, 2, functionListBox);
+
+		// button for Delete
+		Button deleteButton = new Button("Delete", IconType.TRASH, new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				observer.onDeleteClick(functionListBox.getSelectedItemText());
+			}
+		} );
+		deleteButton.setType(ButtonType.LINK);
+		deleteButton.getElement().setId("deleteButton_" + populationClauseObject.getDisplayName());
+		deleteButton.setTitle("Delete");			
+		deleteButton.getElement().setAttribute("aria-label", "Delete");			
+		deleteButton.setIconSize(IconSize.LARGE);
+		deleteButton.setColor("#0964A2");
+		
+		populationGrid.setWidget(i, 3, deleteButton);
+
+		// button for View Human Readable
+		Button viewHRButton = new Button("View", IconType.BINOCULARS, new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				PopulationClauseObject population = new PopulationClauseObject(populationClauseObject);
+				
+				population.setCqlExpressionType("cqlfunction");
+				
+				if(!functionListBox.getSelectedItemText().equals("--Select--")) {
+					population.setCqlExpressionDisplayName(functionListBox.getSelectedItemText());
+					population.setCqlExpressionUUID(functionListBox.getSelectedValue());
+				}else {
+					population.setCqlExpressionDisplayName("");
+					population.setCqlExpressionUUID("");
+				}
+				
+				if(!aggFuncListBox.getSelectedItemText().equals("--Select--")) {
+					population.setAggFunctionName(aggFuncListBox.getSelectedItemText());
+				}else {
+					population.setAggFunctionName("");
+				}				
+				
+				observer.onViewHRClick(population);
+			}
+		});
+		viewHRButton.setType(ButtonType.LINK);
+		viewHRButton.getElement().setId("viewHRButton_" + populationClauseObject.getDisplayName());
+		viewHRButton.setTitle("View Human Readable");
+		viewHRButton.setSize("20px", "30px");
+		viewHRButton.getElement().setAttribute("aria-label", "View Human Readable");
+		viewHRButton.setIcon(IconType.BINOCULARS);			
+		viewHRButton.setColor("black");
+					
+		populationGrid.setWidget(i, 4, viewHRButton);
 	}
 
 	

15c055413d3dc34ba3d4c56621398259b7c0d6ee
Jack Meyer <jmeyer@telligen.com>
2018-03-22 16:08:55 -0500

    MAT-9079 fixes issue where clearing oid box would not put program list box, release list box back to their original states
    Former-commit-id: c26da57ee1dc23b6e74d12552aa80efe21ba4415

--- src/mat/client/clause/cqlworkspace/CQLWorkSpacePresenter.java
+++ src/mat/client/clause/cqlworkspace/CQLWorkSpacePresenter.java
@@ -4958,13 +4958,22 @@ public class CQLWorkSpacePresenter implements MatPresenter {
 
 			@Override
 			public void onValueChange(ValueChangeEvent<String> event) {
+				previousIsRetrieveButtonEnabled = isRetrieveButtonEnabled;
+				previousIsProgramListBoxEnabled = isProgramListBoxEnabled;
+				previousIsReleaseListBoxEnabled = isReleaseListBoxEnabled;
+				
 				searchDisplay.resetMessageDisplay();
 				isUserDefined = searchDisplay.getValueSetView().validateOIDInput(isUserDefined);
 				if (searchDisplay.getValueSetView().getOIDInput().getValue().length() <= 0 ) {
-					searchDisplay.getValueSetView().getProgramListBox().setEnabled(true);
-					searchDisplay.getValueSetView().getReleaseListBox().setEnabled(true);
-					searchDisplay.getValueSetView().getHelpBlock().setText("Program and Release selection is enabled");
-				}
+					isRetrieveButtonEnabled = true;
+					isProgramListBoxEnabled = true;
+					isReleaseListBoxEnabled = false; 
+					searchDisplay.getValueSetView().initProgramListBoxContent();
+					searchDisplay.getValueSetView().initializeReleaseListBoxContent();
+					loadPrograms();
+					alert508StateChanges();
+				} 
+				
 			}
 		});
 		

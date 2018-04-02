1de60176c9e7015b910402217b308350a339354e
CORP_USER_DOM\nhunter <nhunter@telligen.com>
2018-04-02 13:35:27 -0500

    MAT-9114 applying code review feedback
    Former-commit-id: bc67a1873e2b3868679df35989bc9d4264e31028

--- src/mat/model/CQLValueSetTransferObject.java
+++ src/mat/model/CQLValueSetTransferObject.java
@@ -5,14 +5,19 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 import mat.model.cql.CQLQualityDataSetDTO;
+import mat.server.service.jobs.CheckUserChangePasswordLimit;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class CQLValueSetTransferObject.
  */
 public class CQLValueSetTransferObject implements IsSerializable, BaseModel {
+	
+	/** The Constant logger. */
+	private static final Log logger=LogFactory.getLog(CQLValueSetTransferObject.class);
 	
 	/** The applied qdm list. */
 	List<CQLQualityDataSetDTO> appliedQDMList;
@@ -33,9 +38,6 @@ public class CQLValueSetTransferObject implements IsSerializable, BaseModel {
 	
 	/** The user defined text. */
 	String userDefinedText;
-	
-	/** The is expansion profile. */
-	//boolean isExpansionProfile;
 	
 	/** The is version. */
 	boolean isVersion;
@@ -149,24 +151,6 @@ public class CQLValueSetTransferObject implements IsSerializable, BaseModel {
 	}
 
 	/**
-	 * Checks if is expansion profile.
-	 *
-	 * @return true, if is expansion profile
-	 */
-	/*public boolean isExpansionProfile() {
-		return isExpansionProfile;
-	}*/
-
-	/**
-	 * Sets the expansion profile.
-	 *
-	 * @param isExpansionProfile the new expansion profile
-	 */
-	/*public void setExpansionProfile(boolean isExpansionProfile) {
-		this.isExpansionProfile = isExpansionProfile;
-	}
-*/
-	/**
 	 * Checks if is version.
 	 *
 	 * @return true, if is version
@@ -190,28 +174,28 @@ public class CQLValueSetTransferObject implements IsSerializable, BaseModel {
 		String markupRegExp = "<[^>]+>";
 		if ((this.getUserDefinedText() != null) && !this.getUserDefinedText().isEmpty()) {
 			String noMarkupText = this.getUserDefinedText().trim().replaceAll(markupRegExp, "");
-			System.out.println("QDM User defined name:" + noMarkupText);
+			logger.info("QDM User defined name:" + noMarkupText);
 			if (this.getUserDefinedText().trim().length() > noMarkupText.length()) {
 				this.setUserDefinedText(noMarkupText);
 			}
 		}
 		if (this.getMatValueSet() != null && this.getMatValueSet().getDisplayName()!= null) {
 			String noMarkupText = this.getMatValueSet().getDisplayName().trim().replaceAll(markupRegExp, "");
-			System.out.println("QDM VSAC Value set name:" + noMarkupText);
+			logger.info("QDM VSAC Value set name:" + noMarkupText);
 			if (this.getMatValueSet().getDisplayName().trim().length() > noMarkupText.length()) {
 				this.getMatValueSet().setDisplayName(noMarkupText);
 			}
 		}
 		if (this.getCqlQualityDataSetDTO() != null) {
 			String noMarkupText = this.getCqlQualityDataSetDTO().getCodeListName().trim().replaceAll(markupRegExp, "");
-			System.out.println("QDM To Be Modified VSAC Value set name:" + noMarkupText);
+			logger.info("QDM To Be Modified VSAC Value set name:" + noMarkupText);
 			if (this.getCqlQualityDataSetDTO().getCodeListName().trim().length() > noMarkupText.length()) {
 				this.getCqlQualityDataSetDTO().setCodeListName(noMarkupText);
 			}
 		}
 		if (this.getCodeListSearchDTO() != null) {
 			String noMarkupText = this.getCodeListSearchDTO().getName().trim().replaceAll(markupRegExp, "");
-			System.out.println("QDM To Be Modified USD Value set name:" + noMarkupText);
+			logger.info("QDM To Be Modified USD Value set name:" + noMarkupText);
 			if (this.getCodeListSearchDTO().getName().trim().length() > noMarkupText.length()) {
 				this.getCodeListSearchDTO().setName(noMarkupText);
 			}
@@ -221,9 +205,7 @@ public class CQLValueSetTransferObject implements IsSerializable, BaseModel {
 	public boolean validateModel(){
 		boolean isValid = true;
 		if(this.matValueSet != null){
-			if(this.getMatValueSet().getDisplayName()!= null && this.getMatValueSet().getDisplayName().trim().isEmpty()){
-				isValid = false;
-			} else if(this.getMatValueSet().getDisplayName()== null){
+			if(StringUtils.isBlank(this.getMatValueSet().getDisplayName())) {
 				isValid = false;
 			} else if ((StringUtils.isNotBlank(this.cqlQualityDataSetDTO.getProgram()) || StringUtils.isNotBlank(this.cqlQualityDataSetDTO.getRelease())) && this.isVersion) {
 				isValid = false;

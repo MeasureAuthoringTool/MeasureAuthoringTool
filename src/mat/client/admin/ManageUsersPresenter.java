231f2b3aa26a5f4838ac5cc650c62e786ee7fb21
Chandra Thondapu <cthondapu@hcareis.com>
2018-04-19 16:44:30 -0500

    MAT-9057. Removed duplicate clear of alert.
    Former-commit-id: 87d6016a6be44340b03340e9844c409559dbc495

--- src/mat/client/admin/ManageUsersPresenter.java
+++ src/mat/client/admin/ManageUsersPresenter.java
@@ -498,8 +498,7 @@ public class ManageUsersPresenter implements MatPresenter {
 			public void onKeyUp(KeyUpEvent event) {
 				searchDisplay.getSuccessMessageDisplay().clearAlert();
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					((Button)searchDisplay.getSearchButton()).click();
-					searchDisplay.getSuccessMessageDisplay().clearAlert();
+					((Button)searchDisplay.getSearchButton()).click();					
 				}
 			}
 		});

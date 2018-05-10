a09b8a8e4d3dba7bc22dc3b3d8711da047321ce2
edurbin <edurbin@hcareis.com>
2018-05-10 16:34:55 -0500

    MAT-9173: remove an unused import
    Former-commit-id: 43f75fe50da854505b05e1b5a6ceb1129b856cbf

--- src/mat/client/measure/metadata/MetaDataPresenter.java
+++ src/mat/client/measure/metadata/MetaDataPresenter.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.TextBox;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;

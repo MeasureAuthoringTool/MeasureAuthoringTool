7d8965e545b0c51250945078ac3709b04037fec7
Fanindra Ketkar <fketkar@hcareis.com>
2018-05-24 08:20:03 -0500

    MAT-9198 : Adding documentation for Test config
    Former-commit-id: 4074e75a0087b7f14bb0820a2d66bb376a3849dd

--- test/mat/server/service/jobs/IntegrationTestConfig.java
+++ test/mat/server/service/jobs/IntegrationTestConfig.java
@@ -5,14 +5,24 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
+/**
+ * To run this Integration Test Config please follow these steps :
+ *  
+ * Comment the 'propertyPlaceholderConfigurer' bean definition
+ * in the mat-persistence.xml (this is needed because the bean
+ * with same name in applicationContext-service.xml get overridden
+ * causing issue with properties not getting loaded for configuration
+ * 
+ * Uncomment the '2FA_AUTH_CLASS' property in MAT.properties file
+ * 
+ * Comment the 'vsacapi' bean definition. Somehow we keep on 
+ * getting the 'groovy/lang/GroovyObject' noClassDefFoundError
+ * even though Groovy is bundled within the VSAC jar
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @org.springframework.context.annotation.Configuration
 @ContextConfiguration({

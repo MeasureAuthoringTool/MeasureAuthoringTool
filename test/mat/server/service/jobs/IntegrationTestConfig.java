e11fef9880c7c3591c647c9715c031594965c266
Fanindra Ketkar <fketkar@hcareis.com>
2018-05-30 13:22:03 -0500

    Changes for jUnit Configuration
    Former-commit-id: 482eb1f57bd084593208140f81c58677572b9167

--- test/mat/server/service/jobs/IntegrationTestConfig.java
+++ test/mat/server/service/jobs/IntegrationTestConfig.java
@@ -2,6 +2,7 @@ package mat.server.service.jobs;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +32,18 @@ import org.springframework.transaction.annotation.Transactional;
 		"file:**/applicationContext-service.xml"})
 @Transactional(transactionManager="txManager")
 public class IntegrationTestConfig {
+	
+	@BeforeClass
+	public static void configInitialization() {
+		System.setProperty("VSAC_DRC_URL", "https://vsac.nlm.nih.gov/vsac");
+		System.setProperty("SERVER_TICKET_URL", "https://vsac.nlm.nih.gov/vsac/ws/Ticket");
+		System.setProperty("SERVER_SINGLE_VALUESET_URL", "https://vsac.nlm.nih.gov/vsac/ws/RetrieveValueSet?");
+		System.setProperty("SERVER_MULTIPLE_VALUESET_URL_NEW", "https://vsac.nlm.nih.gov/vsac/svs/RetrieveMultipleValueSets?");
+		System.setProperty("SERVICE_URL", "http://umlsks.nlm.nih.gov");
+		System.setProperty("PROFILE_SERVICE", "https://vsac.nlm.nih.gov/vsac/profiles");
+		System.setProperty("VERSION_SERVICE", "https://vsac.nlm.nih.gov/vsac/oid/");
+		System.setProperty("2FA_AUTH_CLASS", "mat.server.twofactorauth.DefaultOTPValidatorForUser");
+	}
 	
 	@Test
 	public void setup() {

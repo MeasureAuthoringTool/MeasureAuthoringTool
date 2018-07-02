2837c3651e7d87c3d5c98ff91e637fd809539263
Jack Meyer <jmeyer@telligen.com>
2018-07-02 13:07:56 -0500

    MAT-9282 removed poor documentation
    Former-commit-id: 2719bf44a2bba37292cb60b8c04cc78a39575ab3

--- src/mat/model/clause/MeasureExport.java
+++ src/mat/model/clause/MeasureExport.java
@@ -11,22 +11,14 @@ import org.hibernate.Hibernate;
 import mat.hibernate.HibernateConf;
 import mat.server.util.XmlProcessor;
 
-
-/**
- * The Class MeasureExport.
- */
 public class MeasureExport {
 	
-	/** The id. */
 	private String id;
 	
-	/** The simple xml. */
 	private String simpleXML;
 	
-	/** The code list. */
 	private Blob codeList;
 	
-	/** The measure. */
 	private Measure measure;
 
 	private String humanReadable;
@@ -39,104 +31,49 @@ public class MeasureExport {
 	
 	private String json; 
 	
-	/** XMLProcessor instance to be used for HQMF Export related tasks**/
 	private XmlProcessor hqmfXMLProcessor;
 	
 	private XmlProcessor simpleXMLProcessor;
 	
 	private XmlProcessor humanReadableProcessor;
-	
-	/**
-	 * Gets the id.
-	 * 
-	 * @return the id
-	 */
+
 	public String getId() {
 		return id;
 	}
-	
-	/**
-	 * Sets the id.
-	 * 
-	 * @param id
-	 *            the new id
-	 */
+
 	public void setId(String id) {
 		this.id = id;
 	}
 	
-	/**
-	 * Gets the simple xml.
-	 * 
-	 * @return the simple xml
-	 */
+
 	public String getSimpleXML() {
 		return simpleXML;
 	}
-	
-	/**
-	 * Sets the simple xml.
-	 * 
-	 * @param simpleXML
-	 *            the new simple xml
-	 */
+
 	public void setSimpleXML(String simpleXML) {
 		this.simpleXML = simpleXML;
 	}
 
-	/**
-	 * Gets the measure.
-	 * 
-	 * @return the measure
-	 */
 	public Measure getMeasure() {
 		return measure;
 	}
-	
-	/**
-	 * Sets the measure.
-	 * 
-	 * @param measure
-	 *            the new measure
-	 */
+
 	public void setMeasure(Measure measure) {
 		this.measure = measure;
 	}
-	
-	/**
-	 * Gets the code list barr.
-	 * 
-	 * @return the code list barr
-	 */
+
 	public byte[] getCodeListBarr() {
 		return toByteArray(codeList);
 	}
-	
-	/**
-	 * Sets the code list barr.
-	 * 
-	 * @param codeListBarr
-	 *            the new code list barr
-	 */
+
 	public void setCodeListBarr(byte[] codeListBarr) {
 		this.codeList = Hibernate.getLobCreator(HibernateConf.getHibernateSession()).createBlob(codeListBarr);
 	}
-	
-	  /**
-	 * Sets the code list.
-	 * 
-	 * @param codeList
-	 *            the new code list
-	 */
+
   	public void setCodeList(Blob codeList) {  
 		  this.codeList = codeList; 
 	  } 
-	  
-	  /**
-	 * Gets the code list.
-	 * 
-	 * @return the code list
-	 */
+
   	public Blob getCodeList() {  
 		  return this.codeList; 
 	  } 
@@ -180,14 +117,7 @@ public class MeasureExport {
 	public void setJson(String json) {
 		this.json = json;
 	}
-	  
-	  /**
-	 * To byte array.
-	 * 
-	 * @param fromBlob
-	 *            the from blob
-	 * @return the byte[]
-	 */
+
   	private byte[] toByteArray(Blob fromBlob) {  
 		  ByteArrayOutputStream baos = new ByteArrayOutputStream();  
 		  try {
@@ -206,20 +136,7 @@ public class MeasureExport {
 			  }  
 		  } 
 	  } 
-	  
-	  /**
-	 * To byte array impl.
-	 * 
-	 * @param fromBlob
-	 *            the from blob
-	 * @param baos
-	 *            the baos
-	 * @return the byte[]
-	 * @throws SQLException
-	 *             the sQL exception
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
+
   	private byte[] toByteArrayImpl(Blob fromBlob, ByteArrayOutputStream baos) throws SQLException, IOException {  
 		  byte[] buf = new byte[4000];  
 		  InputStream is = fromBlob.getBinaryStream();  

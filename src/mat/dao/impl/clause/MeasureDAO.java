fe167a5583684fb4f0f2fbdc84776be42f6b4099
CORP_USER_DOM\nhunter <nhunter@telligen.com>
2018-08-09 17:20:37 -0500

    MAT-9315 Updates start index
    Former-commit-id: d44f8c44e845d59d6bfe9fd4cbc38cd93f69074d

--- src/mat/dao/impl/clause/MeasureDAO.java
+++ src/mat/dao/impl/clause/MeasureDAO.java
@@ -679,7 +679,7 @@ public class MeasureDAO extends GenericDAO<Measure, String> implements mat.dao.c
 	
 	private List<Measure> fetchComponentMeasureResultListForCritera(Criteria mCriteria, MeasureSearchModel measureSearchModel, User user) {
 		mCriteria.addOrder(Order.desc("measureSet.id")).addOrder(Order.desc("draft")).addOrder(Order.desc("version"));
-		mCriteria.setFirstResult(1);
+		mCriteria.setFirstResult(0);
 		
 		@SuppressWarnings("unchecked")
 		List<Measure> measureResultList = mCriteria.list();

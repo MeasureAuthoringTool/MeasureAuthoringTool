1e1a32302dc302386eb8ce1b6f711ab457b08440
edurbin <edurbin@hcareis.com>
2018-09-05 16:04:59 -0500

    MAT-9352: remove an unused import
    Former-commit-id: 79c302d41466c78a5da9f796e904a7e0a0407e14

--- src/mat/client/admin/ManageBonnieTokenPresenter.java
+++ src/mat/client/admin/ManageBonnieTokenPresenter.java
@@ -4,7 +4,6 @@ import org.gwtbootstrap3.client.ui.Button;
 
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;

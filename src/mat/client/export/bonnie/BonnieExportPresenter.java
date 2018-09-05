42cbae4538d25f913c3e6c77149f1234085a8ddd
CORP_USER_DOM\nhunter <nhunter@telligen.com>
2018-09-05 15:34:05 -0500

    MAT-9107 rename variable name to make sense
    Former-commit-id: da8a93ad4a0f0a53b20b2410d6ed405f5dbb2d12

--- src/mat/client/export/bonnie/BonnieExportPresenter.java
+++ src/mat/client/export/bonnie/BonnieExportPresenter.java
@@ -22,7 +22,7 @@ public class BonnieExportPresenter implements MatPresenter {
 
 	private static final String SIGN_INTO_BONNIE_MESSAGE = "Please sign into Bonnie.";
 	private static final String SIGN_INTO_UMLS = "Please sign into UMLS";
-	private static final String SUCCESSFUL_LOG_OUT_OF_BONNIE_MESSAGE ="You have been disconnected from Bonnie. If you need to continue uploading measures to the Bonnie system you will need to click the login again.";
+	private static final String DISCONNECTED_FROM_BONNIE_MESSAGE ="You have been disconnected from Bonnie. If you need to continue uploading measures to the Bonnie system you will need to click the login again.";
 	public static final String UNABLE_TO_CONNECT_TO_BONNIE_MESSAGE = "Unable to complete this action at this time. Please try again. If the problem persists, contact the MAT Support Desk using the Contact Us form link at the bottom of the tool.";
 	
 	
@@ -102,7 +102,7 @@ public class BonnieExportPresenter implements MatPresenter {
 			public void onSuccess(Boolean result) {
 				
 				setVeiwAsLoggedOutOfBonnie();
-				createSuccessMessage(SUCCESSFUL_LOG_OUT_OF_BONNIE_MESSAGE);
+				createSuccessMessage(DISCONNECTED_FROM_BONNIE_MESSAGE);
 			}
 
 		});

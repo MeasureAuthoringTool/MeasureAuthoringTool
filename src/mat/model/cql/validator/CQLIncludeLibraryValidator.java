d09329e160a3c665a098bca02bf1de0545e6d181
Jack Meyer <jmeyer@telligen.com>
2018-09-11 15:29:12 -0500

    MAT-9363 fixes issue with a spelling of a variable
    Former-commit-id: 565e256b6133b32dbcc94795dc5a0e73debd0143

--- src/mat/model/cql/validator/CQLIncludeLibraryValidator.java
+++ src/mat/model/cql/validator/CQLIncludeLibraryValidator.java
@@ -9,7 +9,7 @@ import mat.shared.CQLModelValidator;
 public class CQLIncludeLibraryValidator extends Validator {
 	
 	private static final String LIBRARY_ALIAS_UNIQUE_AND_HAVE_PROPER_NAMING_CONVENTION = "Invalid Library Alias. Must be unique, start with an alpha-character or underscore followed by an alpha-numeric character(s) or underscore(s), and must not contain spaces.";
-	private static final String DUPLCIATE_CQL_KEYWORD = "A library alias can not be an exact match to a defined CQL keyword.";
+	private static final String DUPLICATE_CQL_KEYWORD = "A library alias can not be an exact match to a defined CQL keyword.";
 	private CQLModelValidator validator = new CQLModelValidator();
 
 	public void validate(CQLIncludeLibrary includedLibrary, CQLModel model) {	
@@ -56,7 +56,7 @@ public class CQLIncludeLibraryValidator extends Validator {
 	private void isCQLReservedKeyword(String aliasName) {
 		boolean isDuplicateCQLKeyword = CQLValidationUtil.isCQLReservedWord(aliasName);
 		if(isDuplicateCQLKeyword) {
-			this.getMessages().add(DUPLCIATE_CQL_KEYWORD);
+			this.getMessages().add(DUPLICATE_CQL_KEYWORD);
 			this.setValid(false);
 		}
 	}

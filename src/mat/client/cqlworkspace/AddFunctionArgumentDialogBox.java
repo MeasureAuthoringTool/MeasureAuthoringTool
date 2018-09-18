9bcee3167c935281a20b9aa6745b19cf9cf96bd3
edurbin <edurbin@hcareis.com>
2018-09-18 12:42:36 -0500

    MAT-9376: remove comments/commented out code
    Former-commit-id: ad514af25c276842685a169c1853205b438acea7

--- src/mat/client/cqlworkspace/AddFunctionArgumentDialogBox.java
+++ src/mat/client/cqlworkspace/AddFunctionArgumentDialogBox.java
@@ -37,49 +37,11 @@ import mat.shared.CQLModelValidator;
 import mat.shared.UUIDUtilClient;
 
 
-
-/**
- * @author jnarang
- *
- */
 public class AddFunctionArgumentDialogBox {
-	/**
-	 * String Message - Argument name is not unique.
-	 */
 	private static final String ARGUMENT_NAME_IS_NOT_UNIQUE = "Argument name already exists.";
-
-	/**
-	 * String Message - CQL datatype is required.
-	 */
 	private static final String MESSAGE_CQL_DATATYPE_IS_REQUIRED = "CQL datatype is required.";
-	/**
-	 * String Message - Argument name is required.
-	 */
 	private static final String MESSAGE_ARGUMENT_NAME_IS_REQUIRED = "Argument name is required.";
-	/**
-	 * String Message - Select QDM datatype..
-	 */
 	private static final String MESSAGE_SELECT_QDM_DATATYPE = "Select QDM datatype.";
-	
-	/**
-	 * Constant - SELECT
-	 */
-//	private static final String SELECT = "Select";
-	
-	/**
-	 * List of Attributes.
-	 */
-	//private static final List<String> attributeList = new ArrayList<String>();
-	/**
-	 * AttributeService Instance.
-	 */
-	/*private static QDSAttributesServiceAsync attributeService = (QDSAttributesServiceAsync) GWT
-			.create(QDSAttributesService.class);*/
-	/**
-	 * @param functionArg - CQLFunctionArgument
-	 * @param isEdit - {@link Boolean}
-	 * @param searchDisplay - {@link CQLMeasureWorkSpaceView}
-	 */
 	public static  void showArgumentDialogBox(final CQLFunctionArgument functionArg,
 			final boolean isEdit, final CQLFunctionsView cqlFunctionsView, final MessagePanel messagePanel, final boolean isEditable) {
 		List<String> allCqlDataType = MatContext.get().getCqlConstantContainer().getCqlKeywordList().getCqlDataTypeList();
@@ -115,15 +77,13 @@ public class AddFunctionArgumentDialogBox {
 		for (int i = 0; i < allCqlDataType.size(); i++) {
 			listAllDataTypes.addItem(allCqlDataType.get(i));
 		}
-		// main form
+
 		Form bodyForm = new Form();
-		// message Form group
 		final FormGroup messageFormgroup = new FormGroup();
 		final HelpBlock helpBlock = new HelpBlock();
 		messageFormgroup.add(helpBlock);
 		messageFormgroup.getElement().setAttribute("role", "alert");
 		helpBlock.setId("helpBlock");
-		// CQL Data Type Drop down Form group
 		final FormGroup dataTypeFormGroup = new FormGroup();
 		FormLabel availableDataFormLabel = new FormLabel();
 		availableDataFormLabel.setText("Available Datatypes");
@@ -133,7 +93,6 @@ public class AddFunctionArgumentDialogBox {
 		listAllDataTypes.getElement().setId("listDataType");
 		dataTypeFormGroup.add(availableDataFormLabel);
 		dataTypeFormGroup.add(listAllDataTypes);
-		// Argument Name Form group
 		final FormGroup argumentNameFormGroup = new FormGroup();
 		FormLabel argumentNameFormLabel = new FormLabel();
 		argumentNameFormLabel.setText("Argument Name");
@@ -141,13 +100,11 @@ public class AddFunctionArgumentDialogBox {
 		argumentNameFormLabel.setFor("inputArgumentName");
 		argumentNameFormLabel.setId("ArgumentNameLanel");
 		final TextBox argumentNameTextArea = new TextBox();
-		//argumentNameTextArea.setPlaceholder("Enter Argument Name");
 		argumentNameTextArea.setWidth("290px");
 		argumentNameTextArea.setHeight("38px");
 		argumentNameTextArea.getElement().setId("inputArgumentName");
 		argumentNameFormGroup.add(argumentNameFormLabel);
 		argumentNameFormGroup.add(argumentNameTextArea);
-		// Other Type Form group
 		final FormGroup otherTypeFormGroup = new FormGroup();
 		FormLabel otherTypeFormLabel = new FormLabel();
 		otherTypeFormLabel.setText("Other Type");
@@ -160,7 +117,7 @@ public class AddFunctionArgumentDialogBox {
 		otherTypeTextArea.setId("inputOtherType");
 		otherTypeFormGroup.add(otherTypeFormLabel);
 		otherTypeFormGroup.add(otherTypeTextArea);
-		// QDM Data types drop down Form group
+
 		final FormGroup selectFormGroup = new FormGroup();
 		FormLabel selectFormLabel = new FormLabel();
 		selectFormLabel.setText("Select QDM Datatype Object");
@@ -173,14 +130,14 @@ public class AddFunctionArgumentDialogBox {
 		listSelectItem.setEnabled(false);
 		selectFormGroup.add(selectFormLabel);
 		selectFormGroup.add(listSelectItem);
-		// Main form field Set
+
 		FieldSet formFieldSet = new FieldSet();
 		formFieldSet.add(messageFormgroup);
 		formFieldSet.add(argumentNameFormGroup);
 		formFieldSet.add(dataTypeFormGroup);
 		formFieldSet.add(otherTypeFormGroup);
 		formFieldSet.add(selectFormGroup);
-		//formFieldSet.add(selectAttributeFormGroup);
+
 		bodyForm.add(formFieldSet);
 		modalBody.add(bodyForm);
 		ModalFooter modalFooter = new ModalFooter();
@@ -197,7 +154,7 @@ public class AddFunctionArgumentDialogBox {
 		modalFooter.add(buttonToolBar);
 		dialogModal.add(modalBody);
 		dialogModal.add(modalFooter);
-		// Set values for Edit flow.
+
 		if (isEdit) {
 			String selectedDataType = null;
 			argumentNameTextArea.setText(functionArg.getArgumentName());
@@ -216,7 +173,6 @@ public class AddFunctionArgumentDialogBox {
 					String itemValue = listSelectItem.getItemText(i);
 					if (itemValue.equalsIgnoreCase(functionArg.getQdmDataType())) {
 						listSelectItem.setSelectedIndex(i);
-						//	attributeListBox.setEnabled(true);
 						break;
 					}
 				}
@@ -232,7 +188,7 @@ public class AddFunctionArgumentDialogBox {
 				listSelectItem.setEnabled(false);
 			}
 		}
-		// CQL Data Types List box change handler.
+
 		listAllDataTypes.addChangeHandler(new ChangeHandler() {
 			@Override
 			public void onChange(ChangeEvent event) {
@@ -246,28 +202,22 @@ public class AddFunctionArgumentDialogBox {
 					otherTypeTextArea.clear();
 					populateAllDataType(listSelectItem, allDataTypes);
 					listSelectItem.setEnabled(true);
-					/*attributeListBox.clear();
-					attributeListBox.setEnabled(false);*/
 					addButton.setEnabled(true);
 				} else if (listAllDataTypes.getValue().equalsIgnoreCase(CQLWorkSpaceConstants.CQL_OTHER_DATA_TYPE)) {
 					otherTypeTextArea.setEnabled(true);
 					otherTypeTextArea.clear();
 					listSelectItem.clear();
 					listSelectItem.setEnabled(false);
-					/*attributeListBox.clear();
-					attributeListBox.setEnabled(false);*/
 				} else {
 					listSelectItem.clear();
 					listSelectItem.setEnabled(false);
-					/*attributeListBox.clear();
-					attributeListBox.setEnabled(false);*/
 					addButton.setEnabled(true);
 					otherTypeTextArea.setEnabled(false);
 					otherTypeTextArea.clear();
 				}
 			}
 		});
-		// QDM Data Types List Box change handler.
+
 		listSelectItem.addChangeHandler(new ChangeHandler() {
 			@Override
 			public void onChange(ChangeEvent event) {
@@ -278,7 +228,7 @@ public class AddFunctionArgumentDialogBox {
 				helpBlock.setText("");
 			}
 		});
-		//Argument Text Area click handler
+
 		argumentNameTextArea.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
@@ -289,7 +239,7 @@ public class AddFunctionArgumentDialogBox {
 				helpBlock.setText("");
 			}
 		});
-		// Add Button Click Handler
+
 		addButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
@@ -304,16 +254,11 @@ public class AddFunctionArgumentDialogBox {
 					argumentName = argumentNameTextArea.getText();
 					if (selectedIndex != 0) {
 						argumentDataType = listAllDataTypes.getItemText(selectedIndex);
-						// Selected data type is QDM Data Type
+
 						if (argumentDataType.contains(CQLWorkSpaceConstants.CQL_MODEL_DATA_TYPE)) {
 							int selectedItemIndex = listSelectItem.getSelectedIndex();
 							if (selectedItemIndex != 0) {
 								qdmDataType =  listSelectItem.getItemText(selectedItemIndex);
-								//	int selectedAttributeIndex = attributeListBox.getSelectedIndex();
-								//if (selectedAttributeIndex != 0) {
-								/*	attributeName = attributeListBox.
-											getItemText(selectedAttributeIndex);*/
-								//}
 							} else {
 								isValid = false;
 								selectFormGroup.setValidationState(ValidationState.ERROR);
@@ -347,7 +292,7 @@ public class AddFunctionArgumentDialogBox {
 					helpBlock.setText(MESSAGE_ARGUMENT_NAME_IS_REQUIRED);
 					messageFormgroup.setValidationState(ValidationState.ERROR);
 				}
-				// Argument Name Validation Check
+
 				if (isValid) {
 					boolean checkIfDuplicate = cqlFunctionsView.getFunctionArgNameMap().containsKey(argumentName.toLowerCase());
 					if (checkIfDuplicate && isEdit) {
@@ -360,7 +305,6 @@ public class AddFunctionArgumentDialogBox {
 					CQLModelValidator validator = new CQLModelValidator();
 					boolean isValidName = validator.doesAliasNameFollowCQLAliasNamingConvention(argumentName);
 					if (isValidName && !checkIfDuplicate) {
-						//&& !searchDisplay.getFuncNameTxtArea().getText().equalsIgnoreCase(argumentName)){
 						isValid = true;
 						helpBlock.setText("");
 						messageFormgroup.setValidationState(ValidationState.NONE);
@@ -377,9 +321,9 @@ public class AddFunctionArgumentDialogBox {
 						messageFormgroup.setValidationState(ValidationState.ERROR);
 					}
 				}
-				// Add or Modify Argument if valid.
+
 				if (isValid && (argumentName != null) && (argumentDataType != null)) {
-					if (isEdit) { // Existing Function Argument.
+					if (isEdit) { 
 						for (int i = cqlFunctionsView.getFunctionArgumentList().size()-1; i >= 0; i--) {
 							CQLFunctionArgument currentFunctionArgument = cqlFunctionsView.
 									getFunctionArgumentList().get(i);
@@ -401,7 +345,7 @@ public class AddFunctionArgumentDialogBox {
 								break;
 							}
 						}
-					} else { // New Argument
+					} else {
 						functionArg.setArgumentName(argumentName);
 						functionArg.setArgumentType(argumentDataType);
 						functionArg.setId(UUIDUtilClient.uuid(16));
@@ -415,23 +359,17 @@ public class AddFunctionArgumentDialogBox {
 					}
 					dialogModal.hide();
 				}
-				//508 change for Function Argument section
 				cqlFunctionsView.getAddNewArgument().setFocus(true);
 			}
 		});
 		dialogModal.show();
 	}
-	/**
-	 * Populate all data type.
-	 * @param listDataType - listBox
-	 * @param allDataTypeList - List of all available QDM Data types.
-	 */
+
 	private static void populateAllDataType(final ListBoxMVP listDataType, List<String> allDataTypeList) {
 		listDataType.clear();
 		listDataType.addItem(MatContext.PLEASE_SELECT);
 		for (String dataType : allDataTypeList) {
 			listDataType.addItem(dataType);
 		}
-	}
-	
+	}	
 }

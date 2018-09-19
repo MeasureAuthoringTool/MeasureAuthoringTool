43086fb41e2817421b088d02b75303bba69860c1
CORP_USER_DOM\nhunter <nhunter@telligen.com>
2018-09-19 15:16:03 -0500

    MAT-9308 Adds required to MAT account tabs for 508
    Former-commit-id: e09529aa415c2ab3378c427c80a8a8a26d08fe57

--- src/mat/client/shared/PhoneNumberWidget.java
+++ src/mat/client/shared/PhoneNumberWidget.java
@@ -67,7 +67,7 @@ public class PhoneNumberWidget extends Composite {
 	 */
 	public PhoneNumberWidget() {
 		phoneNumber.getElement().setId("phoneNumber_TextBox");
-		phoneNumber.setTitle("PhoneNumber");
+		phoneNumber.setTitle("Phone Number Required");
 		phoneNumber.setMaxLength(12);
 		Grid phoneLayout = new Grid(2,2);
 		phoneLayout.addStyleName("leftAligned");

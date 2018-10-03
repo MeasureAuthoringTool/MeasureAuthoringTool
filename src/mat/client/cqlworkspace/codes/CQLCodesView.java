3a5ad8ac6b9a19e3450e2fb5cd3ed49c01e1c3d5
npabbisetty <npabbisetty@hcareis.com>
2018-10-03 10:01:36 -0500

    MAT-9439 Fix for the duplication Issue
    Former-commit-id: 67923a5d351e8796469fef6f7f8e33678d682f4b

--- src/mat/client/cqlworkspace/codes/CQLCodesView.java
+++ src/mat/client/cqlworkspace/codes/CQLCodesView.java
@@ -842,7 +842,7 @@ public class CQLCodesView {
 					public void update(int index, CQLCode object,
 							SafeHtml value) {
 						if ((object != null)) {
-							int indexOfSuffix = object.getCodeName().lastIndexOf("("+object.getSuffix()+")");
+							int indexOfSuffix = object.getCodeName().lastIndexOf(" ("+object.getSuffix()+")");
 							if(indexOfSuffix >0) {
 								object.setCodeName(object.getCodeName().substring(0,indexOfSuffix));
 							}

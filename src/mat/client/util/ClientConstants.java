f985c2a244d0129ff69cede6883e83abf9084be5
Jack Meyer <jmeyer@telligen.com>
2018-10-08 13:54:33 -0500

    MAT-9462 fixes UMLS login link going to a 404
    Former-commit-id: 222d29f9f66d7f7f3f1d69d088d6cdbb5c804eaa

--- src/mat/client/util/ClientConstants.java
+++ src/mat/client/util/ClientConstants.java
@@ -3,102 +3,64 @@
  */
 package mat.client.util;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class ClientConstants.
- * 
- * @author jnarang
- */
 public class ClientConstants {
 	
-	
-	
-	/** The accessibility policy url. */
 	public static String ACCESSIBILITY_POLICY_URL="https://www.emeasuretool.cms.gov/web/guest/accessibility-statement";
 	
-	/** The Constant ADMINISTRATOR. */
 	public final static String ADMINISTRATOR ="Administrator";
 	
-	/** The Constant ANCHOR_SIGN_OUT. */
 	public final static String ANCHOR_SIGN_OUT = "Sign Out";
 	
-	/** The Constant EXT_LINK_FOIA. */
 	public final static String EXT_LINK_FOIA ="https://www.cms.gov/center/freedom-of-information-act-center.html";
 	
-	/** The Constant EXT_LINK_HHS_Site. */
 	public final static String EXT_LINK_HHS_Site ="http://www.hhs.gov/";
 	
-	/** The Constant EXT_LINK_UMLS. */
-	public final static String EXT_LINK_UMLS="https://utslogin.nlm.nih.gov/cas/login/";
+	public final static String EXT_LINK_UMLS="https://utslogin.nlm.nih.gov/cas/login";
 	
-	/** The Constant HTML_LOGIN. */
 	public final static String HTML_LOGIN ="/Login.html";
 	
-	/** The Constant HTML_MAT. */
 	public final static String HTML_MAT ="/Mat.html";
 	
 	public final static String HTML_BONNIE = "/Bonnie.html";
 	
-	/** The Constant MAINLAYOUT_ALERT_TITLE. */
 	public final static String MAINLAYOUT_ALERT_TITLE ="Informational Alert";
 	
-	/** The Constant MAINLAYOUT_LOADING_WIDGET_MSG. */
 	public final static String MAINLAYOUT_LOADING_WIDGET_MSG ="Loading Please Wait...";
 	
-	/** The Constant MAINLAYOUT_SIGNOUT_WIDGET_MSG. */
 	public final static String MAINLAYOUT_SIGNOUT_WIDGET_MSG =" Signing Out Please Wait...";
 	
-	/** The Constant MSIE. */
 	public final static String MSIE ="msie";
 	
-	/** The privacypolicy url. */
 	public static String PRIVACYPOLICY_URL="http://www.cms.gov/About-CMS/Agency-Information/Aboutwebsite/Privacy-Policy.html";
 	
-	/** The termsofuse url. */
 	public static String TERMSOFUSE_URL="https://www.emeasuretool.cms.gov/web/guest/terms-of-use";
 	
-	/** The Constant TEXT_ACCESSIBILITY_POLICY. */
 	public final static String TEXT_ACCESSIBILITY_POLICY ="Accessibility Policy";
 	
-	/** The Constant TEXT_FOIA. */
 	public static final String TEXT_FOIA ="Freedom of Information Act";
 	
-	/** The Constant TEXT_HHS_LINK. */
 	public static final String TEXT_HHS_LINK = "Link to Health and Human Services home page";
 	
-	/** The Constant TEXT_PRIVACYPOLICY. */
 	public static final String TEXT_PRIVACYPOLICY = "Privacy Policy";
 	
-	/** The Constant TEXT_TERMSOFUSE. */
 	public final static String TEXT_TERMSOFUSE ="Terms Of Use";
 	
-	
-	/** The Constant TEXT_THE_TITLE. */
 	public final static String TEXT_THE_TITLE ="The Title";
 	
-	/** The Constant TEXT_USER_GUIDE. */
 	public final static String TEXT_USER_GUIDE ="User Guide";
 	
-	/** The Constant TITLE_ADMIN. */
-//	public final static String TITLE_ADMIN ="Admin";
 	public final static String TITLE_ADMIN ="Account Management";
 	
-	/** The Constant TITLE_CMS_GOV_LOGO. */
 	public final static String TITLE_CMS_GOV_LOGO="CMS.gov";
 	
-	/** The Constant TITLE_MEASURE_COMPOSER. */
 	public final static String TITLE_MEASURE_COMPOSER ="Measure Composer";
 	
-	/** The Constant TITLE_MEASURE_LIB. */
 	public final static String TITLE_MEASURE_LIB ="Measure Library";
 	
-	/** The Constant TITLE_CQL_LIB. */
 	public final static String TITLE_CQL_LIB="CQL Library";
 	
-	/** The Constant TITLE_CQL_COMPOSER. */
 	public final static String TITLE_CQL_COMPOSER="CQL Composer";
 	
-	/** The Constant TITLE_MEASURE_LIB_CHANGE_OWNERSHIP. */
 	public final static String TITLE_MEASURE_LIB_CHANGE_OWNERSHIP ="Measure Library Ownership";
 	
 	public final static String COMPOSITE_MEASURE_EDIT = "Composite Measure Edit";
@@ -107,36 +69,23 @@ public class ClientConstants {
 	
 	public final static String ADMIN_REPORTS = "Administrator Reports";
 	
-	/** The Constant TITLE_MY_ACCOUNT. */
 	public final static String TITLE_MY_ACCOUNT="MAT Account";
 	
-	/** The Constant TITLE_ADMIN_ACCOUNT. */
 	public final static String TITLE_ADMIN_ACCOUNT="Administrator Account";
 	
-	/** The Constant TITLE_UMLS. */
 	public final static String TITLE_UMLS="UMLS Account";
 	
-	/** The Constant TITLE_WELCOME. */
 	public final static String TITLE_WELCOME="WelcomeUser";
 	
-	/** The Constant TITLE_VALUE_SET_CHANGE_OWNERSHIP. */
 	public final static String TITLE_VALUE_SET_CHANGE_OWNERSHIP ="Value Set Ownership";
 	
-	/** The Constant TITLE_VALUE_SET_LIB. */
 	public final static String TITLE_VALUE_SET_LIB ="Value Set Archive";
 	
-	/** The Constant USER_STATUS_NOT_LOGGEDIN. */
 	public final static String USER_STATUS_NOT_LOGGEDIN ="NotLoggedInYet";
 	
-	/** The userguide url. */
 	public static String USERGUIDE_URL="https://www.emeasuretool.cms.gov/documents/10179/13108/MAT+User+Guide/c50cc2fb-01ee-4b97-9cd1-2e33feca8763";
 	
-	/** The Constant TEXT_CONTACT_US . */
 	public final static String TEXT_CONTACT_US ="Contact Us";
 	
-	/** The contactus url.*/
-	public static String CONTACTUS_URL = "https://www.emeasuretool.cms.gov/web/guest/contact-us";
-	
-	
-	
+	public static String CONTACTUS_URL = "https://www.emeasuretool.cms.gov/web/guest/contact-us";	
 }

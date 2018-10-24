973e565b39ef540584e61a3348f594a8248676c6
npabbisetty <npabbisetty@hcareis.com>
2018-10-24 14:13:02 -0500

    MAT-9431 removed unneeded imports and mappedby column
    Former-commit-id: 4ae57cfea817674c481b40f75c11140b4ce18b53

--- src/main/java/mat/model/UnitMatrix.java
+++ src/main/java/mat/model/UnitMatrix.java
@@ -2,10 +2,7 @@ package mat.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.google.gwt.user.client.rpc.IsSerializable;

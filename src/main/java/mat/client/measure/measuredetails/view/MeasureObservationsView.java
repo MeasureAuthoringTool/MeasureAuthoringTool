8c441da936e76a95d2297f5211366070bd28858f
edurbin <edurbin@hcareis.com>
2018-10-30 09:19:21 -0500

    MAT-9455: Add functionality to the populations navigation menu
    Former-commit-id: a213b030cf631aa84e2ed9afc2633895623a6d31

---
+++ src/main/java/mat/client/measure/measuredetails/view/MeasureObservationsView.java
@@ -0,0 +1,26 @@
+package mat.client.measure.measuredetails.view;
+
+import org.gwtbootstrap3.client.ui.gwt.FlowPanel;
+
+import com.google.gwt.user.client.ui.Widget;
+
+public class MeasureObservationsView implements ComponentDetailView {
+
+	private FlowPanel mainPanel = new FlowPanel();
+
+	public MeasureObservationsView() {
+
+	}
+
+	@Override
+	public boolean isValid() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public Widget getWidget() {
+		return mainPanel;
+	}
+
+
+}

462bbd835a64b1f48ec396ebcdee5ba20f6c89f2
CORP_USER_DOM\nhunter <nhunter@telligen.com>
2018-10-31 15:23:59 -0500

    MAT-9444 removing extra comments
    Former-commit-id: 21de7fd11580466f3662de2d29bc210d9e005caf

--- src/main/java/mat/client/shared/HorizontalSpacerWidget.java
+++ src/main/java/mat/client/shared/HorizontalSpacerWidget.java
@@ -3,14 +3,8 @@ package mat.client.shared;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.SimplePanel;
 
-/**
- * The Class SpacerWidget.
- */
 public class HorizontalSpacerWidget extends Composite {
 	
-	/**
-	 * Instantiates a new spacer widget.
-	 */
 	public HorizontalSpacerWidget() {
 		SimplePanel panel = new SimplePanel();
 		panel.getElement().setId("panel_HorizontalSimplePanel");

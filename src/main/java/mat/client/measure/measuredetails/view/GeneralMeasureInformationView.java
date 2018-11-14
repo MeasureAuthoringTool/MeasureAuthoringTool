6609bcd0bf4dd2489633cf6a9ca5059b81d098ad
Jack Meyer <jmeyer@telligen.com>
2018-11-13 18:51:36 -0600

    MAT-9478 adds finalized date, GUID, and eCQM Version Number fields to the general measure information screen
    Former-commit-id: 5b94fc8b16fbb5ef904f34885578eafeb4c2092f

--- src/main/java/mat/client/measure/measuredetails/view/GeneralMeasureInformationView.java
+++ src/main/java/mat/client/measure/measuredetails/view/GeneralMeasureInformationView.java
@@ -3,6 +3,7 @@ package mat.client.measure.measuredetails.view;
 import org.gwtbootstrap3.client.ui.FormLabel;
 import org.gwtbootstrap3.client.ui.TextBox;
 
+import com.google.gwt.user.client.ui.CellPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -10,12 +11,13 @@ import com.google.gwt.user.client.ui.Widget;
 
 import mat.client.measure.measuredetails.MeasureDetailState;
 import mat.client.measure.measuredetails.components.GeneralInformationModel;
+import mat.client.shared.SpacerWidget;
 
 public class GeneralMeasureInformationView implements ComponentDetailView{
 	private FlowPanel mainPanel = new FlowPanel();
 	private GeneralInformationModel generalInformationModel;
-	private TextBox abbrInput = new TextBox();
-	private TextBox measureNameInput = new TextBox();
+	private TextBox eCQMAbbreviatedTitleTextBox = new TextBox();
+	private TextBox measureNameTextBox = new TextBox();
 	private boolean readOnly = false;
 	public GeneralMeasureInformationView(GeneralInformationModel generalInformationModel) {
 		this.generalInformationModel = generalInformationModel;
@@ -45,39 +47,110 @@ public class GeneralMeasureInformationView implements ComponentDetailView{
 		HorizontalPanel detailPanel = new HorizontalPanel();
 		VerticalPanel leftPanel = new VerticalPanel();
 		leftPanel.getElement().getStyle().setProperty("marginRight", "10px");
+
+		buildMeasureName(leftPanel);
+		buildFinalizedDate(leftPanel);
+		buildeCQMVersionNumber(leftPanel);
+		
+		VerticalPanel rightPanel = new VerticalPanel();
+		rightPanel.getElement().getStyle().setProperty("marginLeft", "10px");
+		buildAbbreviatedName(rightPanel);
+		buildGUID(rightPanel);
+		
+		detailPanel.add(leftPanel);
+		detailPanel.add(rightPanel);
+		mainPanel.add(detailPanel);
+	}
+	
+	private void buildMeasureName(CellPanel panel) {
 		FormLabel measureNameLabel =  new FormLabel();
 		measureNameLabel.setText("Measure Name");
 		measureNameLabel.setTitle(measureNameLabel.getText());
 		measureNameLabel.setId("measureNameLabel");
 		measureNameLabel.setFor("measureNameInput");
-		measureNameInput.setId("measureNameInput");
-		measureNameInput.setText(generalInformationModel.getMeasureName());
-		leftPanel.add(measureNameLabel);
-		leftPanel.add(measureNameInput);
-		
-		VerticalPanel rightPanel = new VerticalPanel();
-		rightPanel.getElement().getStyle().setProperty("marginLeft", "10px");
-		FormLabel abbrInputLabel =  new FormLabel();
-		abbrInputLabel.setText("eCQM Abbreviated Title");
-		abbrInputLabel.setTitle(abbrInputLabel.getText());
-		abbrInputLabel.setId("eCQMAbbrTitleLabel");
-		abbrInputLabel.setFor("abbrInput");
-		rightPanel.add(abbrInputLabel);
-		abbrInput.setId("abbrInput");
-		abbrInput.setText(generalInformationModel.geteCQMAbbreviatedTitle());
-		rightPanel.add(abbrInput);
-		detailPanel.add(leftPanel);
-		detailPanel.add(rightPanel);
-		mainPanel.add(detailPanel);
+		measureNameTextBox.setId("measureNameInput");
+		measureNameTextBox.setText(generalInformationModel.getMeasureName());
+		panel.add(measureNameLabel);
+		panel.add(measureNameTextBox);
+		panel.add(new SpacerWidget());
 	}
+	
+	private void buildAbbreviatedName(CellPanel panel) {
+		FormLabel eCQMAbbreviatedTitleLabel =  new FormLabel();
+		eCQMAbbreviatedTitleLabel.setText("eCQM Abbreviated Title");
+		eCQMAbbreviatedTitleLabel.setTitle(eCQMAbbreviatedTitleLabel.getText());
+		eCQMAbbreviatedTitleLabel.setId("eCQMAbbrTitleLabel");
+		eCQMAbbreviatedTitleLabel.setFor("abbrInput");
+		panel.add(eCQMAbbreviatedTitleLabel);
+		eCQMAbbreviatedTitleTextBox.setId("abbrInput");
+		eCQMAbbreviatedTitleTextBox.setText(generalInformationModel.geteCQMAbbreviatedTitle());
+		panel.add(eCQMAbbreviatedTitleTextBox);
+		panel.add(new SpacerWidget());
+	}
+	
+	private void buildFinalizedDate(CellPanel panel) {
+		TextBox finalizedDateTextBox = new TextBox();
+		FormLabel finalizedDateLabel = new FormLabel();
+		finalizedDateLabel.setText("Finalized Date");
+		finalizedDateLabel.setTitle(finalizedDateLabel.getText());
+		panel.add(finalizedDateLabel);
+		finalizedDateLabel.setId("finalizedDateLabel");
+		finalizedDateLabel.setFor("finalizedDate");
+		finalizedDateTextBox.setId("finalizedDate");
+		finalizedDateTextBox.setReadOnly(true);
+		finalizedDateTextBox.setEnabled(false);
+		finalizedDateTextBox.setWidth("300px");
+		panel.add(finalizedDateTextBox);
+		panel.add(new SpacerWidget());
+		
+		finalizedDateTextBox.setText(generalInformationModel.getFinalizedDate());
+	}
+	
+	
+	
+	private void buildGUID(CellPanel panel) {
+		FormLabel guidLabel = new FormLabel();
+		guidLabel.setText("GUID");
+		guidLabel.setTitle(guidLabel.getText());
+		guidLabel.setId("guidLabel");
+		guidLabel.setFor("guidLabel");
+		TextBox guidTextBox = new TextBox(); 
+		guidTextBox.setId("guidLabel");
+		guidTextBox.setReadOnly(true);
+		guidTextBox.setEnabled(false);
+		guidTextBox.setWidth("300px");
+		panel.add(guidLabel);
+		panel.add(guidTextBox);
+		panel.add(new SpacerWidget());
+		
+		guidTextBox.setText(generalInformationModel.getGuid());
+	}
+	
+	private void buildeCQMVersionNumber(CellPanel panel) {
+		TextBox eCQMVersionNumberTextBox = new TextBox(); 
+		FormLabel eCQMVersionNumberLabel = new FormLabel();
+		eCQMVersionNumberLabel.setText("eCQM Version Number");
+		eCQMVersionNumberLabel.setTitle(eCQMVersionNumberLabel.getText());
+		eCQMVersionNumberLabel.setId("versionInputLabel");
+		eCQMVersionNumberLabel.setFor("versionInput");
+		eCQMVersionNumberTextBox.setReadOnly(true);
+		eCQMVersionNumberTextBox.setEnabled(false);
+		eCQMVersionNumberTextBox.setWidth("300px");
+		eCQMVersionNumberTextBox.setId("versionInput");
+		panel.add(eCQMVersionNumberLabel);
+		panel.add(eCQMVersionNumberTextBox);
+		
+		eCQMVersionNumberTextBox.setText(generalInformationModel.geteCQMVersionNumber());
+	}
+	
 
 	@Override
 	public void setReadOnly(boolean readOnly) {
 		this.readOnly = readOnly;
-		abbrInput.setReadOnly(readOnly);
-		abbrInput.setEnabled(!readOnly);
-		measureNameInput.setReadOnly(readOnly);
-		measureNameInput.setEnabled(!readOnly);
+		eCQMAbbreviatedTitleTextBox.setReadOnly(readOnly);
+		eCQMAbbreviatedTitleTextBox.setEnabled(!readOnly);
+		measureNameTextBox.setReadOnly(readOnly);
+		measureNameTextBox.setEnabled(!readOnly);
 	}
 
 	@Override

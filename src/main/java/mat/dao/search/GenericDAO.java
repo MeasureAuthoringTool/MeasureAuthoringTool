2214b90b3dd0e41852c9cdf25db98401674c8b45
Chandra Thondapu <cthondapu@hcareis.com>
2018-11-29 17:00:02 -0600

    MAT-9203:
    1. Removed org.hibernate.Criteria queries completely from MAT
    application.
    2. Changed any remaining queries to
    javax.persistence.criteria.CriteriaQuery.
    3. Enabled the Hibernate deprecation warnings.
    
    Former-commit-id: 8387499725397a7c6d9a1e6d5afd652b20fa8ffc

--- src/main/java/mat/dao/search/GenericDAO.java
+++ src/main/java/mat/dao/search/GenericDAO.java
@@ -6,19 +6,19 @@ import java.lang.reflect.Type;
 import java.text.MessageFormat;
 import java.util.List;
 
-import mat.dao.IDAO;
-import mat.dao.IQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.criterion.ProjectionList;
-import org.hibernate.criterion.Projections;
+import org.hibernate.query.Query;
 import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import mat.dao.IDAO;
 
 
 
@@ -34,20 +34,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public abstract class GenericDAO <T, ID extends Serializable> implements IDAO<T, ID> {
 	
-	/** The clazz. */
-	private Class<T> clazz; 
-	
-	/** The clazz id. */
-	private Class<ID> clazzId; 
+	private final Class<T> clazz; 
+
+	private final Class<ID> clazzId; 
 	
 	@Autowired
 	private SessionFactory sessionFactory;
 	
-	/**
-	 * Instantiates a new generic dao.
-	 */
+	@SuppressWarnings("unchecked")
 	public GenericDAO() {
-		Type[] parameters = ((ParameterizedType) getClass()
+		final Type[] parameters = ((ParameterizedType) getClass()
 				.getGenericSuperclass()).getActualTypeArguments();
 		
 		this.clazz = (Class<T>)parameters[0];
@@ -57,18 +53,14 @@ public abstract class GenericDAO <T, ID extends Serializable> implements IDAO<T,
 	/* (non-Javadoc)
 	 * @see org.ifmc.hcmf.hibernate.dao.IDAO#save(T)
 	 */
+	@Override
 	public void save(T entity) {
 		if(isEmpty(entity)) return;
-		Session session = getSessionFactory().getCurrentSession();
+		final Session session = getSessionFactory().getCurrentSession();
 		session.saveOrUpdate(entity);
 	}
 
-	/**
-	 * Save.
-	 * 
-	 * @param entities
-	 *            the entities
-	 */
+	@SuppressWarnings("unchecked")
 	public void save(T...entities){
 		if(isEmpty(entities)) return;
 		Session session = null;
@@ -94,16 +86,18 @@ public abstract class GenericDAO <T, ID extends Serializable> implements IDAO<T,
 	/* (non-Javadoc)
 	 * @see org.ifmc.hcmf.hibernate.dao.IDAO#delete(ID)
 	 */
+	@SuppressWarnings("unchecked")
+	@Override
 	public void delete(ID...ids)  {
 		//if ids is empty or full of nulls, just return
 		if(isEmpty((Object[])ids)) return;
 
-		boolean quoted = (clazzId == String.class);
+		final boolean quoted = (clazzId == String.class);
 		boolean first = true;
 		
-		StringBuilder sb = new StringBuilder();
+		final StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < ids.length; ++ i) {
-			ID id = ids[i];
+			final ID id = ids[i];
 			if(id != null) {
 				if(!first)
 					sb.append(',');
@@ -116,49 +110,39 @@ public abstract class GenericDAO <T, ID extends Serializable> implements IDAO<T,
 			}
 		}
 
-		String query = MessageFormat.format("delete from {0} sometable where sometable.id IN ({1})", clazz.getName(), sb.toString());
+		final String query = MessageFormat.format("delete from {0} sometable where sometable.id IN ({1})", clazz.getName(), sb);
 
-		Session session = getSessionFactory().getCurrentSession();
-		org.hibernate.Query q = session.createQuery(query);
+		final Session session = getSessionFactory().getCurrentSession();
+		final Query<?> q = session.createQuery(query);
 		q.executeUpdate();
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ifmc.hcmf.hibernate.dao.IDAO#delete(T)
 	 */
+	@SuppressWarnings("unchecked")
+	@Override
 	public void delete(T...entities) {
 		//if entities is empty or full of nulls, just return
 		if(isEmpty(entities)) return;
 		
-		Session session = getSessionFactory().getCurrentSession();
-		for(T entity : entities) {
+		final Session session = getSessionFactory().getCurrentSession();
+		for(final T entity : entities) {
 			if(entity != null)
 				session.delete(entity);
 		}
-	}
-			
-	
-	/* (non-Javadoc)
-	 * @see mat.dao.IDAO#count(mat.dao.IQuery)
-	 */
-	@Override
-	public long count(IQuery query) {
-		Session session = getSessionFactory().getCurrentSession();
-		Criteria criteria = query.buildCriteria(session, clazz);
-		
-		// We only wish to get the # of records matching this query
-		return (Long)criteria.setProjection(Projections.rowCount()).uniqueResult();
 	}
 
 	/* (non-Javadoc)
 	 * @see org.ifmc.hcmf.hibernate.dao.IDAO#find(ID)
 	 */
+	@Override
 	public T find(ID id)  {
-		if(isEmpty(id)) return null;
-		
-		Session session = getSessionFactory().getCurrentSession();
-		T t = (T)session.load(clazz, id);
-		return t;
+		if (isEmpty(id)) {
+			return null;
+		}
+
+		return getSessionFactory().getCurrentSession().load(clazz, id);
 	}
 	
 	
@@ -168,67 +152,13 @@ public abstract class GenericDAO <T, ID extends Serializable> implements IDAO<T,
 	@Override
 	//for look up tables
 	public List<T> find() {
-		List<T>	list = null;
-		
-		Session	session = getSessionFactory().getCurrentSession();
-		Criteria criteria = session.createCriteria(clazz);				
-		// obtain the actual records
-        list = criteria.list();
-		return list;
+		final Session	session = getSessionFactory().getCurrentSession();
+		final CriteriaBuilder cb = session.getCriteriaBuilder();
+		final CriteriaQuery<T> query = cb.createQuery(clazz);
+		query.from(clazz);
+		return session.createQuery(query).getResultList();
 	}	
-	
-	
-	
-	/* (non-Javadoc)
-	 * @see mat.dao.IDAO#find(mat.dao.IQuery)
-	 */
-	@Override
-	public List<T> find(IQuery query) {
-		List<T>	list = null;
-		
-		Session session = getSessionFactory().getCurrentSession();
-		Criteria criteria = query.buildCriteria(session, clazz);
-		// obtain the actual records
-        list = criteria.list();
-		return list;
-	}
 
-	/* (non-Javadoc)
-	 * @see mat.dao.IDAO#findID(mat.dao.IQuery)
-	 */
-	@Override
-	public List<ID> findID(IQuery query) {
-		List<ID> list = null;
-		
-		Session session = getSessionFactory().getCurrentSession();
-		Criteria criteria = query.buildCriteria(session, clazz);
-
-		criteria.setProjection(Projections.id());
-		// obtain the actual records
-        list = criteria.list();
-		return list;
-	}
-
-	/* (non-Javadoc)
-	 * @see mat.dao.IDAO#find(mat.dao.IQuery, java.lang.String[])
-	 */
-	@Override
-	public List<Object[]> find(IQuery query, String[] properties) {
-		List<Object[]> list = null;
-		
-		Session session = getSessionFactory().getCurrentSession();
-		Criteria criteria = query.buildCriteria(session, clazz);
-
-		ProjectionList projList = Projections.projectionList();
-		for(String property : properties) {
-	        projList.add(Projections.property(property));
-		}
-		criteria.setProjection(projList);
-		// obtain the actual records
-        list = criteria.list();
-		
-		return list;
-	}
 
 	/**
 	 * Rollback uncommitted.
@@ -301,7 +231,7 @@ public abstract class GenericDAO <T, ID extends Serializable> implements IDAO<T,
 	protected boolean isEmpty(Object...objects) {
 		if(objects.length == 0) return true;
 		
-		for(Object o : objects) {
+		for(final Object o : objects) {
 			if(o != null) {
 				return false;
 			}

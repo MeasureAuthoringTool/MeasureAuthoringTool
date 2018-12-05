e4a867dc243b505ed61538199c4e83d28b452691
Jack Meyer <jmeyer@telligen.com>
2018-12-04 18:55:28 -0600

    MAT-9509 adds numerator editor
    Former-commit-id: d92d6e46fb6b1e28ea8efcb717f142747c0e2164

--- src/main/java/mat/client/measure/measuredetails/observers/NumeratorObserver.java
+++ src/main/java/mat/client/measure/measuredetails/observers/NumeratorObserver.java
@@ -1,19 +1,26 @@
 package mat.client.measure.measuredetails.observers;
 
 import mat.client.measure.measuredetails.views.MeasureDetailViewInterface;
+import mat.client.measure.measuredetails.views.NumeratorView;
+import mat.shared.measure.measuredetails.models.NumeratorModel;
 
 public class NumeratorObserver implements MeasureDetailsComponentObserver {
-
+	private NumeratorView view; 
+	
 	@Override
 	public void handleValueChanged() {
-		// TODO Auto-generated method stub
-		
+		this.view.setMeasureDetailsComponentModel(updateFromView());
 	}
 
 	@Override
 	public void setView(MeasureDetailViewInterface view) {
-		// TODO Auto-generated method stub
-		
+		this.view = (NumeratorView) view; 
 	}
-
+	
+	private NumeratorModel updateFromView() {
+		NumeratorModel model = (NumeratorModel) view.getMeasureDetailsComponentModel();
+		model.setFormattedText(view.getRichTextEditor().getFormattedText());
+		model.setPlainText(view.getRichTextEditor().getPlainText());
+		return model;
+	}
 }
--- src/main/java/mat/client/measure/measuredetails/views/NumeratorView.java
+++ src/main/java/mat/client/measure/measuredetails/views/NumeratorView.java
@@ -5,15 +5,27 @@ import org.gwtbootstrap3.client.ui.gwt.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import mat.client.measure.measuredetails.observers.MeasureDetailsComponentObserver;
+import mat.client.measure.measuredetails.observers.NumeratorObserver;
 import mat.client.shared.ConfirmationDialogBox;
 import mat.client.shared.editor.RichTextEditor;
 import mat.shared.measure.measuredetails.models.MeasureDetailsComponentModel;
+import mat.shared.measure.measuredetails.models.NumeratorModel;
 
 public class NumeratorView implements MeasureDetailViewInterface {
 	private FlowPanel mainPanel = new FlowPanel();
-
+	private MeasureDetailsRichTextEditor measureDetailsRichTextEditor;
+	private NumeratorModel model;
+	private NumeratorModel originalModel;
+	private NumeratorObserver observer;
+	
 	public NumeratorView() {
 
+	}
+	
+	public NumeratorView(NumeratorModel model) {
+		this.originalModel = model; 
+		buildModel(this.originalModel);
+		buildDetailView();
 	}
 
 	@Override
@@ -34,14 +46,15 @@ public class NumeratorView implements MeasureDetailViewInterface {
 
 	@Override
 	public void buildDetailView() {
-		// TODO Auto-generated method stub
-		
+		measureDetailsRichTextEditor = new MeasureDetailsRichTextEditor(mainPanel);
+		measureDetailsRichTextEditor.getRichTextEditor().setTitle("Measure Population Exclusions View");
+		measureDetailsRichTextEditor.getRichTextEditor().setEditorText(this.model.getFormattedText());	
+		addEventHandlers();
 	}
 
 	@Override
 	public void setReadOnly(boolean readOnly) {
-		// TODO Auto-generated method stub
-		
+		this.measureDetailsRichTextEditor.setReadOnly(readOnly);		
 	}
 
 	@Override
@@ -57,14 +70,19 @@ public class NumeratorView implements MeasureDetailViewInterface {
 
 	@Override
 	public MeasureDetailsComponentModel getMeasureDetailsComponentModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.model; 
+	}
+	
+	@Override
+	public void setMeasureDetailsComponentModel(MeasureDetailsComponentModel model) {
+		this.model = (NumeratorModel) model; 	
+		this.originalModel = this.model;
+		buildModel(this.originalModel);		
 	}
 
 	@Override
 	public RichTextEditor getRichTextEditor() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.measureDetailsRichTextEditor.getRichTextEditor();
 	}
 
 	@Override
@@ -74,14 +92,15 @@ public class NumeratorView implements MeasureDetailViewInterface {
 	}
 
 	@Override
-	public void setMeasureDetailsComponentModel(MeasureDetailsComponentModel model) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void setObserver(MeasureDetailsComponentObserver observer) {
-		// TODO Auto-generated method stub
-		
+		this.observer = (NumeratorObserver) observer; 
+	}
+	
+	private void buildModel(NumeratorModel model) {
+		this.model = new NumeratorModel(model);
+	}
+	
+	private void addEventHandlers() {
+		measureDetailsRichTextEditor.getRichTextEditor().addKeyUpHandler(event -> observer.handleValueChanged());		
 	}
 }

07783a8549f85f7bef9b9975183a302e1aa74457
Jack Meyer <jmeyer@telligen.com>
2018-12-07 13:23:35 -0600

    MAT-9599 fixes off by one error in the view cql sections
    Former-commit-id: d9dae355f827c90ab06fbd79909ef9672f7ff571

--- src/main/java/mat/client/cqlworkspace/SharedCQLWorkspaceUtility.java
+++ src/main/java/mat/client/cqlworkspace/SharedCQLWorkspaceUtility.java
@@ -34,7 +34,8 @@ public class SharedCQLWorkspaceUtility {
 	
 	public static void displayMessagesForViewCQL(SaveUpdateCQLResult result, AceEditor aceEditor, MessagePanel messagePanel) {
 		String formattedName = result.getCqlModel().getFormattedName();
-		SharedCQLWorkspaceUtility.setCQLWorkspaceExceptionAnnotations(formattedName, result.getLibraryNameErrorsMap(), result.getLibraryNameWarningsMap(), aceEditor);
+		SharedCQLWorkspaceUtility.createCQLWorkspaceAnnotations(result.getLibraryNameErrorsMap().get(formattedName), ERROR_PREFIX, AceAnnotationType.ERROR, aceEditor);
+		SharedCQLWorkspaceUtility.createCQLWorkspaceAnnotations(result.getLibraryNameWarningsMap().get(formattedName), WARNING_PREFIX, AceAnnotationType.WARNING, aceEditor);
 		SharedCQLWorkspaceUtility.displayMessageBannerForViewCQL(result, messagePanel);
 	}
 

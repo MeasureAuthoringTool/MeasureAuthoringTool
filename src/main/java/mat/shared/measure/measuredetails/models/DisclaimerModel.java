4e32036d4101d516251166bd1b4fce9052dc63f8
Jack Meyer <jmeyer@telligen.com>
2018-12-09 21:24:30 -0600

    MAT-9492 adds disclaimer dirty check
    Former-commit-id: c42524d2eb4649908f8e0df740a13ef0bf45f43e

--- src/main/java/mat/shared/measure/measuredetails/models/DisclaimerModel.java
+++ src/main/java/mat/shared/measure/measuredetails/models/DisclaimerModel.java
@@ -34,4 +34,9 @@ public class DisclaimerModel extends MeasureDetailsRichTextAbstractModel impleme
 	public List<String> validateModel(MeasureDetailsModelVisitor measureDetailsModelVisitor) {
 		return measureDetailsModelVisitor.validateModel(this);
 	}
+
+	@Override
+	public boolean isDirty(MeasureDetailsModelVisitor measureDetailsModelVisitor) {
+		return measureDetailsModelVisitor.isDirty(this);
+	}
 }

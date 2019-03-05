ab58bdd36600a43b337d65948b4c36b48a5bcf2f
edurbin <edurbin@hcareis.com>
2019-03-05 10:51:25 -0600

    MAT-9728: remove the information icon
    Former-commit-id: fd2b9bf0aae7b0e91518dc3fd5fed889a0bd2ca6

--- src/main/java/mat/client/measure/measuredetails/views/MeasureDetailsRichTextEditor.java
+++ src/main/java/mat/client/measure/measuredetails/views/MeasureDetailsRichTextEditor.java
@@ -1,46 +1,25 @@
 package mat.client.measure.measuredetails.views;
 
-import org.gwtbootstrap3.client.ui.Button;
-import org.gwtbootstrap3.client.ui.ButtonGroup;
-import org.gwtbootstrap3.client.ui.DropDownMenu;
-
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
-import mat.client.buttons.InfoToolBarButton;
-import mat.client.buttons.RichTextEditorInfoDropDownMenu;
 import mat.client.shared.editor.RichTextEditor;
 
 public class MeasureDetailsRichTextEditor {
 	private RichTextEditor richTextEditor;
-	private ButtonGroup infoButtonGroup = new ButtonGroup();
-	private Button infoButton;
 	
 	public MeasureDetailsRichTextEditor(FlowPanel mainPanel) {
 		richTextEditor = new RichTextEditor();
 		HorizontalPanel infoPanel = new HorizontalPanel();
-		buildInfoButtonGroup();
-		infoPanel.add(infoButtonGroup);
 		HorizontalPanel textAreaPanel = new HorizontalPanel();
 		textAreaPanel.add(richTextEditor);
         textAreaPanel.setWidth("625px");
         mainPanel.add(infoPanel);
         mainPanel.add(textAreaPanel);
 	}
-	
-	private void buildInfoButtonGroup() {
-		infoButton = new InfoToolBarButton("");
-		DropDownMenu dropDownMenu = new RichTextEditorInfoDropDownMenu();
-		
-		infoButtonGroup.getElement().setAttribute("class", "btn-group");
-		infoButtonGroup.add(infoButton);
-		infoButtonGroup.add(dropDownMenu);
-		infoButtonGroup.getElement().setAttribute("style", "margin-top:-10px;");
-	}
 
 	public void setReadOnly(boolean readOnly) {
 		richTextEditor.setEnabled(!readOnly);
-		infoButton.setEnabled(!readOnly);
 	}
 	
 	public RichTextEditor getRichTextEditor() {

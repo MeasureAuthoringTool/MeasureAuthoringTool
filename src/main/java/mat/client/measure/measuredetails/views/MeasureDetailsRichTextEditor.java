8718320f5d2bf1b6513e264281b184d88081c2ed
CORP_USER_DOM\nhunter <nhunter@telligen.com>
2019-04-02 17:21:21 -0500

    MAT-9619 adding rich text editors into the measure details editor sections.
    Former-commit-id: d46fd7d1136996c84902e2081bc7683438abbce2

--- src/main/java/mat/client/measure/measuredetails/views/MeasureDetailsRichTextEditor.java
+++ src/main/java/mat/client/measure/measuredetails/views/MeasureDetailsRichTextEditor.java
@@ -2,16 +2,22 @@ package mat.client.measure.measuredetails.views;
 
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import mat.client.shared.editor.RichTextEditor;
+import mat.client.shared.editor.RichTextToolbar;
 
 public class MeasureDetailsRichTextEditor {
 	private RichTextEditor richTextEditor;
+	private RichTextToolbar toolBar;
 	
 	public MeasureDetailsRichTextEditor(FlowPanel mainPanel) {
 		richTextEditor = new RichTextEditor();
+		toolBar = new RichTextToolbar(richTextEditor);
+		toolBar.setWidth("100%");
 		HorizontalPanel infoPanel = new HorizontalPanel();
-		HorizontalPanel textAreaPanel = new HorizontalPanel();
+		VerticalPanel textAreaPanel = new VerticalPanel();
+		textAreaPanel.add(toolBar);
 		textAreaPanel.add(richTextEditor);
         textAreaPanel.setWidth("625px");
         mainPanel.add(infoPanel);

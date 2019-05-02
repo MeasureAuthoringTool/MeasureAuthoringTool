65adc0f46d9d388dc38043da1755d20e8e55764f
Jack Meyer <jmeyer@telligen.com>
2019-05-02 11:24:36 -0500

    MAT-9798 fixes issue with code system version not showing up in appied code tables after fixing code not found error in free text editor
    Former-commit-id: 8bde33c1d26ea8812fd8df021cfb6d0cbf8a8e09

--- src/main/java/mat/server/CQLServiceImpl.java
+++ src/main/java/mat/server/CQLServiceImpl.java
@@ -1795,6 +1795,8 @@ public class CQLServiceImpl implements CQLService {
 				previousMatchingCodes.forEach(c -> {
 					c.setSuffix(appliedCode.getSuffix());
 					c.setCodeIdentifier(appliedCode.getCodeIdentifier());
+					c.setCodeSystemName(appliedCode.getCodeSystemName());
+					c.setCodeSystemVersion(appliedCode.getCodeSystemVersion());
 					c.setIsCodeSystemVersionIncluded(appliedCode.isIsCodeSystemVersionIncluded());
 				});
 			} else {

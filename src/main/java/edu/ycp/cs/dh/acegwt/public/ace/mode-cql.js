c2545cf72705f8271975bc931ef51c0f1ce4bff4
Gabe Smith <gsmith@hcareis.com>
2019-07-02 12:42:47 -0500

    MAT-9983: highlighting error fix
    Former-commit-id: 00b1779b00e3ce545264d8fa896260d7791c05fe

--- src/main/java/edu/ycp/cs/dh/acegwt/public/ace/mode-cql.js
+++ src/main/java/edu/ycp/cs/dh/acegwt/public/ace/mode-cql.js
@@ -48,7 +48,7 @@ var SqlHighlightRules = function() {
 	        'activeDatetime|admissionSource|anatomicalLocationSite|authorDatetime|birthDatetime|category|cause|code|components|daysSupplied|denominator|diagnoses|dischargeDisposition|dosage|expiredDatetime|' +
 	        'facilityLocation|facilityLocations|frequency|high|id|incisionDatetime|lengthOfStay|locationPeriod|low|medium|method|negationRationale|numerator|participationPeriod|performer|prevalencePeriod|' +
 	        'priority|prescriber|reason|recipient|receivedDatetime|recorder|referenceRange|refills|relatedTo|relationship|relevantDatetime|relevantPeriod|reporter|result|resultDatetime|requester|route|sender|statusDate|sentDatetime|setting|severity|status|supply|targetOutcome|type|' + 
-	        'Entity|PatientEntity|Organization|Practitioner|CarePartner'
+	        'Entity|PatientEntity|Organization|participant|Practitioner|CarePartner'
 	    );
 	    
     var keywordMapper = this.createKeywordMapper({

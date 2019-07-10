afa362d750909f2538a960a47dee2f68f56d7ad6
Gabe Smith <gsmith@hcareis.com>
2019-07-10 14:23:17 -0500

    MAT-9934: fix for issue on code names not identifying
    Former-commit-id: 3fb9e1f58e3d38997e1eee3b1da2051d61eddc1f

--- src/main/java/mat/server/util/CQLValidationUtil.java
+++ src/main/java/mat/server/util/CQLValidationUtil.java
@@ -45,7 +45,7 @@ public class CQLValidationUtil {
 		    	identifiersSet.add(dto.getName());
 		    }
 	    	for (CQLCode code : cqlModel.getCodeList()) {
-	    		if (identifiersSet.contains(code.getName())) {
+	    		if (identifiersSet.contains(code.getDisplayName())) {
 	    			return true;
 	    		}
     			identifiersSet.add(code.getName());

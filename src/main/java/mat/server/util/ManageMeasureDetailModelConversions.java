5b3a347babe623950b93988d66357d7c4c085f15
CORP_USER_DOM\nhunter <nhunter@telligen.com>
2019-07-23 11:30:54 -0500

    Fixing Measure Steward save
    Former-commit-id: 5d3a34c44ca56444f54c1999bc3490ae7ae41e1a

--- src/main/java/mat/server/util/ManageMeasureDetailModelConversions.java
+++ src/main/java/mat/server/util/ManageMeasureDetailModelConversions.java
@@ -228,7 +228,7 @@ public class ManageMeasureDetailModelConversions {
 	private void createMeasureSteward(Measure measure, ManageMeasureDetailModel measureDetailModel, OrganizationDAO organizationDao) {
 		if(measure.getMeasureStewardId() != null) {
 			Organization stewardOrg = organizationDao.findById(measure.getMeasureStewardId());
-			measureDetailModel.setStewardId(stewardOrg.getOrganizationOID());
+			measureDetailModel.setStewardId(stewardOrg.getId().toString());
 			measureDetailModel.setStewardValue(stewardOrg.getOrganizationName());
 		}
 	}

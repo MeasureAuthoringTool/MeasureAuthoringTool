8071c8d87a162159c4c547ef7659dd001ebd54fe
Jack Meyer <jmeyer@telligen.com>
2019-08-19 14:16:14 -0500

    MAT-10032 changes to display name instead of email in cases where it's possible
    Former-commit-id: f54d301f0b18916c71f1723cefd4997d89555cef

--- src/main/java/mat/server/service/impl/MeasureAuditServiceImpl.java
+++ src/main/java/mat/server/service/impl/MeasureAuditServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import mat.DTO.AuditLogDTO;
 import mat.DTO.SearchHistoryDTO;
 import mat.dao.CodeListAuditLogDAO;
 import mat.dao.MeasureAuditLogDAO;
@@ -12,8 +13,10 @@ import mat.dao.QualityDataSetDAO;
 import mat.dao.clause.MeasureDAO;
 import mat.model.ListObject;
 import mat.model.QualityDataSet;
+import mat.model.User;
 import mat.model.clause.Measure;
 import mat.server.service.MeasureAuditService;
+import mat.server.service.UserService;
 
 /**
  * Service implementation for Measure Audit Service.
@@ -35,6 +38,9 @@ public class MeasureAuditServiceImpl implements MeasureAuditService{
 	/** The quality data set dao. */
 	@Autowired
 	private QualityDataSetDAO qualityDataSetDAO; 
+	
+	@Autowired
+	private UserService userService;
 	
 	/* Records the custom measure event to the MeasureAuditLog table.
 	 * @see mat.server.service.MeasureAuditService#recordMeasureEvent(java.lang.String, java.lang.String, java.lang.String)
@@ -69,6 +75,16 @@ public class MeasureAuditServiceImpl implements MeasureAuditService{
 	 */
 	@Override
 	public SearchHistoryDTO executeSearch(String measureId, int startIndex, int numberOfRows,List<String> filterList){
-		return measureAuditLogDAO.searchHistory(measureId, startIndex, numberOfRows,filterList);
+		SearchHistoryDTO history = measureAuditLogDAO.searchHistory(measureId, startIndex, numberOfRows,filterList);
+		List<AuditLogDTO> dtos = history.getLogs();
+		dtos.forEach(d -> {
+			// in this case, the user id is the email.
+			User user = userService.findByEmailID(d.getUserId());
+			if(user != null) {
+				d.setUserId(user.getFullName());
+			}
+		});
+		
+		return history;
 	}	
 }

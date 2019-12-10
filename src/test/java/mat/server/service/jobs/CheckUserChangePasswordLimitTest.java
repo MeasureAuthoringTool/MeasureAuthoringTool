aa96808146e047eea94bc55e531f33371732b060
serhii.ilin <serhii.ilin@semanticbits.com>
2019-12-10 16:38:48 -0500

    MAT-86 1. Upgrade Junit to Jupiter version 2. Add required test libraries including power mockito 3. Resolve majority of unit test failures.

--- src/test/java/mat/server/service/jobs/CheckUserChangePasswordLimitTest.java
+++ src/test/java/mat/server/service/jobs/CheckUserChangePasswordLimitTest.java
@@ -1,39 +1,40 @@
 package mat.server.service.jobs;
 
-import static org.junit.Assert.assertNotNull;
-
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
-import mat.config.TestContextConfiguration;
+import mat.config.MatAppContextTest;
 import mat.dao.UserDAO;
 
-public class CheckUserChangePasswordLimitTest extends TestContextConfiguration {
-	@Autowired UserDAO userDAO;
-	@Autowired JavaMailSender mailSender;
-	@Autowired SimpleMailMessage simpleMailMessage;
-	@Autowired CheckUserChangePasswordLimit checkUserChangePasswordLimit;
-	
-	@Before
-	public void setup() {
-		checkUserChangePasswordLimit.setUserDAO(userDAO);
-		checkUserChangePasswordLimit.setMailSender(mailSender);
-		checkUserChangePasswordLimit.setSimpleMailMessage(simpleMailMessage);
-	}
-	
-	@Test
-	public void verifySpringInitialization() {
-		assertNotNull("CheckUserChangePasswordLimit bean is not null", checkUserChangePasswordLimit);
-	}
-	
-	@Ignore("We don't want to send emails to all users. "
-			+ "Comment the ignore and add logic for specific user when testing")
-	@Test
-	public void testWarningEmail() {
-		checkUserChangePasswordLimit.CheckUserPasswordLimitDays();
-	}
+import static org.junit.Assert.assertNotNull;
+
+public class CheckUserChangePasswordLimitTest extends MatAppContextTest {
+    @MockBean
+    UserDAO userDAO;
+    @Autowired
+    JavaMailSender mailSender;
+    @Autowired
+    SimpleMailMessage simpleMailMessage;
+    @Autowired
+    CheckUserChangePasswordLimit checkUserChangePasswordLimit;
+
+    @BeforeEach
+    public void before() {
+        // Autowiring is not working here due to a mix of xml/java config and missing annotations.
+        checkUserChangePasswordLimit.setUserDAO(userDAO);
+    }
+
+    @Test
+    public void verifySpringInitialization() {
+        assertNotNull("CheckUserChangePasswordLimit bean is not null", checkUserChangePasswordLimit);
+    }
+
+    @Test
+    public void testWarningEmail() {
+        checkUserChangePasswordLimit.checkUserPasswordLimitDays();
+    }
 }

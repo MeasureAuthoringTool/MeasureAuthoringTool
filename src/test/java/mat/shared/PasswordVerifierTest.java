b1bf48f8faf9a61aa5e30fe460d37f15b99e9d21
carson.day <carson.day@semanticbits.com>
2020-02-12 15:31:09 -0500

    Fixed import requirement issue.

--- src/test/java/mat/shared/PasswordVerifierTest.java
+++ src/test/java/mat/shared/PasswordVerifierTest.java
@@ -2,7 +2,9 @@ package mat.shared;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class PasswordVerifierTest {

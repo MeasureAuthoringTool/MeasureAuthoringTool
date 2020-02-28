30282c1e1c954a8085356e6cf4daeb46b4efaa06
Joe Kotanchik <joseph.kotanchik@semanticbits.com>
2020-02-28 16:03:33 -0500

    Revert -- committed wrong file
    This reverts commit 1c0b054560abfa80be61dbc317535e8ebb9308be.

--- src/test/java/mat/config/MatAppContextTest.java
+++ src/test/java/mat/config/MatAppContextTest.java
@@ -1,6 +1,5 @@
 package mat.config;
 
-import mat.server.service.jobs.CheckUserLastLoginTask;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,6 +25,9 @@ import static org.junit.Assert.assertNotNull;
 @Import(MatTestConfig.class)
 public class MatAppContextTest {
 
+    @Autowired
+    private CheckUserChangePasswordLimit checkUserChangePasswordLimit;
+
     @BeforeAll
     public static void configInitialization() {
         // Multiple services directly query system properties, instead of spring env properties.
@@ -36,6 +38,12 @@ public class MatAppContextTest {
         System.setProperty("SERVICE_URL", "http://umlsks.nlm.nih.gov");
         System.setProperty("PROFILE_SERVICE", "https://vsac.nlm.nih.gov/vsac/profiles");
         System.setProperty("VERSION_SERVICE", "https://vsac.nlm.nih.gov/vsac/oid/");
+    }
+
+    @Test
+    public void setup() {
+        // Successful Spring Initialization
+        assertNotNull("CheckUserChangePasswordLimit bean created successfully", checkUserChangePasswordLimit);
     }
 
 }

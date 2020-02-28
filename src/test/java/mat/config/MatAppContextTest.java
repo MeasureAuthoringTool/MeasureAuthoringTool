1c0b054560abfa80be61dbc317535e8ebb9308be
Joe Kotanchik <joseph.kotanchik@semanticbits.com>
2020-02-28 15:59:15 -0500

    Added test case for dedup warning emails

--- src/test/java/mat/config/MatAppContextTest.java
+++ src/test/java/mat/config/MatAppContextTest.java
@@ -1,5 +1,6 @@
 package mat.config;
 
+import mat.server.service.jobs.CheckUserLastLoginTask;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,9 +26,6 @@ import static org.junit.Assert.assertNotNull;
 @Import(MatTestConfig.class)
 public class MatAppContextTest {
 
-    @Autowired
-    private CheckUserChangePasswordLimit checkUserChangePasswordLimit;
-
     @BeforeAll
     public static void configInitialization() {
         // Multiple services directly query system properties, instead of spring env properties.
@@ -38,12 +36,6 @@ public class MatAppContextTest {
         System.setProperty("SERVICE_URL", "http://umlsks.nlm.nih.gov");
         System.setProperty("PROFILE_SERVICE", "https://vsac.nlm.nih.gov/vsac/profiles");
         System.setProperty("VERSION_SERVICE", "https://vsac.nlm.nih.gov/vsac/oid/");
-    }
-
-    @Test
-    public void setup() {
-        // Successful Spring Initialization
-        assertNotNull("CheckUserChangePasswordLimit bean created successfully", checkUserChangePasswordLimit);
     }
 
 }

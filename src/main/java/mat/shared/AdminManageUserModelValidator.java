fcd6ee573c21fcf9c54f69626997bee0640a5e69
Serhii Ilin <serhii.ilin@semanticbits.com>
2020-05-01 10:57:16 -0400

    MAT-900 admin harp id - add unit tests

--- src/main/java/mat/shared/AdminManageUserModelValidator.java
+++ src/main/java/mat/shared/AdminManageUserModelValidator.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package mat.shared;
 
 import java.util.ArrayList;
@@ -20,46 +17,46 @@ public class AdminManageUserModelValidator {
     /**
      * Checks if is valid users detail.
      *
-     * @param model
-     *            the model
+     * @param model the model
      * @return the list
      */
     public List<String> isValidUsersDetail(ManageUsersDetailModel model) {
         List<String> message = new ArrayList<>();
-        if ("".equals(model.getFirstName().trim())) {
+        if (model.getFirstName() == null || model.getFirstName().trim().isEmpty()) {
             message.add(MatContext.get().getMessageDelegate().getFirstNameRequiredMessage());
         }
-        if ("".equals(model.getLastName().trim())) {
+        if (model.getLastName() == null || model.getLastName().trim().isEmpty()) {
             message.add(MatContext.get().getMessageDelegate().getLastNameRequiredMessage());
         }
-        if ("".equals(model.getEmailAddress().trim())) {
+        if (model.getEmailAddress() == null || model.getEmailAddress().trim().isEmpty()) {
             message.add(MatContext.get().getMessageDelegate().getEmailIdRequired());
         }
-        if ("".equals(model.getPhoneNumber().trim())) {
+        if (model.getPhoneNumber() == null || model.getPhoneNumber().trim().isEmpty()) {
             message.add(MatContext.get().getMessageDelegate().getPhoneRequiredMessage());
         }
-        if (!model.isExistingUser() && "".equals(model.getHarpId().trim())) {
+        if (!model.isExistingUser() && (model.getHarpId() == null || model.getHarpId().trim().isEmpty())) {
             message.add(MatContext.get().getMessageDelegate().getHarpIdRequiredMessage());
         }
-
+        int numCount = 0;
         String phoneNum = model.getPhoneNumber();
-        int i, numCount;
-        numCount = 0;
-        for (i = 0; i < phoneNum.length(); i++) {
-            if (Character.isDigit(phoneNum.charAt(i))) {
-                numCount++;
+        if (phoneNum != null) {
+            int i;
+            for (i = 0; i < phoneNum.length(); i++) {
+                if (Character.isDigit(phoneNum.charAt(i))) {
+                    numCount++;
+                }
             }
         }
         if (numCount != 10) {
             message.add(MatContext.get().getMessageDelegate().getPhoneTenDigitMessage());
         }
 
-        if ("".equals(model.getOrganizationId().trim())) {
+        if (model.getOrganizationId() == null || model.getOrganizationId().trim().isEmpty()) {
             if (model.isActive()) {
                 message.add(MatContext.get().getMessageDelegate().getOrgRequiredMessage());
             }
         }
-        if ("".equals(model.getOid().trim())) {
+        if (model.getOid() == null || model.getOid().trim().isEmpty()) {
             if (model.isActive()) {
                 message.add(MatContext.get().getMessageDelegate().getOIDRequiredMessage());
             }
@@ -67,10 +64,10 @@ public class AdminManageUserModelValidator {
 		/*if("".equals(model.getRootOid().trim())) {
 			message.add(MatContext.get().getMessageDelegate().getRootOIDRequiredMessage());
 		}*/
-        if (model.getFirstName().length() < 2) {
+        if (model.getFirstName() == null || model.getFirstName().length() < 2) {
             message.add(MatContext.get().getMessageDelegate().getFirstMinMessage());
         }
-        if (model.getOid().length() > 50) {
+        if (model.getOid() != null && model.getOid().length() > 50) {
             message.add(MatContext.get().getMessageDelegate().getOIDTooLongMessage());
         }
 		/*if(model.getRootOid().length() > 50) {

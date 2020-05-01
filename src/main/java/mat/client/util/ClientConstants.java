1a4317190651aa13dfb71e16330fbe9e03e81bdf
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2020-05-01 17:44:26 -0400

    Updates to user profile page

--- src/main/java/mat/client/util/ClientConstants.java
+++ src/main/java/mat/client/util/ClientConstants.java
@@ -1,91 +1,91 @@
 /**
- * 
+ *
  */
 package mat.client.util;
 
 public class ClientConstants {
-	
+
 	public static String ACCESSIBILITY_POLICY_URL="https://www.emeasuretool.cms.gov/web/guest/accessibility-statement";
-	
+
 	public final static String ADMINISTRATOR ="Administrator";
-	
+
 	public final static String ANCHOR_SIGN_OUT = "Sign Out";
-	
+
 	public final static String EXT_LINK_FOIA ="https://www.cms.gov/center/freedom-of-information-act-center.html";
-	
+
 	public final static String EXT_LINK_HHS_Site ="http://www.hhs.gov/";
-	
+
 	public final static String EXT_LINK_UMLS="https://utslogin.nlm.nih.gov/cas/login";
-	
+
 	public final static String HTML_LOGIN ="/Login.html";
-	
+
 	public final static String HTML_MAT ="/Mat.html";
-	
+
 	public final static String HTML_BONNIE = "/Bonnie.html";
-	
+
 	public final static String MAINLAYOUT_ALERT_TITLE ="Informational Alert";
-	
+
 	public final static String MAINLAYOUT_LOADING_WIDGET_MSG ="Loading Please Wait...";
-	
+
 	public final static String MAINLAYOUT_SIGNOUT_WIDGET_MSG =" Signing Out Please Wait...";
-	
+
 	public final static String MSIE ="msie";
-	
+
 	public static String PRIVACYPOLICY_URL="http://www.cms.gov/About-CMS/Agency-Information/Aboutwebsite/Privacy-Policy.html";
-	
+
 	public static String TERMSOFUSE_URL="https://www.emeasuretool.cms.gov/web/guest/terms-of-use";
-	
+
 	public final static String TEXT_ACCESSIBILITY_POLICY ="Accessibility Policy";
-	
+
 	public static final String TEXT_FOIA ="Freedom of Information Act";
-	
+
 	public static final String TEXT_HHS_LINK = "Link to Health and Human Services home page";
-	
+
 	public static final String TEXT_PRIVACYPOLICY = "Privacy Policy";
-	
+
 	public final static String TEXT_TERMSOFUSE ="Terms Of Use";
-	
+
 	public final static String TEXT_THE_TITLE ="The Title";
-	
+
 	public final static String TEXT_USER_GUIDE ="User Guide";
-	
+
 	public final static String TITLE_ADMIN ="Account Management";
-	
+
 	public final static String TITLE_CMS_GOV_LOGO="CMS.gov";
-	
+
 	public final static String TITLE_MEASURE_COMPOSER ="Measure Composer";
-	
+
 	public final static String TITLE_MEASURE_LIB ="Measure Library";
-	
+
 	public final static String TITLE_CQL_LIB="CQL Library";
-	
+
 	public final static String TITLE_CQL_COMPOSER="CQL Composer";
-	
+
 	public final static String TITLE_MEASURE_LIB_CHANGE_OWNERSHIP ="Measure Library Ownership";
-	
+
 	public final static String COMPOSITE_MEASURE_EDIT = "Composite Measure Edit";
-	
+
 	public final static String CQL_LIBRARY_OWNERSHIP = "CQL Library Ownership";
-	
+
 	public final static String ADMIN_REPORTS = "Administrator Reports";
-	
+
 	public final static String TITLE_MY_ACCOUNT="MAT Account";
-	
+
 	public final static String TITLE_ADMIN_ACCOUNT="Administrator Account";
-	
+
 	public final static String TITLE_UMLS="UMLS Account";
-	
+
 	public final static String TITLE_WELCOME="WelcomeUser";
-	
+
 	public final static String TITLE_VALUE_SET_CHANGE_OWNERSHIP ="Value Set Ownership";
-	
+
 	public final static String TITLE_VALUE_SET_LIB ="Value Set Archive";
-	
+
 	public final static String USER_STATUS_NOT_LOGGEDIN ="NotLoggedInYet";
-	
-	public static String USERGUIDE_URL="https://www.emeasuretool.cms.gov/documents/10179/13108/MAT+User+Guide/c50cc2fb-01ee-4b97-9cd1-2e33feca8763";
-	
+
+	public static String USERGUIDE_URL="https://www.emeasuretool.cms.gov/sites/default/files/2020-03/MAT%20User%20Guide.pdf";
+
 	public final static String TEXT_CONTACT_US ="Contact Us";
-	
-	public static String CONTACTUS_URL = "https://www.emeasuretool.cms.gov/web/guest/contact-us";	
+
+	public static String CONTACTUS_URL = "https://www.emeasuretool.cms.gov/web/guest/contact-us";
 }

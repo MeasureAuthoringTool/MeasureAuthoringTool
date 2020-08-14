ffcb8655058776975b09c42d8b297cb5246be95f
carson.day <carson.day@semanticbits.com>
2020-08-14 16:04:17 -0400

    bugfix Reformatted request/response logging a bit to be more readable.

--- src/main/java/mat/server/logging/ServletLogging.java
+++ src/main/java/mat/server/logging/ServletLogging.java
@@ -1,6 +1,5 @@
 package mat.server.logging;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 
@@ -8,53 +7,47 @@ import java.net.URI;
 
 public final class ServletLogging {
     private static final Log log = LogFactory.getLog(ServletLogging.class);
+
     private ServletLogging() {
     }
 
     public static void logIncomingRequest(String requestURI, String queryString, String method, String headers, String body) {
-        log.info("Incoming Request Uri: " + requestURI);
-
-        if (StringUtils.isNotBlank(queryString)) {
-            log.info("QueryString: " + queryString);
-        }
-
-        log.info("Method: " + method);
-        log.info("Headers: " + headers);
-
-        if (StringUtils.isNotBlank(body)) {
-            log.info("Request body : " + body);
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nMAT request URI=" + requestURI + " METHOD=" + method + "\n" +
+                    "\tHeaders=\n" + headers + "\n" +
+                    "\tBody=\n " + StringUtils.defaultString(body));
+            log.info(builder.toString());
         }
     }
 
     public static void logIncomingResponse(String status, long executionTime, String headers, String body) {
-        log.info("Incoming Response Status: " + status);
-        log.info("Exec Time ms: " + executionTime);
-        log.info("Headers: " + headers);
-
-        if (StringUtils.isNotBlank(body)) {
-            log.info("Response body: " + body);
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nMAT response. Time=" + executionTime + "ms Status=" + status + "\n" +
+                    "\tHeaders=" + headers + "\n" +
+                    "\tBody=\n" + StringUtils.defaultString(body));
+            log.info(builder.toString());
         }
     }
 
     public static void logOutgoingRequest(URI requestURI, String method, String headers, String body) {
-        log.info("Outgoing Request Uri: " + requestURI);
-        log.info("Method: " + method);
-        log.info("Headers: " + headers);
-
-        if (StringUtils.isNotBlank(body)) {
-            log.info("Request body : " + body);
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nExternal request. URI=" + requestURI + " METHOD=" + method + "\n" +
+                    "\tHeaders=\n" + headers + "\n" +
+                    "\tBody=\n " + StringUtils.defaultString(body));
+            log.info(builder.toString());
         }
     }
 
     public static void logOutgoingResponse(String statusCode, String statusText, long executionTime, String headers, String body) {
-        log.info("Outgoing Response Status: " + statusCode);
-
-        if (StringUtils.isNotBlank(statusText)) {
-            log.info("Status Text: " + statusText);
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nExternal response. Time="+ executionTime +"ms Status=" + statusCode + " StatusText=" + StringUtils.defaultString(statusText) + "\n" +
+                    "\tHeaders=\n" + headers + "\n" +
+                    "\tBody=\n" + StringUtils.defaultString(body));
+            log.info(builder.toString());
         }
-
-        log.info("Exec Time ms: " + executionTime);
-        log.info("Headers: " + headers);
-        log.info("Response body: " + body);
     }
 }

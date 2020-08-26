56e44837fec5a01ef949afe72af6431c2ea4d52f
carson.day <carson.day@semanticbits.com>
2020-08-26 16:37:11 -0400

    MAT-1688 Remove ServletLogging changes.

--- src/main/java/mat/server/logging/ServletLogging.java
+++ src/main/java/mat/server/logging/ServletLogging.java
@@ -12,42 +12,42 @@ public final class ServletLogging {
     }
 
     public static void logIncomingRequest(String requestURI, String queryString, String method, String headers, String body) {
-//        if (log.isInfoEnabled()) {
-//            StringBuilder builder = new StringBuilder();
-//            builder.append("\nMAT request URI=" + requestURI + " METHOD=" + method + "\n" +
-//                    "\tHeaders=\n" + headers + "\n" +
-//                    "\tBody=\n " + StringUtils.defaultString(body));
-//            log.info(builder.toString());
-//        }
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nMAT request URI=" + requestURI + " METHOD=" + method + "\n" +
+                    "\tHeaders=\n" + headers + "\n" +
+                    "\tBody=\n " + StringUtils.defaultString(body));
+            log.info(builder.toString());
+        }
     }
 
     public static void logIncomingResponse(String status, long executionTime, String headers, String body) {
-//        if (log.isInfoEnabled()) {
-//            StringBuilder builder = new StringBuilder();
-//            builder.append("\nMAT response. Time=" + executionTime + "ms Status=" + status + "\n" +
-//                    "\tHeaders=" + headers + "\n" +
-//                    "\tBody=\n" + StringUtils.defaultString(body));
-//            log.info(builder.toString());
-//        }
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nMAT response. Time=" + executionTime + "ms Status=" + status + "\n" +
+                    "\tHeaders=" + headers + "\n" +
+                    "\tBody=\n" + StringUtils.defaultString(body));
+            log.info(builder.toString());
+        }
     }
 
     public static void logOutgoingRequest(URI requestURI, String method, String headers, String body) {
-//        if (log.isInfoEnabled()) {
-//            StringBuilder builder = new StringBuilder();
-//            builder.append("\nExternal request. URI=" + requestURI + " METHOD=" + method + "\n" +
-//                    "\tHeaders=\n" + headers + "\n" +
-//                    "\tBody=\n " + StringUtils.defaultString(body));
-//            log.info(builder.toString());
-//        }
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nExternal request. URI=" + requestURI + " METHOD=" + method + "\n" +
+                    "\tHeaders=\n" + headers + "\n" +
+                    "\tBody=\n " + StringUtils.defaultString(body));
+            log.info(builder.toString());
+        }
     }
 
     public static void logOutgoingResponse(String statusCode, String statusText, long executionTime, String headers, String body) {
-//        if (log.isInfoEnabled()) {
-//            StringBuilder builder = new StringBuilder();
-//            builder.append("\nExternal response. Time="+ executionTime +"ms Status=" + statusCode + " StatusText=" + StringUtils.defaultString(statusText) + "\n" +
-//                    "\tHeaders=\n" + headers + "\n" +
-//                    "\tBody=\n" + StringUtils.defaultString(body));
-//            log.info(builder.toString());
-//        }
+        if (log.isInfoEnabled()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("\nExternal response. Time="+ executionTime +"ms Status=" + statusCode + " StatusText=" + StringUtils.defaultString(statusText) + "\n" +
+                    "\tHeaders=\n" + headers + "\n" +
+                    "\tBody=\n" + StringUtils.defaultString(body));
+            log.info(builder.toString());
+        }
     }
 }

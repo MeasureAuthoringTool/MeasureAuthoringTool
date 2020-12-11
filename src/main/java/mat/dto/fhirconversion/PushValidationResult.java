4d5f15f368736d65ee5e3362c0b7ca26a616d632
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2020-12-11 10:29:54 -0500

    Added @AllArgsConstructor

--- src/main/java/mat/dto/fhirconversion/PushValidationResult.java
+++ src/main/java/mat/dto/fhirconversion/PushValidationResult.java
@@ -1,5 +1,6 @@
 package mat.dto.fhirconversion;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.util.List;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class PushValidationResult {
     boolean isValid;
     String url;

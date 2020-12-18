33452586b580d9732b9ea22b4f48184b0700b29b
carson.day <carson.day@semanticbits.com>
2020-12-18 11:48:19 -0500

    BUGFIX rebuilt widget

--- war/signInWidget/js/okta-sign-in.js
+++ war/signInWidget/js/okta-sign-in.js
@@ -51691,9 +51691,167 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;
 
 /***/ }),
 /* 220 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed: SyntaxError: Unexpected token, expected , (149:4)\n\n  147 |     }\n  148 |     \n> 149 |     try {\n      |     ^\n  150 |       window.setTimeout(function () {\n  151 |         document.querySelector('.email-request-button').focus();\n  152 |       },300);\n");
+"use strict";
+var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;
+
+/*!
+ * Copyright (c) 2015-2016, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+/* eslint complexity: [2, 7] */
+!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(0), __webpack_require__(4), __webpack_require__(6)], __WEBPACK_AMD_DEFINE_RESULT__ = (function (Okta, Q, TextBox) {
+
+  var subtitleTpl = Okta.Handlebars.compile('({{subtitle}})');
+  var _ = Okta._;
+  var API_RATE_LIMIT = 30000; //milliseconds
+  var warningTemplate = '<div class="okta-form-infobox-warning infobox infobox-warning login-timeout-warning">\
+                           <span class="icon warning-16"></span>\
+                           <p>{{{warning}}}</p>\
+                         </div>\
+                         ';
+
+  function getFormAndButtonDetails(factorType) {
+    switch (factorType) {
+      case 'sms':
+        return {
+          buttonDataSe: 'sms-send-code',
+          buttonClassName: 'sms-request-button',
+          formSubmit: Okta.loc('mfa.sendCode', 'login'),
+          formRetry: Okta.loc('mfa.resendCode', 'login'),
+          formSubmitted: Okta.loc('mfa.sent', 'login'),
+          subtitle: subtitleTpl({ subtitle: this.model.get('phoneNumber') }),
+          warning: Okta.loc('factor.sms.time.warning', 'login')
+        };
+      case 'call':
+        return {
+          buttonDataSe: 'make-call',
+          buttonClassName: 'call-request-button',
+          formSubmit: Okta.loc('mfa.call', 'login'),
+          formRetry: Okta.loc('mfa.redial', 'login'),
+          formSubmitted: Okta.loc('mfa.calling', 'login'),
+          subtitle: subtitleTpl({ subtitle: this.model.get('phoneNumber') }),
+          warning: Okta.loc('factor.call.time.warning', 'login')
+        };
+      case 'email':
+        return {
+          buttonDataSe: 'email-send-code',
+          buttonClassName: 'email-request-button',
+          formSubmit: Okta.loc('mfa.sendEmail', 'login'),
+          formRetry: Okta.loc('mfa.resendEmail', 'login'),
+          formSubmitted: Okta.loc('mfa.sent', 'login'),
+          subtitle: subtitleTpl({ subtitle: this.model.get('email') })
+        };
+      default:
+        return {
+          buttonDataSe: '',
+          buttonClassName: '',
+          formSubmit: '',
+          formRetry: '',
+          formSubmitted: ''
+        };
+    }
+  }
+
+  return Okta.Form.extend({
+    className: 'mfa-verify-passcode',
+    autoSave: true,
+    noCancelButton: true,
+    save: _.partial(Okta.loc, 'mfa.challenge.verify', 'login'),
+    scrollOnError: false,
+    layout: 'o-form-theme',
+
+    disableSubmitButton: function disableSubmitButton() {
+      return this.model.appState.get('isMfaChallenge') && this.model.get('answer');
+    },
+
+    showWarning: function showWarning(msg) {
+      this.clearWarnings();
+      this.add(warningTemplate, '.o-form-error-container', { options: { warning: msg } });
+    },
+    clearWarnings: function clearWarnings() {
+      this.$('.okta-form-infobox-warning').remove();
+    },
+
+    initialize: function initialize() {
+      var form = this;
+      this.title = this.model.get('factorLabel');
+
+      var factorType = this.model.get('factorType');
+      var formAndButtonDetails = getFormAndButtonDetails.call(this, factorType);
+      var warningDetails = formAndButtonDetails.warning;
+      this.$el.attr('data-se', 'factor-' + factorType);
+
+      this.subtitle = formAndButtonDetails.subtitle;
+      this.listenTo(this.model, 'error', function () {
+        this.clearErrors();
+      });
+      this.addInput({
+        label: Okta.loc('mfa.challenge.enterCode.placeholder', 'login'),
+        'label-top': true,
+        className: 'o-form-fieldset o-form-label-top auth-passcode',
+        name: 'answer',
+        input: TextBox,
+        type: 'tel'
+      });
+      this.add(Okta.createButton({
+        attributes: { 'data-se': formAndButtonDetails.buttonDataSe },
+        className: 'button ' + formAndButtonDetails.buttonClassName + ' no-left-margin no-float',
+        title: formAndButtonDetails.formSubmit,
+        click: function click() {
+          form.clearErrors();
+          this.disable();
+          form.clearWarnings();
+          this.options.title = formAndButtonDetails.formSubmitted;
+          this.render();
+          // To send an OTP to the device, make the same request but use
+          // an empty passCode
+          this.model.set('answer', '');
+          this.model.save().then(function () {
+            // render and focus on the passcode input field.
+            form.getInputs().first().render().focus();
+            return Q.delay(API_RATE_LIMIT);
+          }).then(_.bind(function () {
+            this.options.title = formAndButtonDetails.formRetry;
+            this.enable();
+            if (factorType === 'call' || factorType === 'sms') {
+              form.showWarning(warningDetails);
+            }
+            this.render();
+          }, this));
+        }
+      }), '.o-form-fieldset', { prepend: true });
+
+      if (this.options.appState.get('allowRememberDevice')) {
+        this.addInput({
+          label: false,
+          'label-top': true,
+          placeholder: this.options.appState.get('rememberDeviceLabel'),
+          className: 'margin-btm-0',
+          name: 'rememberDevice',
+          type: 'checkbox'
+        });
+      }
+
+      try {
+        window.setTimeout(function () {
+          document.querySelector('.email-request-button').focus();
+        }, 300);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
+}).apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
 
 /***/ }),
 /* 221 */

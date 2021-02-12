607985f4602f160af327df3aa9f7073536d3da73
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2021-02-12 15:28:32 -0500

    unit test import changes

--- src/test/java/mat/server/service/impl/MeasurePackageServiceImplTest.java
+++ src/test/java/mat/server/service/impl/MeasurePackageServiceImplTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class MeasurePackageServiceImplTest {
@@ -21,9 +21,9 @@ class MeasurePackageServiceImplTest {
     private MeasurePackageService measurePackageService;
 
     @Test
-    private void testReturnMaxEMeasureId() {
-        when(measureDAO.getMaxEMeasureId()).thenReturn(1024);
+    void testReturnMaxEMeasureId() {
+        Mockito.when(measureDAO.getMaxEMeasureId()).thenReturn(1024);
         int maxId = measurePackageService.returnMaxEMeasureId();
-        assertEquals(maxId, 1025);
+        assertEquals(1025 , maxId);
     }
 }

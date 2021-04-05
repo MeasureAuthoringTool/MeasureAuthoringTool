14ffddbf7e9d9f9877623c5b7d3568e5d58fec1e
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2021-04-05 15:10:52 -0400

    fixed snyk issues and MeasurePackageServiceImplTest

--- src/test/java/mat/server/util/fhirxmlclean/CodeCleanerTest.java
+++ src/test/java/mat/server/util/fhirxmlclean/CodeCleanerTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CodeCleanerTest implements FhirCleanerTestHelper {
     private final static String codeName = "Birth date";

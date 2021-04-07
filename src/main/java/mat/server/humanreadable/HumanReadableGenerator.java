1e49bf1df7b24fddc4d9d607944f044f4024d7e4
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2021-04-06 22:06:17 -0400

    MAT-2586 rework

--- src/main/java/mat/server/humanreadable/HumanReadableGenerator.java
+++ src/main/java/mat/server/humanreadable/HumanReadableGenerator.java
@@ -4,6 +4,8 @@ import freemarker.template.TemplateException;
 import mat.client.measure.service.CQLService;
 import mat.client.shared.MatContext;
 import mat.dao.clause.CQLLibraryDAO;
+import mat.dao.clause.MeasureXMLDAO;
+import mat.model.clause.MeasureXML;
 import mat.model.cql.CQLDefinition;
 import mat.model.cql.CQLFunctions;
 import mat.model.cql.CQLModel;
@@ -77,7 +79,9 @@ public class HumanReadableGenerator {
 
     @Autowired
     private FhirMeasureRemoteCall fhirMeasureRemoteCall;
-    private Consumer<HumanReadableCodeModel> humanReadableCodeModelConsumer;
+
+    @Autowired
+    private MeasureXMLDAO measureXMLDAO;
 
     public String generateHTMLForPopulationOrSubtree(String measureId, String subXML, String measureXML, CQLLibraryDAO cqlLibraryDAO) {
 
@@ -118,9 +122,10 @@ public class HumanReadableGenerator {
         log.debug("Generating human readable for ver:" + measureReleaseVersion);
         if (MatContext.get().isCQLMeasure(measureReleaseVersion)) {
             try {
+                MeasureXML measureXML = measureXMLDAO.findForMeasure(measureId);
                 XmlProcessor processor = new XmlProcessor(simpleXml);
 
-                CQLModel cqlModel = CQLUtilityClass.getCQLModelFromXML(simpleXml);
+                CQLModel cqlModel = CQLUtilityClass.getCQLModelFromXML(measureXML.getMeasureXMLAsString());
                 String cqlString = CQLUtilityClass.getCqlString(cqlModel, "").getLeft();
 
                 CQLArtifactHolder usedCQLArtifactHolder = CQLUtil.getCQLArtifactsReferredByPoplns(processor.getOriginalDoc());

1b454eb320437f1f8afb964d018bf63fc66d8fc6
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2021-04-22 10:27:23 -0400

    Delete deprecated HQMF Generators
    Deleting old HQMF generation code that was tailored for QDM versions that are no longer supported by the MAT.

--- src/main/java/mat/server/hqmf/HQMFGeneratorFactory.java
+++ src/main/java/mat/server/hqmf/HQMFGeneratorFactory.java
@@ -20,18 +20,7 @@ public class HQMFGeneratorFactory {
 		if (matVersion >= 6.05) {
 			logger.debug("HQMF Generator Factory selected QDM v5.6 HQMF Generator");
 			return new mat.server.hqmf.qdm_5_6.HQMFGenerator();
-		} else if (matVersion >= 5.8) {
-			logger.debug("HQMF Generator Factory selected QDM v5.5 HQMF Generator");
-			return new mat.server.hqmf.qdm_5_5.HQMFGenerator();
-		} else if (matVersion == 5.6 || matVersion == 5.7) {
-			logger.debug("HQMF Generator Factory selected QDM v5.4 HQMF Generator");
-			return new mat.server.hqmf.qdm_5_4.HQMFGenerator();
-		} else if (matVersion >= 5.0 && matVersion < 5.6) {
-			logger.debug("HQMF Generator Factory selected QDM v5.3 HQMF Generator");
-			return new mat.server.hqmf.qdm_5_3.HQMFGenerator();
-		} else {
-			logger.debug("HQMF Generator Factory selected QDM v4.x HQMF Generator");
-			return new mat.server.hqmf.qdm.HQMFGenerator();
 		}
+		throw new IllegalArgumentException("Unknown MAT Version: " + matVersionNumber);
 	}
 }

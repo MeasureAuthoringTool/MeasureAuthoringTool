5b1ca8213ee13ac8dbd521e8d8bfe6405cfe7241
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2021-05-06 09:39:30 -0400

    Add comment reminding us not to delete the old HQMF generators

--- src/main/java/mat/server/hqmf/HQMFGeneratorFactory.java
+++ src/main/java/mat/server/hqmf/HQMFGeneratorFactory.java
@@ -6,14 +6,19 @@ import org.springframework.stereotype.Component;
 
 /**
  * Factory class to select the proper HQMF generator based on version of MAT. This class specifically excludes v3 MAT HQMF files
- * due to the fact that they do not follow the same pattern. This class would not suit the needs of v3 MAT HQMF files.  
+ * due to the fact that they do not follow the same pattern. This class would not suit the needs of v3 MAT HQMF files.
+ *
+ * DO NOT DELETE OLDER GENERATORS.
+ *
+ * On Export, MAT will serve up the latest pre-existing instance in the MEASURE_EXPORT table. If no HQMF exists,
+ * then MAT will generate the HQMF during the Export operation and save it to the table. Older versions of the Generators
+ * are kept to ensure the HQMF can be generated on-demand.
  */
 @Component
 public class HQMFGeneratorFactory {
-	
+
 	private static final Log logger = LogFactory.getLog(HQMFGeneratorFactory.class);
 
-	
 	public Generator getHQMFGenerator(String matVersionNumber) {
 		matVersionNumber = matVersionNumber.replace("v", "");
 		double matVersion = Double.parseDouble(matVersionNumber);
--- src/main/java/mat/server/hqmf/QDMTemplateProcessorFactory.java
+++ src/main/java/mat/server/hqmf/QDMTemplateProcessorFactory.java
@@ -7,6 +7,13 @@ import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+/**
+ * DO NOT DELETE OLDER GENERATORS.
+ *
+ * On Export, MAT will serve up the latest pre-existing instance in the MEASURE_EXPORT table. If no HQMF exists,
+ * then MAT will generate the HQMF during the Export operation and save it to the table. Older versions of the Generators
+ * are kept to ensure the HQMF can be generated on-demand.
+ */
 public class QDMTemplateProcessorFactory {
 	
 	public static XmlProcessor getTemplateProcessor(double qdmVersion) {

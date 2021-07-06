075ec389328418789088d651444830a14723bb20
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2021-07-06 10:23:11 -0400

    Add mapping values for VSAC Error responses
    Breaking up the generic "request failed" mapping to provide more specific error messages to end users:
    - Request not found on 404
    - Server error on 5XX responses
    
    There are additional custom response enumerations in `VsacApiResult` that map to int values <10, so these new mapping values are int values >90 to avoid conflicts.

--- src/main/java/mat/vsacmodel/BasicResponse.java
+++ src/main/java/mat/vsacmodel/BasicResponse.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class BasicResponse implements Serializable {
     public static final int REQUEST_TIMEDOUT = 3;
     public static final int REQUEST_FAILED = 4;
+    public static final int REQUEST_NOT_FOUND = 98;
+    public static final int SERVER_ERROR = 99;
 
     private String xmlPayLoad;
     private List<String> pgmRels;
